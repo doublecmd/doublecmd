39b75f4b2bd645a9e43c19547003cf0f377f5d38
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2008-01-03 16:52:39 +0000

    ADD: German and Spanish languages UPD: _make.bat for creating *.lrs files

--- trunk/_make.bat
+++ trunk/_make.bat
@@ -1,4 +1,9 @@
-
-set lazpath=X:\Prog\lazarus
-
-fpc doublecmd.lpr -S2cdgi -OG3 -g -gl -vewnhi -l -Ficomponents\KASToolBar\ -Ficomponents\KASToolBar\lib\i386-win32\ -Ficomponents\viewer\ -Fu%lazpath%\components\jpeg\lib\i386-win32\ -Fucomponents\KASToolBar\lib\i386-win32\ -Fu%lazpath%\components\synedit\units\i386-win32\ -Fu%lazpath%\lcl\units\i386-win32\ -Fu%lazpath%\lcl\units\i386-win32\win32\ -Fucomponents\viewer\lib\i386-win32\ -Fu%lazpath%\packager\units\i386-win32\ -Fu. -odoublecmd 
+rem the next line must be changed before run on your computer 
+set lazpath=X:\Prog\lazarus 
+ 
+for %%f in (*.lfm) do %lazpath%\Tools\lazres %%~nf.lrs %%f 
+ 
+%lazpath%\lazbuild.exe components\KASToolBar\kascomp.lpk 
+%lazpath%\lazbuild.exe components\viewer\viewerpackage.lpk 
+ 
+fpc doublecmd.lpr -S2cdgi -OG3 -g -gl -vewnhi -l -Ficomponents\KASToolBar\ -Ficomponents\KASToolBar\lib\i386-win32\ -Ficomponents\viewer\ -Fu%lazpath%\components\images\lib\i386-win32\ -Fucomponents\KASToolBar\lib\i386-win32\ -Fu%lazpath%\components\synedit\units\i386-win32\ -Fu%lazpath%\lcl\units\i386-win32\ -Fu%lazpath%\lcl\units\i386-win32\win32\ -Fucomponents\viewer\lib\i386-win32\ -Fu%lazpath%\packager\units\i386-win32\ -Fu. -odoublecmd.exe
