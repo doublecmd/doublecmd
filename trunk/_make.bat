9e937e30d52b712d10b8aba453d508a024a0ee32
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2008-01-10 21:22:24 +0000

    FIX: Bug [ 1861911 ] must add version.inc

--- trunk/_make.bat
+++ trunk/_make.bat
@@ -1,9 +1,9 @@
 rem the next line must be changed before run on your computer 
-set lazpath=X:\Prog\lazarus 
+set lazpath=X:\Prog\lazarus
  
-for %%f in (*.lfm) do %lazpath%\Tools\lazres %%~nf.lrs %%f 
+for %%f in (*.lfm) do %lazpath%\Tools\lazres %%~nf.lrs %%f
  
-%lazpath%\lazbuild.exe components\KASToolBar\kascomp.lpk 
-%lazpath%\lazbuild.exe components\viewer\viewerpackage.lpk 
+%lazpath%\lazbuild.exe components\KASToolBar\kascomp.lpk
+%lazpath%\lazbuild.exe components\viewer\viewerpackage.lpk
  
-fpc doublecmd.lpr -S2cdgi -OG3 -g -gl -vewnhi -l -Ficomponents\KASToolBar\ -Ficomponents\KASToolBar\lib\i386-win32\ -Ficomponents\viewer\ -Fu%lazpath%\components\images\lib\i386-win32\ -Fucomponents\KASToolBar\lib\i386-win32\ -Fu%lazpath%\components\synedit\units\i386-win32\ -Fu%lazpath%\lcl\units\i386-win32\ -Fu%lazpath%\lcl\units\i386-win32\win32\ -Fucomponents\viewer\lib\i386-win32\ -Fu%lazpath%\packager\units\i386-win32\ -Fu. -odoublecmd.exe
+fpc doublecmd.lpr -S2cdgi -OG3 -g -gl -vewnhi -l -Ficomponents\KASToolBar\ -Ficomponents\KASToolBar\lib\i386-win32\ -Ficomponents\viewer\ -Fu%lazpath%\components\images\lib\i386-win32\ -Fucomponents\KASToolBar\lib\i386-win32\ -Fu%lazpath%\components\synedit\units\i386-win32\ -Fu%lazpath%\lcl\units\i386-win32\ -Fu%lazpath%\lcl\units\i386-win32\win32\ -Fucomponents\viewer\lib\i386-win32\ -Fu%lazpath%\packager\units\i386-win32\ -Fu. -odoublecmd.exe -Fi%lazpath%\ide\
