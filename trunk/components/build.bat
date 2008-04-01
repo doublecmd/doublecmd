cdafa3f12bdc074c9aff655b86f10c042305063a
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2008-04-01 10:18:17 +0000

    UPD: Add parameters support to _make.bat

---
+++ trunk/components/build.bat
@@ -0,0 +1,7 @@
+rem Compiling components
+
+rem This script run from _make.bat
+rem If you run it direct, set up %lazpath% first
+
+%lazpath%\lazbuild.exe components\KASToolBar\kascomp.lpk
+%lazpath%\lazbuild.exe components\viewer\viewerpackage.lpk
