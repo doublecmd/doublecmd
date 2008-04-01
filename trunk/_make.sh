ebce1f179ad63577b1c49020f5e3abfa12975055
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2008-04-01 19:55:36 +0000

    UPD: Add parameters support to _make.sh

--- trunk/_make.sh
+++ trunk/_make.sh
@@ -7,24 +7,51 @@
 # "./_make.sh all" it create lrs file and build doublecmd  
 # and you can comment last line and uncomment next to last to use lazbuild
 #                                                 by Attid
-# ToDo recompile component when use parameter "all"
+
+
+# You can execute this script with different parameters:
+# components - compiling components needed for DC
+# plugins - compiling all DC plugins
+# doublecmd - compiling only DC without *.lrs files generation
+# all - compiling components, plugins and DC with *.lrs files generation
+# default - compiling DC with *.lrs files generation (using by default)
 
 #path to lazarus
-lazpath=/root/Prog/lazarus
+export lazpath=/home/alexx/bin/lazarus
 #widgetset gtk or gtk2
 lcl=gtk2
+
+build_doublecmd()
+{
+  #$lazpath/lazbuild --widgetset=$lcl doublecmd.lpi
+  fpc doublecmd.lpr -S2cdgi -OG3 -g -gl -vewnhi -l -Ficomponents/KASToolBar/ -Ficomponents/KASToolBar/lib/i386-linux/ -Ficomponents/viewer/ -Fu$lazpath/components/jpeg/lib/i386-linux/ -Fucomponents/KASToolBar/lib/i386-linux/ -Fu$lazpath/components/synedit/units/i386-linux/ -Fu$lazpath/lcl/units/i386-linux/ -Fu$lazpath/lcl/units/i386-linux/$lcl/ -Fucomponents/viewer/lib/i386-linux/ -Fu$lazpath/packager/units/i386-linux/ -Fu. -odoublecmd -dLCL -dLCL$lcl
+}
+
+build_default()
+{
+  for LFM in `ls ./*.lfm`
+  do
+    LRS=`echo $LFM | sed -e 's/\.lfm$/\.lrs/'`
+    $lazpath/tools/lazres $LRS $LFM
+  done
+  build_doublecmd
+}
+
+build_all()
+{
+  components/build.sh
+  plugins/build.sh
+  build_default
+  build_doublecmd
+}
 
 rm interface.inc
 echo "{\$DEFINE $lcl}">interface.inc
 
-if [ "$1" = "all" ]
-then
-  for LFM in `ls /at/project/doublecmd/*.lfm`
-  do
-    LRS=`echo $LFM | sed -e 's/\.lfm$/\.lrs/'`
-    /home/at/lazarus/svn/tools/lazres $LRS $LFM
-  done
-fi
-
-#$lazpath/lazbuild --widgetset=$lcl doublecmd.lpi
-fpc doublecmd.lpr -S2cdgi -OG3 -g -gl -vewnhi -l -Ficomponents/KASToolBar/ -Ficomponents/KASToolBar/lib/i386-linux/ -Ficomponents/viewer/ -Fu$lazpath/components/jpeg/lib/i386-linux/ -Fucomponents/KASToolBar/lib/i386-linux/ -Fu$lazpath/components/synedit/units/i386-linux/ -Fu$lazpath/lcl/units/i386-linux/ -Fu$lazpath/lcl/units/i386-linux/$lcl/ -Fucomponents/viewer/lib/i386-linux/ -Fu$lazpath/packager/units/i386-linux/ -Fu. -odoublecmd -dLCL -dLCL$lcl
+case $1 in
+  components)  components/build.sh;;
+     plugins)  plugins/build.sh;;
+   doublecmd)  build_doublecmd;;
+         all)  build_all;;
+           *)  build_default;;
+esac
