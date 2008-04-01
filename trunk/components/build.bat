61138c44dc4738f38361235bf1d735c1c3eab4cd
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2008-04-01 19:04:46 +0000

    ADD: build.sh for compiling components

--- trunk/components/build.bat
+++ trunk/components/build.bat
@@ -3,5 +3,7 @@ rem Compiling components
 rem This script run from _make.bat
 rem If you run it direct, set up %lazpath% first
 
-%lazpath%\lazbuild.exe components\KASToolBar\kascomp.lpk
-%lazpath%\lazbuild.exe components\viewer\viewerpackage.lpk
+pushd components
+%lazpath%\lazbuild.exe KASToolBar\kascomp.lpk
+%lazpath%\lazbuild.exe viewer\viewerpackage.lpk
+popd
