4edc2843d5db0e6f8746c5671c40065d76fafac8
Alexander Koblov <alexx2000@mail.ru>
2008-04-06 10:57:09 +0000

    ADD: Building Slackware *.tgz package

--- install/create_packages.sh
+++ install/create_packages.sh
@@ -19,11 +19,15 @@ cd $BUILD_DC_TMP_DIR
 
 # Create *.rpm package
 
-/usr/local/sbin/checkinstall -R --default --pkgname=doublecmd --pkgversion=0.3.5 --pkgrelease=alpha --pkglicense=GPL --pkggroup=Applications/File --maintainer=Alexx2000@mail.ru --nodoc --pakdir=$PACK_DIR $BUILD_DC_TMP_DIR/install/linux/install.sh
+/usr/local/sbin/checkinstall -R --default --pkgname=doublecmd --pkgversion=$DC_VER --pkglicense=GPL --pkggroup=Applications/File --maintainer=Alexx2000@mail.ru --nodoc --pakdir=$PACK_DIR $BUILD_DC_TMP_DIR/install/linux/install.sh
 
 # Create *.deb package
 
 /usr/local/sbin/checkinstall -D --default --pkgname=doublecmd --pkgversion=$DC_VER --pkglicense=GPL --pkggroup=Applications/File --maintainer=Alexx2000@mail.ru --nodoc --pakdir=$PACK_DIR $BUILD_DC_TMP_DIR/install/linux/install.sh
+
+# Create *.tgz package
+
+/usr/local/sbin/checkinstall -S --default --pkgname=doublecmd --pkgversion=$DC_VER --pkglicense=GPL --pkggroup=Applications/File --maintainer=Alexx2000@mail.ru --nodoc --pakdir=$PACK_DIR $BUILD_DC_TMP_DIR/install/linux/install.sh
 
 # Clean DC build dir
 rm -rf $BUILD_DC_TMP_DIR
