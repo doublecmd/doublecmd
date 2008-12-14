e9f08f5fcf1808b38baa6ee24d9758e17af42ad4
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2008-12-14 14:07:43 +0000

    FIX: Small fix

--- trunk/_getsvnrev.cmd
+++ trunk/_getsvnrev.cmd
@@ -5,3 +5,5 @@ $1/tools/svn2revisioninc ./ dcrevision.inc --c=dcRevision
 
 rem get DC revision under Windows
 %1\tools\svn2revisioninc .\ dcrevision.inc --c=dcRevision
+
+echo "This command is need for successful exit code"
