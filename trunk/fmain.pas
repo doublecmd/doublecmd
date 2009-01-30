0bf938f7f473c478cfa3aa5f6f831959b6b8e17b
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2009-01-30 21:08:26 +0000

    FIX: Start quick search from command line

--- trunk/fmain.pas
+++ trunk/fmain.pas
@@ -672,6 +672,7 @@ end;
 
 procedure TfrmMain.FormUTF8KeyPress(Sender: TObject; var UTF8Key: TUTF8Char);
 begin
+  if edtCommand.Focused then Exit;
   // quick search by Letter only
   if (Length(UTF8Key) = 1) and ((Ord(UTF8Key[1]) < 32) or
      (UTF8Key[1] in ['+','-','*','/','\'])) then Exit;
