f7b2f47619d4decf4c1f3a4c25209d661013a05d
cobines <cobines@users.sourceforge.net>
2009-04-01 16:44:04 +0000

    UPD: Show a few other NTFS attributes: compressed, encrypted, sparse, temporary

--- uvfsutil.pas
+++ uvfsutil.pas
@@ -55,14 +55,12 @@ function IsFileInPath(const sPath : String; sFilePath : String; AllowSubDirs: Bo
 procedure ChangeFileListRoot(sNewRootPath: String; var FileList: TFileList);
 
 function ExtractDirLevel(const sPrefix, sPath: String): String;
-function ls2FileInfo(sls:string):TFileRecItem;
-// convert line in ls -la (or vfs) format to our structure
-function ModeStr2Mode(const sMode:String):Integer;
 
 implementation
 
 uses
-  SysUtils, uFileOp, uOSUtils, uFindEx, LCLProc {$IFDEF UNIX}, BaseUnix{$ENDIF};
+  SysUtils, uFileOp, uOSUtils, uFindEx, LCLProc {$IFDEF UNIX}, BaseUnix{$ENDIF}
+  {$IF DEFINED(MSWINDOWS)} , Windows {$ENDIF};
 
 { TFileList }
 
@@ -73,7 +71,6 @@ var
   i:Integer;
   ptr:PFileRecItem;
   sRealName : String;
-  sr : TSearchRec;
   NewFileList: TFileList;
 
 procedure FillAndCountRec(const srcPath, dstPath:String);
@@ -164,11 +161,13 @@ begin
   fi.bExecutable:=False;
   fi.bSysFile := False;
 {$IF DEFINED(MSWINDOWS)}
-  fi.sModeStr:='d-----';
+  fi.iMode := FILE_ATTRIBUTE_DIRECTORY;
 {$ELSE}
-  fi.sModeStr:='drwxr-xr-x';
+  fi.iMode :=__S_IFDIR or S_IRUSR or S_IWUSR or S_IXUSR  // 'drwxr-xr-x';
+                       or S_IRGRP or S_IXGRP
+                       or S_IROTH or S_IXOTH;
 {$ENDIF}
-  fi.iMode:=ModeStr2Mode(fi.sModeStr); //!!!!
+  fi.sModeStr:=AttrToStr(fi.iMode);
   fi.iDirSize:=0;
   fi.sPath := sUpPath;
   ls.AddItem(@fi);
@@ -267,105 +266,5 @@ begin
   else
     Result := sPath;
 end;
-
-function ModeStr2Mode(const sMode:String):Integer;
-begin
-// stupid conversion
-  Result:=0;
-{$IFDEF WINDOWS}
-
-if sMode[1] = 'd' then Result := Result or $10;
-if sMode[2] = 'r' then Result := Result or $01;
-if sMode[3] = 'a' then Result := Result or $20;
-if sMode[4] = 'h' then Result := Result or $02;
-if sMode[5] = 's' then Result := Result or $04;
-if sMode[6] = 'v' then Result := Result or $08;
-
-{$ELSE}
-//  if sMode[1]='-' then Result:=Result+10;
-  if sMode[1]='d' then Result:=Result or __S_IFDIR;
-  if sMode[1]='l' then Result:=Result or __S_IFLNK;
-  if sMode[1]='s' then Result:=Result or __S_IFSOCK;
-  if sMode[1]='f' then Result:=Result or __S_IFIFO;
-  if sMode[1]='b' then Result:=Result or __S_IFBLK;
-  if sMode[1]='c' then Result:=Result or __S_IFCHR;
-
-
-  if sMode[2]='r' then Result:=Result or S_IRUSR;
-  if sMode[3]='w' then Result:=Result or S_IWUSR;
-  if sMode[4]='x' then Result:=Result or S_IXUSR;
-  if sMode[5]='r' then Result:=Result or S_IRGRP;
-  if sMode[6]='w' then Result:=Result or S_IWGRP;
-  if sMode[7]='x' then Result:=Result or S_IXGRP;
-  if sMode[8]='r' then Result:=Result or S_IROTH;
-  if sMode[9]='w' then Result:=Result or S_IWOTH;
-  if sMode[10]='x' then Result:=Result or S_IXOTH;
-
-  if sMode[4]='s' then Result:=Result or S_ISUID;
-  if sMode[7]='s' then Result:=Result or S_ISGID;
-{$ENDIF}
-end;
-
-function ls2FileInfo(sls:string):TFileRecItem;
-var
-  i:Integer;
-  iPoz:Integer;
-begin
-  if sls='' then
-    Raise Exception.Create('ls2FileInfo: sls is empty');
-  Result.sModeStr:=Copy(sls,1,10); // Atrrs
-  Result.iMode:=ModeStr2Mode(Result.sModeStr);
-  iPoz:=11;
-  i:=iPoz;
-  while sls[i]<=' ' do
-    inc(i);
-
-//  Result.iNr:=StrToInt(sls[i]); //???
-  iPoz:=i+1;
-  i:=iPoz+1;
-  while sls[i]>' ' do
-    inc(i);
-
-  Result.sOwner:=Trim(copy(sls,iPoz,i-iPoz)); // Owner
-  while sls[i]<=' ' do
-    inc(i);
-
-  iPoz:=i;
-  while sls[i]>' ' do
-    inc(i);
-  Result.sGroup:=Trim(copy(sls,iPoz,i-iPoz));  //Group
-
-  while sls[i]<=' ' do
-    inc(i);
-
-  iPoz:=i;
-  while sls[i]>' ' do
-    inc(i);
-  Result.iSize:=StrToInt(copy(sls,iPoz,i-iPoz));  // FileSize
-  while sls[i]<=' ' do
-    inc(i);
-
-  iPoz:=i;
-  i:=length(sls);
-  while (sls[i]>' ') and (i>iPoz) do
-    dec(i);
-
-  Result.sTime:=Copy(sls,iPoz,i-iPoz); // date
-//  Result.fTimeI:=StrToDateTime(Result.fTime);
-//  Inc(iPoz,12);
-  iPoz:=i;
-  Result.sName:=Trim(Copy(sls,iPoz,length(sls)-iPoz+1)); // FileName
-  Result.sExt:='';
-  Result.sNameNoExt:=Result.sName;
-
-
-  Result.bSelected:=False;
-  Result.bExecutable:=False;
-  Result.bIsLink:=False;
-  Result.bLinkIsDir:=False;
-  Result.iDirSize:=0;
-end;
-
-
 
 end.
