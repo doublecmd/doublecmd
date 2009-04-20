10b3c171bbf4e5c6fda5207010ff87a3e17bde2a
Alexander Koblov <alexx2000@mail.ru>
2009-04-20 19:34:36 +0000

    UPD: Continue source code relocation

--- _clean.bat
+++ _clean.bat
@@ -1,13 +1,12 @@
 
-delp .\
+delp units\
 
-del /Q f*.lrs
-del /Q dm*.lrs
-del /Q *.or
-del /Q *.*~
-del /Q *.~*
+del /Q units\*.lrs
+del /Q units\*.or
+del /Q src\*.*~
+del /Q src\*.~*
 del /Q doublecmd*.exe
-del /Q doublecmd.compiled
+del /Q units\doublecmd.compiled
 
 del /Q doc\en\dev-help\*.*
 
@@ -18,23 +17,23 @@ del /Q plugins\wcx\zip\lib\*.*
 del /Q plugins\wcx\unbz2\lib\*.*
 del /Q plugins\wcx\unrar\lib\*.*
 
-del /Q plugins\wcx\cpio\*.bak
-del /Q plugins\wcx\rpm\*.bak
-del /Q plugins\wcx\deb\*.bak
-del /Q plugins\wcx\zip\*.bak
-del /Q plugins\wcx\unbz2\*.bak
-del /Q plugins\wcx\unrar\*.bak
+del /Q plugins\wcx\cpio\src\*.bak
+del /Q plugins\wcx\rpm\src\*.bak
+del /Q plugins\wcx\deb\src\*.bak
+del /Q plugins\wcx\zip\src\*.bak
+del /Q plugins\wcx\unbz2\src\*.bak
+del /Q plugins\wcx\unrar\src\*.bak
 
 del /Q plugins\wcx\deb\*.or
 del /Q plugins\wcx\zip\*.or
 del /Q plugins\wcx\unbz2\*.or
 
-del /Q plugins\wcx\cpio\*.*~
-del /Q plugins\wcx\rpm\*.*~
-del /Q plugins\wcx\deb\*.*~
-del /Q plugins\wcx\zip\*.*~
-del /Q plugins\wcx\unbz2\*.*~
-del /Q plugins\wcx\unrar\*.*~
+del /Q plugins\wcx\cpio\src\*.*~
+del /Q plugins\wcx\rpm\src\*.*~
+del /Q plugins\wcx\deb\src\*.*~
+del /Q plugins\wcx\zip\src\*.*~
+del /Q plugins\wcx\unbz2\src\*.*~
+del /Q plugins\wcx\unrar\src\*.*~
 
 del /Q components\CmdLine\lib\i386-win32\*.*
 del /Q components\KASToolBar\lib\i386-win32\*.*
