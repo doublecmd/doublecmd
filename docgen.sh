5ab880d8eda42e98bc2998371bc176b500184e32
Alexander Koblov <alexx2000@mail.ru>
2009-04-22 06:31:06 +0000

    FIX: Documentation generation under Linux

--- docgen.sh
+++ docgen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # http://pasdoc.sourceforge.net
-pasdoc --marker en --output doc/en/dev-help --define UNIX --source units.txt
+pasdoc --marker en --output doc/en/dev-help --define UNIX --source units-doc-unix.txt
