0cdf637331d318b30879a6cd7a62f30d79d63c32
attid <attid@users.sourceforge.net>
2009-05-17 14:42:09 +0000

    error on ubuntu 8.04

--- src/platform/upixmapmanager.pas
+++ src/platform/upixmapmanager.pas
@@ -493,20 +493,20 @@ begin
         generic_icons:= TStringListEx.Create;
         generic_icons.NameValueSeparator:= ':';
         generic_icons.LoadFromFile(mime_generic_icons);
-      end;
-    // fill icons list (format "extension=mimeiconname:genericiconname")
-    for I:= 0 to globs.Count - 1 do
-      begin
-        sGenericIconName:= IfThen(Assigned(generic_icons), generic_icons.Values[globs.Names[I]]);
-        if sGenericIconName <> '' then // if mime has generic icon
+        // fill icons list (format "extension=mimeiconname:genericiconname")
+        for I:= 0 to globs.Count - 1 do
           begin
-            sMimeIconName:= StringReplace(globs.Names[I], '/', '-', []);
-            slGenericIcons.Add(PChar(globs.ValueFromIndex[I])+2 + '=' + sMimeIconName + ':' + sGenericIconName);
-          end
-        else
-          begin
-            sMimeIconName:= StringReplace(globs.Names[I], '/', '-', []);
-            slGenericIcons.Add(PChar(globs.ValueFromIndex[I])+2 + '=' + sMimeIconName);
+            sGenericIconName:= IfThen(Assigned(generic_icons), generic_icons.Values[globs.Names[I]]);
+            if sGenericIconName <> '' then // if mime has generic icon
+              begin
+                sMimeIconName:= StringReplace(globs.Names[I], '/', '-', []);
+                slGenericIcons.Add(PChar(globs.ValueFromIndex[I])+2 + '=' + sMimeIconName + ':' + sGenericIconName);
+              end
+            else
+              begin
+                sMimeIconName:= StringReplace(globs.Names[I], '/', '-', []);
+                slGenericIcons.Add(PChar(globs.ValueFromIndex[I])+2 + '=' + sMimeIconName);
+              end;
           end;
       end;
     Result:= True;
