53b1b1700e9fa08c0177b5535ae6d3be1e58fad1
cobines <cobines@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2009-06-14 18:26:06 +0000

    UPD: Output directories for different cpu and platform.

--- trunk/_clean.sh
+++ trunk/_clean.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-rm -f units/unix/*.*
+rm -f units/i386-linux-gtk2/*
+rm -f units/i386-linux-qt/*
+rm -f units/x86_64-linux-gtk2/*
+rm -f units/x86_64-linux-qt/*
+
+# Clean up components output directories
+rm -rf components/CmdLine/lib/*
+rm -rf components/KASToolBar/lib/*
+rm -rf components/viewer/lib/*
