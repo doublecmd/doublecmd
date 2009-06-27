ddc0df4bed61dfaaee893a2254bd350e4f0a43a9
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2009-06-27 20:43:37 +0000

    ADD: Start to implement console under Windows

---
+++ trunk/src/platform/uterminal.pas
@@ -0,0 +1,106 @@
+{
+   Double Commander
+   -------------------------------------------------------------------------
+   Terminal emulator abstract class
+
+   Copyright (C) 2009  Koblov Alexander (Alexx2000@mail.ru)
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
+unit uTerminal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, uCmdBox;
+
+type
+   Cint = Integer;
+
+   { TTerminal }
+
+   TTerminal = class
+   protected
+     FChildPid: THandle;
+     Fpty: LongInt;
+   public
+     {en
+        Read info from pty
+     }
+     function Read_pty(var Output: UTF8String; const TimeOut: LongInt = 10): LongInt; virtual; abstract;
+     {en
+        Create new pty and start cmd
+     }
+     function Fork_pty(const RowCount, ColCount: Integer; const Command: UTF8String; const Params: UTF8String = ''): THandle; virtual; abstract;
+     {en
+         Write string to pty
+     }
+     function Write_pty(const Input: UTF8String): Boolean; virtual; abstract;
+     //---------------------
+     function SendBreak_pty(): Boolean; virtual; abstract; // ^C
+     function SendSignal_pty(Sig: Cint): Boolean; virtual; abstract;
+     function SetScreenSize(ColCount, RowCount: Integer): Boolean; virtual; abstract;
+     //---------------------
+     function KillShell: LongInt; virtual; abstract;
+     //---------------------}
+     property ShellPid: THandle read FChildPid;
+     property PtyPid: LongInt read Fpty;
+   end;
+
+ { TConsoleThread }
+
+  TConsoleThread = class(TThread)
+  protected
+    FLock: System.TRTLCriticalSection;
+    FTerm: TTerminal;
+    FRowsCount,
+    FColsCount: Integer;
+    FOut: TCmdBox;
+    FShell: String;
+  public
+    property Terminal: TTerminal read FTerm;
+    property RowsCount: integer read FRowsCount write FRowsCount;
+    property ColsCount: integer read FColsCount write FColsCount;
+    property CmdBox: TCmdBox read FOut write FOut;
+    property Shell: String read FShell write FShell;
+  end;
+
+function CreateConsoleThread: TConsoleThread;
+
+implementation
+
+uses
+{$IF DEFINED(WINDOWS)}
+  uWinTerm;
+{$ELSEIF DEFINED(UNIX)}
+  uTerm;
+{$ENDIF}
+
+function CreateConsoleThread: TConsoleThread;
+{$IF DEFINED(WINDOWS)}
+begin
+  Result:= TWinConThread.Create;
+end;
+{$ELSEIF DEFINED(UNIX)}
+begin
+  Result:= TConThread.Create;
+end;
+{$ENDIF}
+
+end.
+
