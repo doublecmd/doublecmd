9090891d033257f2bb10bcb1cf5f0ec5bd9435a5
Alexander Koblov <alexx2000@mail.ru>
2009-07-03 05:18:01 +0000

    FIX: Don't show service mount points

--- src/platform/uOSUtils.pas
+++ src/platform/uOSUtils.pas
@@ -786,7 +786,8 @@ end;
       if (mnt_type = 'tmpfs') or (mnt_type = 'proc') or (mnt_type = 'sysfs') or
          (mnt_type = 'devpts') or (mnt_type = 'fusectl') or (mnt_type = 'securityfs') or
          (mnt_type = 'binfmt_misc') or (mnt_type = 'fuse.gvfs-fuse-daemon') or 
-         (mnt_type = 'fuse.truecrypt') then Exit;
+         (mnt_type = 'fuse.truecrypt') or (mnt_type = 'nfsd') or
+		 (mnt_type = 'rpc_pipefs') then Exit;
       // if already added
       for J:= 0 to DriveList.Count - 1 do
         if PDrive(DriveList.Items[J])^.Path = mnt_dir then
