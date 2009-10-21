8ddabe5a31dbb76d039864f2525757741aea54c8
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2009-10-21 16:37:16 +0000

    FIX: Incorrect wlx plugin name when add new plugin

--- trunk/src/foptions.pas
+++ trunk/src/foptions.pas
@@ -1430,8 +1430,7 @@ begin
   odOpenDialog.Filter := 'Viewer plugins (*.wlx)|*.wlx';
   if odOpenDialog.Execute then
     begin
-      sPluginName := ExtractFileName(odOpenDialog.FileName);
-      delete(sPluginName,length(sPluginName)-4,4);
+      sPluginName := ExtractOnlyFileName(odOpenDialog.FileName);
       I:= tmpWLXPlugins.Add(sPluginName,odOpenDialog.FileName,'');
 
       tmpWLXPlugins.LoadModule(sPluginName);
