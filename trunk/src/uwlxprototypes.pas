0fb9c73aa6ca7134287767ce93e5bb0be32bc371
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2009-10-23 17:59:10 +0000

    ADD: Unicode support to wlx plugins

--- trunk/src/uwlxprototypes.pas
+++ trunk/src/uwlxprototypes.pas
@@ -5,21 +5,28 @@ unit uwlxprototypes;
 interface
 
 uses
-  Classes, SysUtils,WLXPlugin;
+  Classes, SysUtils, WlxPlugin;
 
 type
-    TListLoad= function (ParentWin:thandle;FileToLoad:pchar;ShowFlags:integer):thandle; stdcall;
-    TListLoadNext=  function (ParentWin,PluginWin:thandle;FileToLoad:pchar;ShowFlags:integer):integer; stdcall;
-    TListCloseWindow=  procedure (ListWin:thandle); stdcall;
-    TListGetDetectString=  procedure (DetectString:pchar;maxlen:integer); stdcall;
-    TListSearchText=  function (ListWin:thandle;SearchString:pchar; SearchParameter:integer):integer; stdcall;
-    TListSearchDialog=  function (ListWin:thandle;FindNext:integer):integer; stdcall;
-    TListSendCommand=  function (ListWin:thandle;Command,Parameter:integer):integer; stdcall;
-    TListPrint=  function (ListWin:thandle;FileToPrint,DefPrinter:pchar; PrintFlags:integer;var Margins:trect):integer; stdcall;
-    TListNotificationReceived=  function (ListWin:thandle;Message,wParam,lParam:integer):integer; stdcall;
-    TListSetDefaultParams=  procedure (dps:pListDefaultParamStruct); stdcall;
-    TListGetPreviewBitmap=  function (FileToLoad:pchar;width,height:integer; contentbuf:pchar;contentbuflen:integer):hbitmap; stdcall;
-
+    { Mandatory }
+    TListLoad = function (ParentWin:thandle;FileToLoad:pchar;ShowFlags:integer):thandle; stdcall;
+    { Optional }
+    TListLoadNext = function (ParentWin,PluginWin:thandle;FileToLoad:pchar;ShowFlags:integer):integer; stdcall;
+    TListCloseWindow = procedure (ListWin:thandle); stdcall;
+    TListGetDetectString = procedure (DetectString:pchar;maxlen:integer); stdcall;
+    TListSearchText = function (ListWin:thandle;SearchString:pchar; SearchParameter:integer):integer; stdcall;
+    TListSearchDialog = function (ListWin:thandle;FindNext:integer):integer; stdcall;
+    TListSendCommand = function (ListWin:thandle;Command,Parameter:integer):integer; stdcall;
+    TListPrint = function (ListWin:thandle;FileToPrint,DefPrinter:pchar; PrintFlags:integer;var Margins:trect):integer; stdcall;
+    TListNotificationReceived = function (ListWin:thandle;Message,wParam,lParam:integer):integer; stdcall;
+    TListSetDefaultParams = procedure (dps:pListDefaultParamStruct); stdcall;
+    TListGetPreviewBitmap = function (FileToLoad:pchar;width,height:integer; contentbuf:pchar;contentbuflen:integer):hbitmap; stdcall;
+    { Unicode }
+    TListLoadW = function (ParentWin:thandle;FileToLoad:pwidechar;ShowFlags:integer):thandle; stdcall;
+    TListLoadNextW = function (ParentWin,PluginWin:thandle;FileToLoad:pwidechar;ShowFlags:integer):integer; stdcall;
+    TListSearchTextW = function (ListWin:thandle;SearchString:pwidechar; SearchParameter:integer):integer; stdcall;
+    TListPrintW = function (ListWin:thandle;FileToPrint,DefPrinter:pwidechar; PrintFlags:integer;var Margins:trect):integer; stdcall;
+    TListGetPreviewBitmapW = function (FileToLoad:pwidechar;width,height:integer; contentbuf:pchar;contentbuflen:integer):hbitmap; stdcall;
 
 implementation
 
