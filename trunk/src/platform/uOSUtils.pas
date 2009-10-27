0a2027d8a372c77febcfb259808b05546a574cf4
cobines <cobines@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2009-10-27 06:36:02 +0000

    FIX: Compiling with latest FPC - clash between UTF8Encode(UnicodeString) and UTF8Encode(WideString) on Windows.

--- trunk/src/platform/uOSUtils.pas
+++ trunk/src/platform/uOSUtils.pas
@@ -1716,7 +1716,7 @@ begin
           hp:= hp + lstrlenW(hp) + 1;
         end;
       if (hp^ <> #0) then
-        Result:= UTF8Encode(hp);
+        Result:= UTF8Encode(WideString(hp));
     end;
   FreeEnvironmentStringsW(p);
 end;
