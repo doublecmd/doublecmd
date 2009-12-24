d78d0200c0deac2e02c3c422d937cdb8dd86de19
Alexander Koblov <alexx2000@mail.ru>
2009-12-24 18:15:45 +0000

    FIX: Small fix

--- src/fcolumnssetconf.pas
+++ src/fcolumnssetconf.pas
@@ -347,6 +347,7 @@ begin
              Left:=(Sender as TStringGrid).CellRect(aCol,aRow).Right-Width;
              Top:=(Sender as TStringGrid).CellRect(aCol,aRow).Top;
              Height:=(Sender as TStringGrid).RowHeights[aRow];
+             Tag:=aRow;
              Show;
            end;
 
