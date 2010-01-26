326dbde92f2ff42f0a97fe7c0767bb3d716178c6
cobines <cobines@users.sourceforge.net>
2010-01-26 06:41:24 +0000

    UPD: Move file to unix platform dir.

--- src/platform/unix/umimeactions.pas
+++ src/platform/unix/umimeactions.pas
@@ -1,3 +1,28 @@
+{
+    Double Commander
+    -------------------------------------------------------------------------
+    Handles actions associated with MIME types in .desktop files.
+
+    Based on FreeDesktop.org specification
+    (http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html)
+
+    Copyright (C) 2009-2010  Przemyslaw Nagay (cobines@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+}
+
 unit uMimeActions;
 
 {$mode objfpc}{$H+}
