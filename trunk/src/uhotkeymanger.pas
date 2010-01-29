4acb10448ce1257f91f1656096d2e152ce2d3429
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2010-01-29 16:45:38 +0000

    FIX: Don't execute hotkeys that coincide with quick search combination

--- trunk/src/uhotkeymanger.pas
+++ trunk/src/uhotkeymanger.pas
@@ -796,6 +796,8 @@ begin
  if i=-1 then exit;
  Sinfo:=TObjInfoClass(FFormsList.Objects[i]);
 
+ // Don't execute hotkeys that coincide with quick search/filter combination
+ if not (((Shift = gQuickSearchMode) or (Shift = gQuickFilterMode)) and (Key in [$21..$7E])) then
 {$IFDEF MSWINDOWS}
   // Don't execute hotkeys with AltGr on Windows.
   if not (GetKeyShiftStateEx = [ssAltGr]) then
