74faadd4cdcc429fbd1fd3f093a69fbc9c3bc2dd
cobines <cobines@users.sourceforge.net>
2010-02-02 02:43:50 +0000

    FIX: Check if application is active instead of checking main form's focus if FileWatcher should not work when application is in the background. This fixes the issue where refreshing file list is not working even when DC is active.

--- src/fmain.pas
+++ src/fmain.pas
@@ -2945,7 +2945,7 @@ var
   FileView: TFileView;
 begin
   // if not active and refresh only in foreground then exit
-  if (not Focused) and (watch_only_foreground in gWatchDirs) then Exit;
+  if (watch_only_foreground in gWatchDirs) and (not Application.Active) then Exit;
   if not (Sender is TFileViewNotebook) then Exit;
 
   FileView := (Sender as TFileViewNotebook).ActiveView;
