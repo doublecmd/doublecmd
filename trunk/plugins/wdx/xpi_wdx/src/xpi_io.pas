e0ce147ac4ac31328a45214ebaa2ee46556584b3
Alexx2000 <Alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2010-04-10 16:25:40 +0000

    ADD: Initial version of xpi_wdx

---
+++ trunk/plugins/wdx/xpi_wdx/src/xpi_io.pas
@@ -0,0 +1,137 @@
+{
+   Double commander
+   -------------------------------------------------------------------------
+   xpi_wdx is destined to obtainin the information from xpi-files
+   in which extensions and themes for Gecko-based applications are distributed.
+
+   Copyright (C) 2010 Koblov Alexander (Alexx2000@mail.ru)
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+unit xpi_io;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, xpi_def;
+
+function ParseInsatallManifest(FileName: PAnsiChar; out InstallManifest: TInstallManifest): Boolean;
+
+implementation
+
+uses
+  Unzip, ZipUtils, DOM, XMLRead;
+
+function ExtractInstallManifest(FileName: PAnsiChar; out MemoryStream: TMemoryStream): Boolean;
+var
+  xpiFile: unzFile = nil;
+  rdfInfo: unz_file_info;
+  iError: LongInt;
+  FileBuffer: Pointer = nil;
+begin
+  Result:= False;
+  xpiFile:= unzOpen(FileName);
+  if Assigned(xpiFile) then
+  try
+    iError:= unzLocateFile(xpiFile, 'install.rdf', 0);
+    if iError <> UNZ_OK then Exit;
+    iError:= unzGetCurrentFileInfo(xpiFile, @rdfInfo, nil, 0, nil, 0, nil, 0);
+    if iError <> UNZ_OK then Exit;
+    FileBuffer:= AllocMem(rdfInfo.uncompressed_size);
+    if FileBuffer = nil then Exit;
+    iError := unzOpenCurrentFile(xpiFile);
+    if iError <> UNZ_OK then Exit;
+    iError := unzReadCurrentFile(xpiFile, FileBuffer, rdfInfo.uncompressed_size);
+    if iError < 0 then Exit;
+    if MemoryStream.Write(FileBuffer^, iError) = iError then
+       Result:= True;
+    iError := unzCloseCurrentFile(xpiFile);
+  finally
+    iError := unzClose(xpiFile);
+    if Assigned(FileBuffer) then
+      FreeMem(FileBuffer);
+  end;
+end;
+
+function AddString(TargetStr, SourceStr: AnsiString): AnsiString;
+begin
+  if Length(TargetStr) = 0 then
+    Result:= SourceStr
+  else
+    Result:= TargetStr + ', ' + SourceStr;
+end;
+
+function ParseInsatallManifest(FileName: PAnsiChar; out InstallManifest: TInstallManifest): Boolean;
+var
+  I: LongWord;
+  MemoryStream: TMemoryStream = nil;
+  Doc: TXMLDocument = nil;
+  mainNode: TDOMNode = nil;
+  childNode: TDOMNode = nil;
+begin
+  MemoryStream:= TMemoryStream.Create;
+  try
+    if ExtractInstallManifest(FileName, MemoryStream) then;
+    begin
+      MemoryStream.Position:= 0;
+      Doc := TXMLDocument.Create;
+      ReadXMLFile(Doc, MemoryStream);
+       if Assigned(Doc.documentElement) then
+         mainNode:= Doc.documentElement.FindNode('Description');
+       if not Assigned(mainNode) then
+         mainNode:= Doc.documentElement.FindNode('RDF:Description');
+       if Assigned(mainNode) then
+       begin
+         for I:= 0 to mainNode.ChildNodes.Count - 1 do
+         begin
+           childNode := mainNode.ChildNodes.Item[I];
+           if Assigned(childNode) then
+           with childNode do
+           begin
+             if nodeName = 'em:id' then
+               InstallManifest.ID:= FirstChild.NodeValue
+             else if nodeName = 'em:name' then
+               InstallManifest.Name:= FirstChild.NodeValue
+             else if nodeName = 'em:version' then
+               InstallManifest.Version:= FirstChild.NodeValue
+             else if nodeName = 'em:description' then
+               InstallManifest.Description:= FirstChild.NodeValue
+             else if nodeName = 'em:creator' then
+               InstallManifest.Creator:= FirstChild.NodeValue
+             else if nodeName = 'em:contributor' then
+               InstallManifest.Contributor:= AddString(InstallManifest.Contributor, FirstChild.NodeValue)
+             else if nodeName = 'em:developer' then
+               InstallManifest.Developer:= AddString(InstallManifest.Developer, FirstChild.NodeValue)
+             else if nodeName = 'em:translator' then
+               InstallManifest.Translator:= AddString(InstallManifest.Translator, FirstChild.NodeValue)
+             else if nodeName = 'em:homepageURL' then
+               InstallManifest.HomePageURL:= FirstChild.NodeValue
+             else if nodeName = 'em:updateURL' then
+               InstallManifest.UpdateURL:= FirstChild.NodeValue
+             else if nodeName = 'em:targetPlatform' then
+               InstallManifest.TargetPlatform:= AddString(InstallManifest.TargetPlatform, FirstChild.NodeValue)
+           end;
+         end;
+       end;
+    end;
+  finally
+    FreeAndNil(MemoryStream);
+  end;
+end;
+
+end.
+
---
+++ trunk/plugins/wdx/xpi_wdx/src/xpi_wdx_intf.pas
@@ -0,0 +1,153 @@
+{
+   Double commander
+   -------------------------------------------------------------------------
+   xpi_wdx is destined to obtainin the information from xpi-files
+   in which extensions and themes for Gecko-based applications are distributed.
+
+   Copyright (C) 2010 Koblov Alexander (Alexx2000@mail.ru)
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+unit xpi_wdx_intf;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, WdxPlugin;
+
+procedure ContentGetDetectString(DetectString: PAnsiChar; MaxLen: Integer); stdcall;
+function ContentGetSupportedField(FieldIndex: Integer; FieldName: PAnsiChar;
+                                  Units: PAnsiChar; MaxLen: Integer): Integer; stdcall;
+function ContentGetValue(FileName: PAnsiChar; FieldIndex, UnitIndex: Integer;
+                         FieldValue: PByte; MaxLen, Flags: Integer): Integer; stdcall;
+
+implementation
+
+uses
+  xpi_def, xpi_io;
+
+const
+  IDX_ID             = 0;
+  IDX_NAME           = 1;
+  IDX_VERSION        = 2;
+  IDX_DESCRIPTION    = 3;
+  IDX_CREATOR        = 4;
+  IDX_DEVELOPER      = 5;
+  IDX_CONTRIBUTOR    = 6;
+  IDX_TRANSLATOR     = 7;
+  IDX_HOMEPAGEURL    = 8;
+  IDX_UPDATEURL      = 9;
+  IDX_PLATFORM       = 10;
+
+  FIELD_COUNT        = 11;
+
+var
+  CurrentFileName: AnsiString;
+  InstallManifest: TInstallManifest;       //cache
+
+function EnsureLength(S: AnsiString; MaxLen: Integer): AnsiString;
+begin
+  Result := S;
+  if Length(Result) >= MaxLen then
+    begin
+      Result := Copy(Result, 1, MaxLen - 4);
+      Result := Result + '...';
+    end;
+end;
+
+procedure ContentGetDetectString(DetectString: PAnsiChar; MaxLen: Integer); stdcall;
+begin
+  StrPLCopy(DetectString, 'EXT="XPI"', MaxLen)
+end;
+
+function ContentGetSupportedField(FieldIndex: Integer; FieldName: PAnsiChar;
+  Units: PAnsiChar; MaxLen: Integer): Integer; stdcall;
+var
+  Field: AnsiString;
+begin
+  StrPCopy(Units, EmptyStr);
+
+  if FieldIndex >= FIELD_COUNT then
+    begin
+      Result := FT_NOMOREFIELDS;
+      Exit;
+    end;
+
+  Result := FT_STRING;
+  case FieldIndex of
+      IDX_ID:          Field := 'ID';
+      IDX_NAME:        Field := 'Name';
+      IDX_VERSION:     Field := 'Version';
+      IDX_DESCRIPTION: Field := 'Description';
+      IDX_CREATOR:     Field := 'Creator';
+      IDX_DEVELOPER:   Field := 'Developer(s)';
+      IDX_CONTRIBUTOR: Field := 'Contributor(s)';
+      IDX_TRANSLATOR:  Field := 'Translator(s)';
+      IDX_HOMEPAGEURL: Field := 'Home page';
+      IDX_UPDATEURL:   Field := 'Update URL';
+      IDX_PLATFORM:    Field := 'Platform(s)';
+  end;
+
+  StrPLCopy(FieldName, Field, MaxLen);
+end;
+
+function ContentGetValue(FileName: PAnsiChar; FieldIndex, UnitIndex: Integer;
+  FieldValue: PByte; MaxLen, Flags: Integer): Integer; stdcall;
+var
+  Value : AnsiString;
+begin
+  Result := FT_FILEERROR;
+  if not FileExists(FileName) then Exit;
+
+  if CurrentFileName <> FileName then
+  begin
+     if not ParseInsatallManifest(FileName, InstallManifest) then Exit;
+     CurrentFileName := FileName;
+  end;
+
+  if (FieldIndex >= FIELD_COUNT) then
+   begin
+      Result := FT_NOSUCHFIELD;
+      Exit;
+   end;
+
+  Result := FT_STRING;
+    case FieldIndex of
+        IDX_ID:          Value := InstallManifest.ID;
+        IDX_NAME:        Value := InstallManifest.Name;
+        IDX_VERSION:     Value := InstallManifest.Version;
+        IDX_DESCRIPTION: Value := InstallManifest.Description;
+        IDX_CREATOR:     Value := InstallManifest.Creator;
+        IDX_DEVELOPER:   Value := InstallManifest.Developer;
+        IDX_CONTRIBUTOR: Value := InstallManifest.Contributor;
+        IDX_TRANSLATOR:  Value := InstallManifest.Translator;
+        IDX_HOMEPAGEURL: Value := InstallManifest.HomePageURL;
+        IDX_UPDATEURL:   Value := InstallManifest.UpdateURL;
+        IDX_PLATFORM:    Value := InstallManifest.TargetPlatform;
+    else
+        Result := FT_FIELDEMPTY;
+        Exit;
+    end;
+
+  StrPLCopy(PAnsiChar(FieldValue), EnsureLength(Value, MaxLen), MaxLen);
+end;
+
+initialization
+  CurrentFileName := EmptyStr;
+
+end.
+
