f7d3b6bff02708dbc4a05ae40cf82efe420582b9
Alexander Koblov <alexx2000@mail.ru>
2010-05-15 20:02:26 +0000

    FIX: Encoding in Zip archives

--- plugins/wcx/zip/fparchive/osconvencoding.pas
+++ plugins/wcx/zip/fparchive/osconvencoding.pas
@@ -89,7 +89,7 @@ begin
   Result:= Source;
   if GetSystemEncoding(Language, Encoding) then
     begin
-      if SameText(Language, 'ru') then
+      if SameText(Language, 'ru') or SameText(Language, 'uk') then
         if IsInitIconv then
           begin
             Iconvert(Source, Result, 'CP866', Encoding);
@@ -116,7 +116,7 @@ begin
   Result:= Source;
   if GetSystemEncoding(Language, Encoding) then
     begin
-      if SameText(Language, 'ru') then
+      if SameText(Language, 'ru') or SameText(Language, 'uk') then
         if IsInitIconv then
           begin
             Iconvert(Source, Result, Encoding, 'CP866');
