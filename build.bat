4976c9b6b6ad9c0e943349138b9b8676c921e3a0
Alexander Koblov <alexx2000@mail.ru>
2010-05-30 12:15:17 +0000

    FIX: Build scripts

--- build.bat
+++ build.bat
@@ -4,9 +4,8 @@ set lazpath=D:\Alexx\Prog\FreePascal\Lazarus
 rem You can execute this script with different parameters:
 rem components - compiling components needed for DC
 rem plugins - compiling all DC plugins
-rem doublecmd - compiling only DC without *.lrs files generation
-rem all - compiling components, plugins and DC with *.lrs files generation
-rem default - compiling DC with *.lrs files generation (using by default)
+rem all - compiling components, plugins and DC
+rem default - compiling DC only (using by default)
 
 if not "%OS_TARGET%" == "" (
   set DC_ARCH=%DC_ARCH% --os=%OS_TARGET%
@@ -17,7 +16,6 @@ if not "%CPU_TARGET%" == "" (
 
 if "%1"=="components" components\build.bat
 if "%1"=="plugins" plugins\build.bat
-if "%1"=="doublecmd" goto doublecmd
 if "%1"=="all" goto all
 goto default
 
@@ -26,9 +24,6 @@ call components\build.bat
 call plugins\build.bat
 
 :default
-for %%f in (src\*.lfm) do %lazpath%\Tools\lazres %%~nf.lrs %%f
-
-:doublecmd
 %lazpath%\lazbuild src\doublecmd.lpi %DC_ARCH%
 
 strip --strip-all doublecmd.exe
--- build.sh
+++ build.sh
@@ -1,20 +1,16 @@
 #!/bin/bash 
 
-# if you compile first time you must have "lazres" tools
-# to create it execute "$lazpath/lazbuild $lazpath/tools/lazres.lpi"
-# and change variable "lazpath" and "lcl"
+# if you compile first time you must change variable "lazpath" and "lcl"
 # after it execute this script with parameter "all" at doublecmd dir 
-# "./_make.sh all" it create lrs file and build doublecmd  
-# and you can comment last line and uncomment next to last to use lazbuild
+# "./build.sh all" it build doublecmd
 #                                                 by Attid
 
 
 # You can execute this script with different parameters:
 # components - compiling components needed for DC
 # plugins - compiling all DC plugins
-# doublecmd - compiling only DC without *.lrs files generation
-# all - compiling components, plugins and DC with *.lrs files generation
-# default - compiling DC with *.lrs files generation (using by default)
+# all - compiling components, plugins and DC
+# default - compiling DC only (using by default)
 
 # path to lazarus
 export lazpath=/home/alexx/bin/lazarus
@@ -29,21 +25,11 @@ if [ $CPU_TARGET ]
   then export DC_ARCH=$(echo "$DC_ARCH --cpu=$CPU_TARGET")
 fi
 
-build_doublecmd()
+build_default()
 {
   $lazpath/lazbuild src/doublecmd.lpi $DC_ARCH
   
   strip --strip-all doublecmd
-}
-
-build_default()
-{
-  for LFM in `ls ./src/*.lfm`
-  do
-    LRS=`echo $LFM | sed -e 's/\.lfm$/\.lrs/'`
-    $lazpath/tools/lazres $LRS $LFM
-  done
-  build_doublecmd
 }
 
 build_all()
@@ -51,14 +37,12 @@ build_all()
   components/build.sh
   plugins/build.sh
   build_default
-  build_doublecmd
 }
 
 
 case $1 in
   components)  components/build.sh;;
      plugins)  plugins/build.sh;;
-   doublecmd)  build_doublecmd;;
          all)  build_all;;
            *)  build_default;;
 esac
