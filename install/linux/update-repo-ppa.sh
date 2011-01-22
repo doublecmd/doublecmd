3b51c1c16fb82cfbd6235ded160ff8d929373c33
Alexander Koblov <alexx2000@mail.ru>
2011-01-22 11:30:38 +0000

    UPD: PPA upload script

--- install/linux/update-repo-ppa.sh
+++ install/linux/update-repo-ppa.sh
@@ -9,34 +9,78 @@ DC_VER=0.4.6
 DC_TEMP_DIR=/var/tmp/doublecmd-$(date +%y.%m.%d)
 # Directory for DC source code
 DC_SOURCE_DIR=$DC_TEMP_DIR/doublecmd-$DC_VER
+# Directory for DC help
+DC_HELP_DIR=$DC_TEMP_DIR/doublecmd-help-$DC_VER
 # DC revision number
 DC_REVISION=$(svnversion -n ../../)
 
-# Export from SVN
+# Recreate temp directory
 rm -rf $DC_TEMP_DIR
 mkdir -p $DC_TEMP_DIR
-svn export ../../ $DC_SOURCE_DIR
 
-# Save revision number
-mkdir $DC_SOURCE_DIR/.svn
-cp -a ../../.svn/entries $DC_SOURCE_DIR/.svn/
+update_doublecmd()
+{
+  # Export from SVN
+  svn export ../../ $DC_SOURCE_DIR
 
-# Remove help files
-rm -rf $DC_SOURCE_DIR/doc/en
-rm -rf $DC_SOURCE_DIR/doc/ru
+  # Save revision number
+  mkdir $DC_SOURCE_DIR/.svn
+  cp -a ../../.svn/entries $DC_SOURCE_DIR/.svn/
 
-# Prepare debian directory
-mkdir -p $DC_SOURCE_DIR/debian
-cp -r $DC_SOURCE_DIR/install/linux/deb/* $DC_SOURCE_DIR/debian
+  # Remove help files
+  rm -rf $DC_SOURCE_DIR/doc/en
+  rm -rf $DC_SOURCE_DIR/doc/ru
 
-# Update changelog file
-pushd $DC_SOURCE_DIR/debian
-dch -m -v $DC_VER-0~svn$DC_REVISION "Update to revision $DC_REVISION"
-popd
+  # Prepare debian directory
+  mkdir -p $DC_SOURCE_DIR/debian
+  cp -r $DC_SOURCE_DIR/install/linux/deb/doublecmd/* $DC_SOURCE_DIR/debian
 
-# Create archive with source code and upload it to PPA
-cd $DC_SOURCE_DIR
-debuild -S -sa
+  # Update changelog file
+  pushd $DC_SOURCE_DIR/debian
+  dch -m -v $DC_VER-$DC_REVISION "Update to revision $DC_REVISION"
+  popd
+
+  # Create archive with source code and upload it to PPA
+  pushd $DC_SOURCE_DIR
+  debuild -S -sa
+  popd
+}
+
+update_doublecmd_help()
+{
+  # Export from SVN
+  svn export ../../doc $DC_HELP_DIR
+
+  # Remove text files
+  rm -f $DC_HELP_DIR/*.txt
+
+  # Prepare debian directory
+  svn export deb/doublecmd-help $DC_HELP_DIR/debian
+
+  # Update changelog file
+  pushd $DC_HELP_DIR/debian
+  dch -m -v $DC_VER-$DC_REVISION "Update to revision $DC_REVISION"
+  popd
+
+  # Create archive with source code
+  pushd $DC_HELP_DIR
+  debuild -S -sa
+  popd
+}
+
+update_all()
+{
+  update_doublecmd
+  update_doublecmd_help
+}
+
+case $1 in
+  doublecmd-help)  update_doublecmd_help;;
+       doublecmd)  update_doublecmd;;
+               *)  update_all;;
+esac
+
+# Upload archives to PPA
 cd $DC_TEMP_DIR
 dput ppa:alexx2000/doublecmd $(find -name '*.changes')
 
