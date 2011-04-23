8ecf7c3d0506817225821aa76282c6d1dcc30a37
Alexander Koblov <alexx2000@mail.ru>
2011-04-23 15:26:36 +0000

    ADD: Read mime actions also from mimeapps.list (Feature Request [3139263])

--- src/platform/unix/umimeactions.pas
+++ src/platform/unix/umimeactions.pas
@@ -3,10 +3,12 @@
     -------------------------------------------------------------------------
     Handles actions associated with MIME types in .desktop files.
 
-    Based on FreeDesktop.org specification
+    Based on FreeDesktop.org specifications
     (http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html)
+    (http://www.freedesktop.org/wiki/Specifications/mime-actions-spec)
 
     Copyright (C) 2009-2010  Przemyslaw Nagay (cobines@gmail.com)
+    Copyright (C) 2011  Koblov Alexander (Alexx2000@mail.ru)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -66,7 +68,7 @@ function GetFileMimeType(const FileName: UTF8String): UTF8String;
 implementation
 
 uses
-  uDCUtils, uIconTheme, uClipboard;
+  uClassesEx, uDCUtils, uIconTheme, uClipboard, uOSUtils;
 
 type
   TCDesktopFileEntry = record
@@ -84,7 +86,6 @@ const
 procedure mime_type_init; cdecl; external libmime;
 procedure mime_type_finalize; cdecl; external libmime;
 function mime_type_get_by_filename(filename: PChar; stat: Pointer) : PChar; cdecl; external libmime;
-function mime_type_get_default_action(mimeType: PChar) : PChar; cdecl; external libmime;
 function mime_type_get_actions(mimeType: PChar): PPChar; cdecl; external libmime;
 function mime_type_locate_desktop_file(DirectoryToCheck: PChar; DesktopFileId: PChar): PChar; cdecl; external libmime;
 function mime_get_desktop_entry(DesktopFileName: PChar): TCDesktopFileEntry; cdecl; external libmime;
@@ -202,6 +203,57 @@ begin
   end;
 end;
 
+procedure ReadMimeAppsList(const mimeType: String; out Added, Removed: TStringList);
+const
+  mimeApps1 = '.local/share/applications/mimeapps.list';
+  mimeApps2 = '/usr/share/applications/mimeapps.list';
+var
+  I: LongInt;
+  sTemp: String;
+  mimeApps: TIniFileEx = nil;
+  mimeAppsList: array[1..2] of String = (mimeApps1, mimeApps2);
+
+  function ParseActions(const Actions: String; out ActionList: TStringList): Boolean;
+  var
+    startIndex,
+    finishIndex: LongInt;
+    action: String;
+    desktopFile: PChar = nil;
+  begin
+    startIndex:= 1;
+    for finishIndex:= 1 to Length(Actions) do
+    if (Actions[finishIndex] = ';') then
+    begin
+      action:= Copy(Actions, startIndex, finishIndex - startIndex);
+      desktopFile := mime_type_locate_desktop_file(nil, PChar(action));
+      if (desktopFile <> nil) then
+      begin
+        if (ActionList.IndexOf(action) < 0) then
+          ActionList.Add(action);
+        g_free(desktopFile);
+      end;
+      startIndex:= finishIndex + 1;
+    end;
+  end;
+
+begin
+  Added:= TStringList.Create;
+  Removed:= TStringList.Create;
+  mimeAppsList[1]:= GetHomeDir + mimeAppsList[1];
+  for I:= Low(mimeAppsList) to High(mimeAppsList) do
+  if (mbFileExists(mimeAppsList[I])) then
+  try
+    mimeApps:= TIniFileEx.Create(mimeAppsList[I], fmOpenRead or fmShareDenyNone);
+    sTemp:= mimeApps.ReadString('Added Associations', mimeType, EmptyStr);
+    if (Length(sTemp) <> 0) then ParseActions(sTemp, Added);
+    sTemp:= mimeApps.ReadString('Removed Associations', mimeType, EmptyStr);
+    if (Length(sTemp) <> 0) then ParseActions(sTemp, Removed);
+  finally
+    if Assigned(mimeApps) then
+      FreeAndNil(mimeApps);
+  end;
+end;
+
 function GetDesktopEntries(FileNames: TStringList): TList;
 var
   mimeType: PChar;
@@ -210,24 +262,11 @@ var
   i: Integer;
   app: TCDesktopFileEntry;
   Entry: PDesktopFileEntry;
-begin
-  if FileNames.Count = 0 then
-    Exit(nil);
+  Added, Removed: TStringList;
 
-  Result := TList.Create;
-
-  // This string should not be freed.
-  mimeType := mime_type_get_by_filename(PChar(FileNames[0]), nil);
-
-  actions := mime_type_get_actions(mimeType); // retrieve *.desktop identificators
-
-  if actions = nil then
-    Exit;  // cannot find any actions for this mime
-
-  i := 0;
-  while (actions[i] <> nil) and (actions[i] <> '') do
+  procedure AddAction(action: PChar);
   begin
-    desktopFile := mime_type_locate_desktop_file(nil, actions[i]);
+    desktopFile := mime_type_locate_desktop_file(nil, action);
     app := mime_get_desktop_entry(desktopFile);
 
     New(Entry);
@@ -258,11 +297,46 @@ begin
     g_free(app.Comment);
     g_free(app.Exec);
     g_free(app.IconName);
-
-    i := i + 1;
   end;
 
-  g_strfreev(actions);
+begin
+  if FileNames.Count = 0 then
+    Exit(nil);
+
+  Result := TList.Create;
+
+  // This string should not be freed.
+  mimeType := mime_type_get_by_filename(PChar(FileNames[0]), nil);
+
+  // Retrieve *.desktop identificators
+  actions := mime_type_get_actions(mimeType);
+
+  // Read actions from mimeapps.list
+  ReadMimeAppsList(mimeType, Added, Removed);
+
+  // Add actions from mimeapps.list
+  for i := 0 to Added.Count - 1 do
+    AddAction(PChar(Added[i]));
+
+  // If find any actions for this mime
+  if actions <> nil then
+  begin
+    i := 0;
+    while (actions[i] <> nil) and (actions[i] <> '') do
+    begin
+      // Don't add actions where already in mimeapps.list
+      if (Added.IndexOf(actions[i]) < 0) and (Removed.IndexOf(actions[i]) < 0) then
+        AddAction(actions[i]);
+
+      i := i + 1;
+    end;
+  end;
+
+  // Free resources
+  FreeAndNil(Added);
+  FreeAndNil(Removed);
+  if (actions <> nil) then
+    g_strfreev(actions);
 end;
 
 function GetDefaultAppCmd(FileNames: TStringList): UTF8String;
@@ -274,6 +348,7 @@ var
   desktopFile: PChar;
   app: TCDesktopFileEntry;
   Entry: TDesktopFileEntry;
+  Added, Removed: TStringList;
 begin
   Result:= EmptyStr;
 
@@ -282,21 +357,28 @@ begin
   // This string should not be freed.
   mimeType := mime_type_get_by_filename(PChar(FileNames[0]), nil);
 
-  // Get default action for this mime type
-  action := mime_type_get_default_action(mimeType);
+  // Read actions from mimeapps.list
+  ReadMimeAppsList(mimeType, Added, Removed);
 
-  // If default action is not found then use first valid action
-  if (action = nil) then
-  begin
-    actions := mime_type_get_actions(mimeType); // retrieve *.desktop identificators
+  if (Added.Count > 0) then
+    begin
+      // First action is default
+      action:= PChar(Added[0]);
+    end
+  else
+    begin
+      // Retrieve *.desktop identificators
+      actions := mime_type_get_actions(mimeType);
 
-    if actions = nil then Exit;  // cannot find any actions for this mime
-
-    repeat
-      action := actions[i];
-      inc(i);
-    until (action <> nil) or (action <> EmptyStr);
-  end;
+      // If find any actions for this mime
+      if (actions <> nil) then
+      repeat
+        action := actions[i];
+        inc(i);
+        // If action is removed by mimeapps.list then get next
+        if (Removed.IndexOf(action) >= 0) then Continue;
+      until (action <> nil) or (action <> EmptyStr);
+    end;
 
   if (action <> nil) then
   begin
@@ -321,10 +403,11 @@ begin
     g_free(app.IconName);
   end;
 
+  // Free resources
+  FreeAndNil(Added);
+  FreeAndNil(Removed);
   if (actions <> nil) then
     g_strfreev(actions)
-  else if (action <> nil) then
-    g_free(action);
 end;
 
 function GetFileMimeType(const FileName: UTF8String): UTF8String;
