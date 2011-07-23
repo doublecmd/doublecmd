cc9a0438b0ef7761940887bf6e246193ab57ae23
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2011-07-23 09:00:18 +0000

    ADD: Start to implement samba.wfx plugin based on libsmbclient library

---
+++ trunk/plugins/wfx/samba/src/samba.lpr
@@ -0,0 +1,17 @@
+library samba;
+
+{$mode objfpc}{$H+}
+
+uses
+  Classes, SmbFunc
+  { you can add units after this };
+
+exports
+  FsInit,
+  FsFindFirst,
+  FsFindNext,
+  FsFindClose;
+
+begin
+end.
+
---
+++ trunk/plugins/wfx/samba/src/smbfunc.pas
@@ -0,0 +1,201 @@
+unit SmbFunc;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, WfxPlugin;
+
+function FsInit(PluginNr: Integer; pProgressProc: TProgressProc; pLogProc: TLogProc; pRequestProc: TRequestProc): Integer; stdcall;
+
+function FsFindFirst(Path: PAnsiChar; var FindData: TWin32FindData): THandle; stdcall;
+function FsFindNext(Hdl: THandle; var FindData: TWin32FindData): BOOL; stdcall;
+function FsFindClose(Hdl: THandle): Integer; stdcall;
+
+implementation
+
+uses
+  Unix, BaseUnix, UnixType, StrUtils, libsmbclient;
+
+type
+  PSambaHandle = ^TSambaHandle;
+  TSambaHandle = record
+    Path: String;
+    Handle: LongInt;
+  end;
+
+var
+  ProgressProc: TProgressProc;
+  LogProc: TLogProc;
+  RequestProc: TRequestProc;
+  PluginNumber: Integer;
+  Abort: Boolean = False;
+  NeedAuth: Boolean = False;
+  UserName: array[0..MAX_PATH-1] of AnsiChar;
+  Password: array[0..MAX_PATH-1] of AnsiChar;
+
+function FileTimeToUnixTime(ft: TFileTime): time_t;
+var
+  UnixTime: Int64;
+begin
+  UnixTime:= ft.dwHighDateTime;
+  UnixTime:= (UnixTime shl 32) or ft.dwLowDateTime;
+  UnixTime:= (UnixTime - 116444736000000000) div 10000000;
+  Result:= time_t(UnixTime);
+end;
+
+function UnixTimeToFileTime(mtime: time_t): TFileTime;
+var
+  FileTime: Int64;
+begin
+  FileTime:= Int64(mtime) * 10000000 + 116444736000000000;
+  Result.dwLowDateTime:= (FileTime and $FFFF);
+  Result.dwHighDateTime:= (FileTime shr $20);
+end;
+
+procedure WriteError(const FuncName: String);
+begin
+  WriteLn(FuncName + ': ', SysErrorMessage(GetLastOSError));
+end;
+
+procedure smbc_get_auth_data(server, share: PAnsiChar;
+                                  wg: PAnsiChar; wglen: LongInt;
+                                  un: PAnsiChar; unlen: LongInt;
+                                  pw: PAnsiChar; pwlen: LongInt); cdecl;
+begin
+  if NeedAuth then
+  begin
+    Abort:= True;
+
+    // Query user name
+    if RequestProc(PluginNumber, RT_UserName, nil, nil, un, unlen) then
+    begin
+      Abort:= False;
+      // Save user name
+      StrLCopy(UserName, un, unlen);
+    end;
+
+    if Abort then Exit;
+
+    // Query password
+    if RequestProc(PluginNumber, RT_Password, nil, nil, pw, pwlen) then
+    begin
+      Abort:= False;
+      // Save password
+      StrLCopy(Password, pw, pwlen);
+    end;
+  end
+  else
+    begin
+      // If has saved user name then use it
+      if StrLen(UserName) <> 0 then
+        StrLCopy(un, UserName, unlen);
+      // If has saved password then use it
+      if StrLen(Password) <> 0 then
+        StrLCopy(pw, Password, pwlen);
+    end;
+end;
+
+function BuildNetworkPath(const Path: String): String;
+var
+  I, C: Integer;
+begin
+  C:= 0;
+  if Path = PathDelim then Exit('smb://');
+  Result := ExcludeTrailingPathDelimiter(Path);
+  for I := 1 to Length(Result) do
+  begin
+    if (Result[I] = PathDelim) then
+      Inc(C);
+  end;
+  if (C < 2) then
+    Result:= 'smb:/' + Result + PathDelim
+  else
+    begin
+      I:= PosEx(PathDelim, Result, 2);
+      Result:= 'smb:/' + Copy(Result, I, MaxInt) + PathDelim;
+    end;
+end;
+
+function FsInit(PluginNr: Integer; pProgressProc: tProgressProc; pLogProc: tLogProc; pRequestProc: tRequestProc): Integer; stdcall;
+begin
+  ProgressProc := pProgressProc;
+  LogProc := pLogProc;
+  RequestProc := pRequestProc;
+  PluginNumber := PluginNr;
+  FillChar(UserName, SizeOf(UserName), #0);
+  FillChar(Password, SizeOf(Password), #0);
+
+  Result := smbc_init(@smbc_get_auth_data, 0);
+  if Result < 0 then WriteError('smbc_init');
+end;
+
+function FsFindFirst(Path: PAnsiChar; var FindData: TWin32FindData): THandle; stdcall;
+var
+  NetworkPath: String;
+  SambaHandle: PSambaHandle;
+  Handle: LongInt;
+  r: Boolean;
+  h:LongInt;
+begin
+  Abort:= False;
+  NetworkPath:= BuildNetworkPath(Path);
+  repeat
+    Handle:= smbc_opendir(PChar(NetworkPath));
+    NeedAuth:= (Handle = -1);
+  until not NeedAuth or Abort;
+  if Handle < 0 then
+    begin
+      WriteError('smbc_opendir');
+      Result:= wfxInvalidHandle;
+    end
+  else
+    begin
+      New(SambaHandle);
+      SambaHandle^.Path:= NetworkPath;
+      SambaHandle^.Handle:= Handle;
+      Result:= THandle(SambaHandle);
+      FsFindNext(Result, FindData);
+    end;
+end;
+
+function FsFindNext(Hdl: THandle; var FindData: TWin32FindData): BOOL; stdcall;
+var
+  dirent: psmbc_dirent;
+  FileInfo: BaseUnix.Stat;
+  SambaHandle: PSambaHandle absolute Hdl;
+begin
+  Result:= True;
+  dirent := smbc_readdir(SambaHandle^.Handle);
+  if (dirent = nil) then Exit(False);
+  FillByte(FindData, SizeOf(TWin32FindData), 0);
+  StrLCopy(FindData.cFileName, dirent^.name, dirent^.namelen);
+  if dirent^.smbc_type in [SMBC_WORKGROUP, SMBC_SERVER, SMBC_FILE_SHARE] then
+    FindData.dwFileAttributes:= FILE_ATTRIBUTE_DIRECTORY;
+  if dirent^.smbc_type in [SMBC_DIR, SMBC_FILE, SMBC_LINK] then
+    begin
+      if smbc_stat(PChar(SambaHandle^.Path + FindData.cFileName), @FileInfo) = 0 then
+      begin
+        FindData.dwFileAttributes:= FILE_ATTRIBUTE_UNIX_MODE;
+        FindData.dwReserved0:= FileInfo.st_mode;
+        FindData.nFileSizeLow := (FileInfo.st_size and $FFFF);
+        FindData.nFileSizeHigh := (FileInfo.st_size shr $20);
+        FindData.ftLastAccessTime:= UnixTimeToFileTime(FileInfo.st_atime);
+        FindData.ftCreationTime:= UnixTimeToFileTime(FileInfo.st_ctime);
+        FindData.ftLastWriteTime:= UnixTimeToFileTime(FileInfo.st_mtime);
+      end;
+  end;
+end;
+
+function FsFindClose(Hdl: THandle): Integer; stdcall;
+var
+  SambaHandle: PSambaHandle absolute Hdl;
+begin
+  Result:= smbc_closedir(SambaHandle^.Handle);
+  if Result < 0 then WriteError('smbc_closedir');
+  Dispose(SambaHandle);
+end;
+
+end.
+
