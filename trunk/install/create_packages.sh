3ad1bfffdf2b716832ec72869665152686075222
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2011-08-07 11:29:24 +0000

    UPD: Version info in install scripts (trunk)

--- trunk/install/create_packages.sh
+++ trunk/install/create_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Set Double Commander version
-DC_VER=0.4.6
+DC_VER=0.5.5
 
 # The new package will be saved here
 PACK_DIR=$(pwd)/linux/release
--- trunk/install/linux/update-repo-ppa.sh
+++ trunk/install/linux/update-repo-ppa.sh
@@ -3,7 +3,7 @@
 # This script updates Double Commander Personal Package Archive (PPA) repository
 
 # Set Double Commander version
-DC_VER=0.4.6
+DC_VER=0.5.5
 
 # Temp directory
 DC_TEMP_DIR=/var/tmp/doublecmd-$(date +%y.%m.%d)
