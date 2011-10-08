623304197380edbad6e146255da74185d4d984dc
Alexander Koblov <alexx2000@mail.ru>
2011-10-08 18:37:14 +0000

    UPD: Make debian source package for each distro at one step

--- install/linux/update-repo-ppa.sh
+++ install/linux/update-repo-ppa.sh
@@ -4,6 +4,8 @@
 
 # Set Double Commander version
 DC_VER=0.5.5
+# Set Ubuntu series
+DISTRO=( lucid maverick natty )
 
 # Temp directory
 DC_TEMP_DIR=/var/tmp/doublecmd-$(date +%y.%m.%d)
@@ -14,14 +16,13 @@ DC_HELP_DIR=$DC_TEMP_DIR/doublecmd-help-$DC_VER
 # DC revision number
 DC_REVISION=$(svnversion -n ../../)
 
-# Set distribution
+# If second parameter is not null then
+# create archive with original source (*.orig.tar.gz)
 if [ -z $2 ]
    then
        export ORIG=1
-       export DIST=lucid
    else
        export ORIG=0
-       export DIST=$2
 fi
 
 # Recreate temp directory
@@ -47,24 +48,28 @@ update_doublecmd()
   tar -cvzf $DC_TEMP_DIR/doublecmd_$DC_VER.orig.tar.gz doublecmd-$DC_VER
   popd
 
-  # Prepare debian directory
-  mkdir -p $DC_SOURCE_DIR/debian
-  cp -r $DC_SOURCE_DIR/install/linux/deb/doublecmd/* $DC_SOURCE_DIR/debian
+  # Create source package for each distro
+  for DIST in "${DISTRO[@]}"
+  do
+    # Prepare debian directory
+    mkdir -p $DC_SOURCE_DIR/debian
+    cp -r $DC_SOURCE_DIR/install/linux/deb/doublecmd/* $DC_SOURCE_DIR/debian
 
-  # Update changelog file
-  pushd $DC_SOURCE_DIR/debian
-  dch -m -D $DIST -v $DC_VER-$DC_REVISION~$DIST "Update to revision $DC_REVISION"
-  popd
+    # Update changelog file
+    pushd $DC_SOURCE_DIR/debian
+    dch -m -D $DIST -v $DC_VER-$DC_REVISION~$DIST "Update to revision $DC_REVISION"
+    popd
 
-  # Create archive with source code
-  pushd $DC_SOURCE_DIR
-  if [ $ORIG = '1' ]
-    then
-        debuild -S -sa
-    else
-        debuild -S -sd
-  fi
-  popd
+    # Create archive with source code
+    pushd $DC_SOURCE_DIR
+    if [ $ORIG = '1' ] && [ $DIST = ${DISTRO[0]} ]
+      then
+          debuild -S -sa
+      else
+          debuild -S -sd
+    fi
+    popd
+  done
 }
 
 update_doublecmd_help()
@@ -83,20 +88,24 @@ update_doublecmd_help()
   # Prepare debian directory
   svn export deb/doublecmd-help $DC_HELP_DIR/debian
 
-  # Update changelog file
-  pushd $DC_HELP_DIR/debian
-  dch -m -v $DC_VER-$DC_REVISION~ppa "Update to revision $DC_REVISION"
-  popd
+  # Create source package for each distro
+  for DIST in "${DISTRO[@]}"
+  do
+    # Update changelog file
+    pushd $DC_HELP_DIR/debian
+    dch -m -v $DC_VER-$DC_REVISION~$DIST "Update to revision $DC_REVISION"
+    popd
 
-  # Create archive with source code
-  pushd $DC_HELP_DIR
-  if [ $ORIG = '1' ]
-    then
-        debuild -S -sa
-    else
-        debuild -S -sd
-  fi
-  popd
+    # Create archive with source code
+    pushd $DC_HELP_DIR
+    if [ $ORIG = '1' ] && [ $DIST = ${DISTRO[0]} ]
+      then
+          debuild -S -sa
+      else
+          debuild -S -sd
+    fi
+    popd
+  done
 }
 
 update_all()
