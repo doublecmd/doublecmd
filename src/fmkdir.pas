22dcd90243d8080d799ea750850c32a8f59b9aa9
cobines <cobines@users.sourceforge.net>
2011-11-08 15:19:25 +0000

    UPD: Updated Make Dir dialog to store new input value first in drop down list, limit drop down list size to 20 items, correct set focus to edit control. Part of issue [0000222], patch from vitaliyg.

--- src/fmkdir.pas
+++ src/fmkdir.pas
@@ -16,12 +16,9 @@ type
     btnOK: TBitBtn;
     btnCancel: TBitBtn;
     lblMakeDir: TLabel;
-    procedure ButtonsClick(Sender: TObject);
+
     procedure FormKeyPress(Sender: TObject; var Key: Char);
-  private
-    { Private declarations }
   public
-    { Public declarations }
    function ShowMkDir(var sPath:string):Boolean;  // 21.05.2009 - перенес в public
   end;
 
@@ -46,44 +43,29 @@ begin
   end;
 end;
 
-procedure TfrmMkDir.ButtonsClick(Sender: TObject);
+Function TfrmMkDir.ShowMkDir(var sPath:string):Boolean; var Index: Integer;
+const
+  MAX_LINES = 20;
 begin
- cbMkDir.SetFocus; // 21.05.2009 - если нажимаем на кнопки мышью, то возвращаем фокус в поле ввода.
-end;
-
-// 21.05.2009 - функция переписана по подобию тотала.
-Function TfrmMkDir.ShowMkDir(var sPath:string):Boolean;
- var i : byte;
-begin
-{
-  Теперь форма создается при запуске программы и уничтожается при закрытии.
-  Нужно для сохранения значений в списке.
-}
-//  with TfrmMkDir.Create(Application) do
- try
+  ActiveControl := cbMkDir;
   if (sPath <> '..') then
     cbMkDir.Text := sPath
   else
-    cbMkDir.Text := '';      // передаем имя из фокуса панели в строку ввода, если не фокус не в '..'
+    cbMkDir.Text := '';
   cbMkDir.SelectAll;
-  Result:= (ShowModal = mrOK);  // отображение окна
-  sPath:=cbMkDir.Text;
-{
-  Добавляем созданный каталог в список.
-  Просматриваем список - если такое имя уже было, то не добавляем.
-  Пока сделал просмотр вручную, наверно должно быть свойство!
-}
-  If Result then                       // если mrOK
+  Result := (ShowModal = mrOK);
+  sPath := cbMkDir.Text;
+  If Result then
    begin
-    i:=0;
-    while (i <= cbMkDir.Items.Count - 1) and (cbMkDir.Items.Strings[i] <> cbMkDir.Text) do
-     Inc(i);                           // то просмотр списка и поиск в нем такого элемента
-    if (i > cbMkDir.Items.Count - 1) then
-     cbMkDir.Items.Add(cbMkDir.Text);  // если такой элемент не найден, то добавляем его.
+     Index := cbMkDir.Items.IndexOf(cbMkDir.Text);
+     if (Index = -1) then
+       cbMkDir.Items.Insert(0, cbMkDir.Text)
+     else
+       cbMkDir.Items.Move(Index, 0);
+
+     if (cbMkDir.Items.Count > MAX_LINES) then
+       cbMkDir.Items.Delete(cbMkDir.Items.Count - 1);
    end;
-  finally
-//    Free;  -- перенес в TActs.cm_Exit
-  end;
 end;
 
-end.
+end.
