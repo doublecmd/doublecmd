800620f31762e9a86ad7f905b1515a65a93fcc72
cobines <cobines@users.sourceforge.net>
2012-03-11 13:18:07 +0000

    UPD: Use dcpcrypt and chsdet as packages.

---
+++ components/chsdet/chsdet.pas
@@ -0,0 +1,19 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit chsdet; 
+
+interface
+
+uses
+    Big5Freq, CharDistribution, chsdIntf, CustomDetector, EUCKRFreq, 
+  EUCSampler, EUCTWFreq, GB2312Freq, JISFreq, JpCntx, MBUnicodeMultiProber, 
+  MultiModelProber, nsCodingStateMachine, nsCore, nsEscCharsetProber, 
+  nsGroupProber, nsHebrewProber, nsLatin1Prober, nsMBCSMultiProber, nsPkg, 
+  nsSBCharSetProber, nsSBCSGroupProber, nsUniversalDetector, 
+  LangBulgarianModel, LangCyrillicModel, LangGreekModel, LangHebrewModel;
+
+implementation
+
+end.
