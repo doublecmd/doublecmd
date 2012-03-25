7b182a661a928755d3f8257acca3a1c4c0ad24df
vitaliyg <vitaliyg@users.sourceforge.net>
2012-03-25 17:50:14 +0000

    UPD: Updated clean.bat to clean all plugins

--- clean.bat
+++ clean.bat
@@ -1,51 +1,62 @@
-rem Clean up output directory
-del /Q units\i386-win32-win32\*.*
-del /Q units\x86_64-win64-win32\*.*
-del /Q src\*.*~
-del /Q src\*.~*
-del /Q doublecmd*.exe
+@echo Clean up output directory
+@del /Q /S units\*.*
+@del /Q src\*.*~
+@del /Q src\*.~*
+@del /Q doublecmd*.exe
 
-rem Remove generated help files
-del /Q doc\en\dev-help\*.*
+@echo Remove generated help files
+@del /Q doc\en\dev-help\*.*
 
-rem Clean up plugins output directories
-del /Q plugins\wcx\cpio\lib\*.*
-del /Q plugins\wcx\rpm\lib\*.*
-del /Q plugins\wcx\deb\lib\*.*
-del /Q plugins\wcx\lzma\lib\*.*
-del /Q plugins\wcx\zip\lib\*.*
-del /Q plugins\wcx\unbz2\lib\*.*
-del /Q plugins\wcx\unrar\lib\*.*
+@echo Clean up plugins output directories
+@del /Q /S plugins\dsx\DSXLocate\lib\*.*
 
-rem Remove backup files
-del /Q plugins\wcx\cpio\src\*.bak
-del /Q plugins\wcx\rpm\src\*.bak
-del /Q plugins\wcx\deb\src\*.bak
-del /Q plugins\wcx\lzma\src\*.bak
-del /Q plugins\wcx\zip\src\*.bak
-del /Q plugins\wcx\unbz2\src\*.bak
-del /Q plugins\wcx\unrar\src\*.bak
+@del /Q /S plugins\wcx\cpio\lib\*.*
+@del /Q /S plugins\wcx\deb\lib\*.*
+@del /Q /S plugins\wcx\lzma\lib\*.*
+@del /Q /S plugins\wcx\rpm\lib\*.*
+@del /Q /S plugins\wcx\unbz2\lib\*.*
+@del /Q /S plugins\wcx\unrar\lib\*.*
+@del /Q /S plugins\wcx\zip\lib\*.*
 
-del /Q plugins\wcx\cpio\src\*.*~
-del /Q plugins\wcx\rpm\src\*.*~
-del /Q plugins\wcx\deb\src\*.*~
-del /Q plugins\wcx\lzma\src\*.*~
-del /Q plugins\wcx\zip\src\*.*~
-del /Q plugins\wcx\unbz2\src\*.*~
-del /Q plugins\wcx\unrar\src\*.*~
+@del /Q /S plugins\wdx\deb_wdx\lib\*.*
+@del /Q /S plugins\wdx\rpm_wdx\lib\*.*
+@del /Q /S plugins\wdx\svn_wdx\lib\*.*
+@del /Q /S plugins\wdx\xpi_wdx\lib\*.*
 
-rem Clean up components output directories
-del /Q components\CmdLine\lib\i386-win32\*.*
-del /Q components\CmdLine\lib\x86_64-win64\*.*
-del /Q components\KASToolBar\lib\i386-win32\*.*
-del /Q components\KASToolBar\lib\x86_64-win64\*.*
-del /Q components\viewer\lib\i386-win32\*.*
-del /Q components\viewer\lib\x86_64-win64\*.*
-del /Q components\chsdet\lib\i386-win32\*.*
-del /Q components\chsdet\lib\x86_64-win64\*.*
-del /Q components\dcpcrypt\lib\i386-win32\*.*
-del /Q components\dcpcrypt\lib\x86_64-win64\*.*
-del /Q components\gifanim\lib\i386-win32\*.*
-del /Q components\gifanim\lib\x86_64-win64\*.*
-del /Q components\ZVDateTimeCtrls\lib\i386-win32\*.*
-del /Q components\ZVDateTimeCtrls\lib\x86_64-win64\*.*
+@del /Q /S plugins\wfx\ftp\lib\*.*
+@del /Q /S plugins\wfx\gvfs\lib\*.*
+@del /Q /S plugins\wfx\Network\lib\*.*
+@del /Q /S plugins\wfx\samba\lib\*.*
+@del /Q /S plugins\wfx\sample\lib\*.*
+
+@del /Q /S plugins\wlx\simplewlx\lib\*.*
+@del /Q /S plugins\wlx\WlxMplayer\lib\*.*
+
+@echo Remove backup files
+@del /Q plugins\wcx\cpio\src\*.bak
+@del /Q plugins\wcx\rpm\src\*.bak
+@del /Q plugins\wcx\deb\src\*.bak
+@del /Q plugins\wcx\lzma\src\*.bak
+@del /Q plugins\wcx\zip\src\*.bak
+@del /Q plugins\wcx\unbz2\src\*.bak
+@del /Q plugins\wcx\unrar\src\*.bak
+
+@del /Q plugins\wcx\cpio\src\*.*~
+@del /Q plugins\wcx\rpm\src\*.*~
+@del /Q plugins\wcx\deb\src\*.*~
+@del /Q plugins\wcx\lzma\src\*.*~
+@del /Q plugins\wcx\zip\src\*.*~
+@del /Q plugins\wcx\unbz2\src\*.*~
+@del /Q plugins\wcx\unrar\src\*.*~
+
+@echo Clean up components output directories
+
+@del /Q /S components\chsdet\lib\*.*
+@del /Q /S components\CmdLine\lib\*.*
+@del /Q /S components\dcpcrypt\lib\*.*
+@del /Q /S components\gifanim\lib\*.*
+@del /Q /S components\KASToolBar\lib\*.*
+@del /Q /S components\viewer\lib\*.*
+@del /Q /S components\ZVDateTimeCtrls\lib\*.*
+
+@echo Done.
