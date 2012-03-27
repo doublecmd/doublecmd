3e5da414e5162b7467958dfb300f5fdf8dafefbf
cobines <cobines@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2012-03-27 19:50:11 +0000

    UPD: Change header of FileOpDlg. The unit has been completely rewritten since the beginning.

--- trunk/src/fFileOpDlg.pas
+++ trunk/src/fFileOpDlg.pas
@@ -1,14 +1,24 @@
 {
-   Seksi Commander
-   ----------------------------
-   Implementing of progress dialog for file operation
+    Double Commander
+    -------------------------------------------------------------------------
+    Window displaying progress for file source operations and queues.
 
-   Licence  : GNU GPL v 2.0
-   Author   : radek.cervinka@centrum.cz
+    Copyright (C) 2008-2011  Koblov Alexander (Alexx2000@mail.ru)
+    Copyright (C) 2012       Przemysław Nagay (cobines@gmail.com)
 
-   contributors:
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-   Copyright (C) 2008-2011  Koblov Alexander (Alexx2000@mail.ru)
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 }
 
 unit fFileOpDlg;
