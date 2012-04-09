23b04af4f22a13b3ab26e64e441df419046c58f3
cobines <cobines@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2012-04-09 12:42:42 +0000

    UPD: Update dcrevision.inc in the build process by directly calling svn2revision.

--- trunk/src/_getsvnrev.cmd
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# get DC revision under Linux
-$1/tools/svn2revisioninc ../ dcrevision.inc --c=dcRevision
-
-rem get DC revision under Windows
-%1\tools\svn2revisioninc ..\ dcrevision.inc --c=dcRevision
-
-echo "This command is need for successful exit code"
