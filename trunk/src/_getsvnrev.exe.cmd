b20992036962cbfeccd43bb1eea98a5c13ff7a9a
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2012-04-14 05:58:34 +0000

    UPD: Revert previous changes (use $(ExeExt) to get HostOS relative script-file name)

--- trunk/src/_getsvnrev.exe.cmd
+++ trunk/src/_getsvnrev.exe.cmd
@@ -1,6 +1,6 @@
 rem @echo off
 
 rem get DC revision under Windows
-%1\tools\svn2revisioninc.exe ..\ dcrevision.inc --c=dcRevision
+%1tools\svn2revisioninc.exe ..\ dcrevision.inc --c=dcRevision
 
 echo "This command is need for successful exit code"
