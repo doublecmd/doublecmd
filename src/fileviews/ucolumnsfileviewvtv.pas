ff27f25ab6e0d6dc7abd5c89900bd454eb81013b
Alexander Koblov <alexx2000@mail.ru>
2012-07-01 10:29:44 +0000

    UPD: Use default color also for clNone

--- src/fileviews/ucolumnsfileviewvtv.pas
+++ src/fileviews/ucolumnsfileviewvtv.pas
@@ -1674,7 +1674,7 @@ var
     // Set text color.
     if ColumnsSet.GetColumnOvercolor(ACol) then
       TextColor := AFile.TextColor;
-    if TextColor = clDefault then
+    if (TextColor = clDefault) or (TextColor = clNone) then
       TextColor := ColumnsSet.GetColumnTextColor(ACol);
 
     if AFile.Selected then
