2d5d3f1157844d591db8e1801501ec00a406b665
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2013-03-23 12:06:50 +0000

    FIX: Bug [0000622] "Disable request to choose default application for opening "exe" files"

--- trunk/src/platform/uOSUtils.pas
+++ trunk/src/platform/uOSUtils.pas
@@ -319,10 +319,17 @@ end;
 
 function ShellExecute(URL: UTF8String): Boolean;
 {$IF DEFINED(MSWINDOWS)}
+var
+  Return: HINST;
+  wsFileName: WideString;
 begin
-  Result:= ExecCmdFork(Format('"%s"', [URL]));
-  if Result = False then
-      Result:= ExecCmdFork('rundll32 shell32.dll OpenAs_RunDLL ' + URL);
+  URL:= NormalizePathDelimiters(URL);
+  wsFileName:= UTF8Decode(QuoteDouble(URL));
+  Return:= ShellExecuteW(0, nil, PWideChar(wsFileName), nil, nil, SW_SHOWNORMAL);
+  if Return = SE_ERR_NOASSOC then
+    Result:= ExecCmdFork('rundll32 shell32.dll OpenAs_RunDLL ' + URL)
+  else
+    Result:= Return > 32;
 end;
 {$ELSEIF DEFINED(DARWIN)}
 var
