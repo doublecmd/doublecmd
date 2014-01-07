824c38462e7c4b36256579d2312d9b5c2bc97211
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2014-01-07 23:53:44 +0000

    ADD: Binary compare initial implementation

--- trunk/components/viewer/viewercontrol.pas
+++ trunk/components/viewer/viewercontrol.pas
@@ -152,7 +152,7 @@ type
   { TViewerControl }
 
   TViewerControl = class(TCustomControl)
-  private
+  protected
     FEncoding:           TViewerEncoding;
     FViewerMode:         TViewerMode;
     FFileName:           UTF8String;
@@ -188,7 +188,7 @@ type
     procedure SetPercent(const AValue: Integer);
     procedure SetBlockBegin(const AValue: PtrInt);
     procedure SetBlockEnd(const AValue: PtrInt);
-    procedure SetPosition(Value: PtrInt);
+    procedure SetPosition(Value: PtrInt); virtual;
     procedure SetHPosition(Value: Integer);
     procedure SetPosition(Value: PtrInt; Force: Boolean); overload;
     procedure SetHPosition(Value: Integer; Force: Boolean); overload;
@@ -270,7 +270,7 @@ type
     procedure OutHex(x, y: Integer; sText: string; StartPos: PtrInt; DataLength: Integer);
     procedure OutBin(x, y: Integer; sText: string; StartPos: PtrInt; DataLength: Integer);
     procedure WriteText;
-    procedure WriteHex;
+    procedure WriteHex; virtual;
     procedure WriteBin;
     function TransformText(const sText: UTF8String; const Xoffset: Integer): UTF8String;
     function TransformHex(var aPosition: PtrInt; aLimit: PtrInt): AnsiString;
