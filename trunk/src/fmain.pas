d9fc57c5f180e598c31b6f9a418105cb4c5a8671
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2014-01-20 15:44:47 +0000

    UPD: Lazarus 1.2 compatibility, remove obsolete defines

--- trunk/src/fmain.pas
+++ trunk/src/fmain.pas
@@ -476,6 +476,7 @@ type
     procedure frmMainShow(Sender: TObject);
     procedure mnuDropClick(Sender: TObject);
     procedure mnuSplitterPercentClick(Sender: TObject);
+    procedure mnuTabMenuExecute(Sender: TObject);
     procedure mnuTabMenuClick(Sender: TObject);
     procedure nbPageAfterMouseDown(Data: PtrInt);
     procedure nbPageMouseDown(Sender: TObject; Button: TMouseButton;
@@ -769,11 +770,7 @@ procedure TfrmMain.FormCreate(Sender: TObject);
     Result.OnCloseTabClicked := @NotebookCloseTabClicked;
     Result.OnMouseDown := @nbPageMouseDown;
     Result.OnMouseUp := @nbPageMouseUp;
-    {$IF DECLARED(lcl_fullversion) and (lcl_fullversion >= 093100) and (lazRevision >= lazRevOnPageChangedRemoved)}
     Result.OnChange := @nbPageChanged;
-    {$ELSE}
-    Result.OnPageChanged := @nbPageChanged;
-    {$ENDIF}
     Result.OnDblClick := @pnlLeftRightDblClick;
   end;
   function GenerateTitle():String;
@@ -1918,6 +1915,11 @@ begin
   end;
 end;
 
+procedure TfrmMain.mnuTabMenuExecute(Sender: TObject);
+begin
+  (Sender as TAction).OnExecute:= @actExecute;
+end;
+
 procedure TfrmMain.mnuTabMenuClick(Sender: TObject);
 var
   Cmd: String;
@@ -1948,6 +1950,10 @@ begin
 
     Commands.Commands.ExecuteCommand(Cmd, NoteBook.Name);
   end;
+  {$IF (lcl_fullversion >= 1020000)}
+  // On click first the OnClick and then the Action.OnExecute is called
+  MenuItem.Action.OnExecute:= @mnuTabMenuExecute;
+  {$ENDIF}
 end;
 
 procedure TfrmMain.nbPageAfterMouseDown(Data: PtrInt);
