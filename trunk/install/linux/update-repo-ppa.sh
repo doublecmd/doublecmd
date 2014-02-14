0f1ec0fca53afb48cbf5e3870ed7851a12ac5473
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2014-02-14 16:58:45 +0000

    UPD: Replace raring by trusty (raring is obsolete now)

--- trunk/install/linux/update-repo-ppa.sh
+++ trunk/install/linux/update-repo-ppa.sh
@@ -5,7 +5,7 @@
 # Set Double Commander version
 DC_VER=0.6.0
 # Set Ubuntu series
-DISTRO=( precise quantal raring saucy )
+DISTRO=( precise quantal saucy trusty )
 
 # Temp directory
 DC_TEMP_DIR=/var/tmp/doublecmd-$(date +%y.%m.%d)
