0c3782965e92252bca6209f82dfebe1999a4169c
Alexander Koblov <alexx2000@mail.ru>
2014-02-16 09:03:12 +0000

    FIX: Ignore invalid "Path[$e]" key when loading .desktop files

--- src/platform/unix/ukeyfile.pas
+++ src/platform/unix/ukeyfile.pas
@@ -42,6 +42,7 @@ const
   DESKTOP_KEY_NAME = 'Name';
   DESKTOP_KEY_NO_DISPLAY = 'NoDisplay';
   DESKTOP_KEY_TERMINAL = 'Terminal';
+  DESKTOP_KEY_KDE_BUG = 'Path[$e]';
 
 type
   TGKeyFile = record end;
@@ -54,6 +55,8 @@ type
   TKeyFile = class(TCustomIniFile)
   private
     FGKeyFile: PGKeyFile;
+  protected
+    function LoadFromFile(const AFileName: String; out AMessage: UTF8String): Boolean; inline;
   public
     constructor Create(const AFileName: String; AEscapeLineFeeds : Boolean = False); override;
     destructor Destroy; override;
@@ -80,6 +83,8 @@ function  g_key_file_new(): PGKeyFile; cdecl; external;
 procedure g_key_file_free(key_file: PGKeyFile); cdecl; external;
 function  g_key_file_load_from_file(key_file: PGKeyFile; const file_name: Pgchar;
                                     flags: TGKeyFileFlags; error: PPGError): gboolean; cdecl; external;
+function  g_key_file_load_from_data(key_file: PGKeyFile; const data: Pgchar; data_length: gsize;
+                                    flags: TGKeyFileFlags; error: PPGError): gboolean; cdecl; external;
 function  g_key_file_has_group(key_file: PGKeyFile; const group_name: Pgchar): gboolean; cdecl; external;
 function  g_key_file_get_string(key_file: PGKeyFile; const group_name: Pgchar;
                                 const key: Pgchar; error: PPGError): Pgchar; cdecl; external;
@@ -90,21 +95,54 @@ function  g_key_file_get_boolean(key_file: PGKeyFile; const group_name: Pgchar;
 
 { TKeyFile }
 
-constructor TKeyFile.Create(const AFileName: String; AEscapeLineFeeds: Boolean);
+function TKeyFile.LoadFromFile(const AFileName: String; out AMessage: UTF8String): Boolean;
 var
-  AMessage: UTF8String;
+  AChar: Pgchar;
+  ALength: gsize;
+  AContents: Pgchar;
   AError: PGError = nil;
-begin
-  FGKeyFile:= g_key_file_new();
-  if not g_key_file_load_from_file(FGKeyFile, Pgchar(AFileName), G_KEY_FILE_NONE, @AError) then
+
+  function FormatMessage: UTF8String;
   begin
     if Assigned(AError) then
     begin
-      AMessage:= StrPas(AError^.message);
+      Result:= StrPas(AError^.message);
       g_error_free(AError);
+      AError:= nil;
     end;
-    raise EFOpenError.CreateFmt(SFOpenErrorEx, [AFileName, AMessage]);
   end;
+
+begin
+  Result:= g_key_file_load_from_file(FGKeyFile, Pgchar(AFileName), G_KEY_FILE_NONE, @AError);
+  if not Result then
+  begin
+    AMessage:= FormatMessage;
+    // KDE menu editor adds invalid "Path[$e]" key. GKeyFile cannot parse
+    // such desktop files. We comment it before parsing to avoid this problem.
+    if Pos(DESKTOP_KEY_KDE_BUG, AMessage) > 0 then
+    begin
+      Result:= g_file_get_contents(Pgchar(AFileName), @AContents, @ALength, @AError);
+      if not Result then
+        AMessage:= FormatMessage
+      else try
+        AChar:= g_strrstr(AContents, DESKTOP_KEY_KDE_BUG);
+        if Assigned(AChar) then AChar^:= '#';
+        Result:= g_key_file_load_from_data(FGKeyFile, AContents, ALength, G_KEY_FILE_NONE, @AError);
+        if not Result then AMessage:= FormatMessage;
+      finally
+        g_free(AContents);
+      end;
+    end;
+  end;
+end;
+
+constructor TKeyFile.Create(const AFileName: String; AEscapeLineFeeds: Boolean);
+var
+  AMessage: UTF8String;
+begin
+  FGKeyFile:= g_key_file_new();
+  if not LoadFromFile(AFileName, AMessage) then
+    raise EFOpenError.CreateFmt(SFOpenErrorEx, [AFileName, AMessage]);
   inherited Create(AFileName, AEscapeLineFeeds);
   CaseSensitive:= True;
 end;
