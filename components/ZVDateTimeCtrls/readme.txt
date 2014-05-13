0d086354a14e3008f6c7c247dc57565e296f91be
Alexander Koblov <alexx2000@mail.ru>
2014-05-13 16:56:23 +0000

    UPD: Split ZVDateTimeCtrls into two packages (design and runtime)

---
+++ components/ZVDateTimeCtrls/readme.txt
@@ -0,0 +1,5 @@
+ZVDateTimeCtrls
+http://wiki.lazarus.freepascal.org/ZVDateTimeControls_Package
+Version 1.4 (05/09/2012)
+
+Some modifications done for Double Commander (see doublecmd.diff).
---
+++ components/ZVDateTimeCtrls/zvdatetimectrlsdsgn.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ZVDateTimeCtrlsDsgn;
+
+interface
+
+uses
+  ZVDateTimePickerPropEdit, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('ZVDateTimeCtrlsDsgn', @Register);
+end.
