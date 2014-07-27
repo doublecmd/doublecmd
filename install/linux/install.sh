4f942bf763c06abfab39b2a8a38fb0eb23881c7d
Alexander Koblov <alexx2000@mail.ru>
2014-07-27 16:19:35 +0000

    ADD: Scripts in debain packages

--- install/linux/install.sh
+++ install/linux/install.sh
@@ -108,6 +108,8 @@ if [ -z $CK_PORTABLE ]
     install -d                $DC_INSTALL_PREFIX/usr/share/doublecmd/doc
     install -m 644 doc/*.txt  $DC_INSTALL_PREFIX/usr/share/doublecmd/doc
     ln -sf ../../share/doublecmd/doc $DC_INSTALL_DIR/doc
+    # Copy scripts
+    cp -r scripts $DC_INSTALL_DIR/
     # Copy languages
     cp -r language $DC_INSTALL_PREFIX/usr/share/doublecmd
     ln -sf ../../share/doublecmd/language $DC_INSTALL_DIR/language
@@ -131,6 +133,7 @@ if [ -z $CK_PORTABLE ]
     # Copy directories
     cp -r language $DC_INSTALL_DIR/
     cp -r pixmaps  $DC_INSTALL_DIR/
+    cp -r scripts  $DC_INSTALL_DIR/
     # Copy libraries
     install -m 644 *.so*    $DC_INSTALL_DIR/
     # Copy DC icon
