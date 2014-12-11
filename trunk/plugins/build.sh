6b42005b3e600c4d51b3417c54bdf0fbe6de22a5
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2014-12-11 19:39:18 +0000

    FIX: Build under OS X

--- trunk/plugins/build.sh
+++ trunk/plugins/build.sh
@@ -10,87 +10,54 @@ set -e
 # CD to plugins directory
 basedir=$(pwd)
 cd plugins
-pluginsdir=$(pwd)
 
 # WCX plugins
 $lazbuild wcx/cpio/src/cpio.lpi $DC_ARCH
+strip wcx/cpio/lib/cpio.wcx
+
 $lazbuild wcx/deb/src/deb.lpi $DC_ARCH
+strip wcx/deb/lib/deb.wcx
+
 $lazbuild wcx/rpm/src/rpm.lpi $DC_ARCH
+strip wcx/rpm/lib/rpm.wcx
+
 $lazbuild wcx/unrar/src/unrar.lpi $DC_ARCH
+strip wcx/unrar/lib/unrar.wcx
+
 $lazbuild wcx/zip/src/Zip.lpi $DC_ARCH
+strip wcx/zip/lib/zip.wcx
 
 # WDX plugins
 $lazbuild wdx/rpm_wdx/src/rpm_wdx.lpi $DC_ARCH
+strip wdx/rpm_wdx/lib/rpm_wdx.wdx
+
 $lazbuild wdx/deb_wdx/src/deb_wdx.lpi $DC_ARCH
+strip wdx/deb_wdx/lib/deb_wdx.wdx
+
 $lazbuild wdx/svn_wdx/src/svn_wdx.lpi $DC_ARCH
+strip wdx/svn_wdx/lib/svn_wdx.wdx
+
 $lazbuild wdx/xpi_wdx/src/xpi_wdx.lpi $DC_ARCH
+strip wdx/xpi_wdx/lib/xpi_wdx.wdx
 
 # WFX plugins
 $lazbuild wfx/ftp/src/ftp.lpi $DC_ARCH
-$lazbuild wfx/samba/src/samba.lpi $DC_ARCH
+strip wfx/ftp/lib/ftp.wfx
 
-# WLX plugins
-$lazbuild wlx/WlxMplayer/src/wlxMplayer.lpi $DC_ARCH
+if [[ $OSTYPE != darwin* ]]; then
+
+  $lazbuild wfx/samba/src/samba.lpi $DC_ARCH
+  strip wfx/samba/lib/samba.wfx
+
+  # WLX plugins
+  $lazbuild wlx/WlxMplayer/src/wlxMplayer.lpi $DC_ARCH
+  strip wlx/WlxMplayer/lib/wlxmplayer.wlx
+
+fi
 
 # DSX plugins
 $lazbuild dsx/DSXLocate/src/DSXLocate.lpi $DC_ARCH
-
-# Strip and rename WCX
-cd wcx/cpio/lib/
-strip --strip-all cpio.wcx
-cd $pluginsdir
-
-cd wcx/deb/lib/
-strip --strip-all deb.wcx
-cd $pluginsdir
-
-cd wcx/rpm/lib/
-strip --strip-all rpm.wcx
-cd $pluginsdir
-
-cd wcx/unrar/lib/
-strip --strip-all unrar.wcx
-cd $pluginsdir
-
-cd wcx/zip/lib/
-strip --strip-all zip.wcx
-cd $pluginsdir
-
-# Strip and rename WDX
-cd wdx/rpm_wdx/lib/
-strip --strip-all rpm_wdx.wdx
-cd $pluginsdir
-
-cd wdx/deb_wdx/lib/
-strip --strip-all deb_wdx.wdx
-cd $pluginsdir
-
-cd wdx/svn_wdx/lib/
-strip --strip-all svn_wdx.wdx
-cd $pluginsdir
-
-cd wdx/xpi_wdx/lib/
-strip --strip-all xpi_wdx.wdx
-cd $pluginsdir
-
-# Strip and rename WFX
-cd wfx/ftp/lib/
-strip --strip-all ftp.wfx
-cd $pluginsdir
-
-cd wfx/samba/lib/
-strip --strip-all samba.wfx
-cd $pluginsdir
-
-# Strip and rename WLX
-cd wlx/WlxMplayer/lib/
-strip --strip-all wlxmplayer.wlx
-cd $pluginsdir
-
-# Strip and rename DSX
-cd dsx/DSXLocate/lib/
-strip --strip-all dsxlocate.dsx
-cd $pluginsdir
+strip dsx/DSXLocate/lib/dsxlocate.dsx
 
 # Return from plugins directory
 cd $basedir
