f754831eb9fd6e7839985a4ccade8fbe8006cbf7
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2015-01-02 15:06:45 +0000

    UPD: Don't strip plugins, use release build mode instead

--- trunk/plugins/build.sh
+++ trunk/plugins/build.sh
@@ -13,52 +13,32 @@ cd plugins
 
 # WCX plugins
 $lazbuild wcx/cpio/src/cpio.lpi $DC_ARCH
-strip wcx/cpio/lib/cpio.wcx
-
 $lazbuild wcx/deb/src/deb.lpi $DC_ARCH
-strip wcx/deb/lib/deb.wcx
-
 $lazbuild wcx/rpm/src/rpm.lpi $DC_ARCH
-strip wcx/rpm/lib/rpm.wcx
-
 $lazbuild wcx/unrar/src/unrar.lpi $DC_ARCH
-strip wcx/unrar/lib/unrar.wcx
-
 $lazbuild wcx/zip/src/Zip.lpi $DC_ARCH
-strip wcx/zip/lib/zip.wcx
 
 # WDX plugins
 $lazbuild wdx/rpm_wdx/src/rpm_wdx.lpi $DC_ARCH
-strip wdx/rpm_wdx/lib/rpm_wdx.wdx
-
 $lazbuild wdx/deb_wdx/src/deb_wdx.lpi $DC_ARCH
-strip wdx/deb_wdx/lib/deb_wdx.wdx
-
 $lazbuild wdx/svn_wdx/src/svn_wdx.lpi $DC_ARCH
-strip wdx/svn_wdx/lib/svn_wdx.wdx
-
 $lazbuild wdx/xpi_wdx/src/xpi_wdx.lpi $DC_ARCH
-strip wdx/xpi_wdx/lib/xpi_wdx.wdx
 
 # WFX plugins
 $lazbuild wfx/ftp/src/ftp.lpi $DC_ARCH
-strip wfx/ftp/lib/ftp.wfx
 
 # Don't build under OS X
 if [ -z $(uname | grep Darwin) ]; then
 
   $lazbuild wfx/samba/src/samba.lpi $DC_ARCH
-  strip wfx/samba/lib/samba.wfx
 
   # WLX plugins
   $lazbuild wlx/WlxMplayer/src/wlxMplayer.lpi $DC_ARCH
-  strip wlx/WlxMplayer/lib/wlxmplayer.wlx
 
 fi
 
 # DSX plugins
 $lazbuild dsx/DSXLocate/src/DSXLocate.lpi $DC_ARCH
-strip dsx/DSXLocate/lib/dsxlocate.dsx
 
 # Return from plugins directory
 cd $basedir
