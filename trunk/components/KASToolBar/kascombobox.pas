ac6b0c4bce2b38bd93ebad972ddcd00f5cb0947b
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2015-02-28 12:37:42 +0000

    ADD: TComboBoxAutoWidth class

--- trunk/components/KASToolBar/kascombobox.pas
+++ trunk/components/KASToolBar/kascombobox.pas
@@ -3,7 +3,8 @@
    -------------------------------------------------------------------------
    Extended ComboBox classes
 
-   Copyright (C) 2012  Przemyslaw Nagay (cobines@gmail.com)
+   Copyright (C) 2012 Przemyslaw Nagay (cobines@gmail.com)
+   Copyright (C) 2015 Alexander Koblov (alexx2000@mail.ru)
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -23,7 +24,7 @@
 
 unit KASComboBox;
 
-{$mode objfpc}
+{$mode objfpc}{$H+}
 
 interface
 
@@ -42,16 +43,27 @@ type
     procedure KeyDown(var Key: Word; Shift: TShiftState); override;
   end;
 
+  { TComboBoxAutoWidth }
+
+  TComboBoxAutoWidth = class(TComboBox)
+  protected
+    procedure CalculatePreferredSize(
+                         var PreferredWidth, PreferredHeight: Integer;
+                         WithThemeSpace: Boolean); override;
+    procedure CalculateSize(MaxWidth: Integer;
+                            var NeededWidth, NeededHeight: Integer);
+  end;
+
 procedure Register;
 
 implementation
 
 uses
-  LCLType;
+  LCLType, LCLIntf;
 
 procedure Register;
 begin
-  RegisterComponents('KASComponents',[TComboBoxWithDelItems]);
+  RegisterComponents('KASComponents',[TComboBoxWithDelItems, TComboBoxAutoWidth]);
 end;
 
 { TComboBoxWithDelItems }
@@ -71,6 +83,76 @@ begin
     end;
   end;
   inherited KeyDown(Key, Shift);
+end;
+
+{ TComboBoxAutoWidth }
+
+procedure TComboBoxAutoWidth.CalculatePreferredSize(var PreferredWidth,
+  PreferredHeight: Integer; WithThemeSpace: Boolean);
+var
+  AWidth: Integer;
+begin
+  inherited CalculatePreferredSize(PreferredWidth, PreferredHeight, WithThemeSpace);
+
+  if csDesigning in ComponentState then Exit;
+
+  if (Parent = nil) or (not Parent.HandleAllocated) then Exit;
+
+  AWidth := Constraints.MinMaxWidth(10000);
+  CalculateSize(AWidth, PreferredWidth, PreferredHeight);
+end;
+
+procedure TComboBoxAutoWidth.CalculateSize(MaxWidth: Integer; var NeededWidth,
+  NeededHeight: Integer);
+var
+  DC: HDC;
+  R: TRect;
+  I, M: Integer;
+  Flags: Cardinal;
+  OldFont: HGDIOBJ;
+  LabelText: String;
+  Idx: Integer = -1;
+begin
+  if Items.Count = 0 then
+    LabelText:= Text
+  else begin
+    M := Canvas.TextWidth(Text);
+    for I := 0 to Items.Count - 1 do
+    begin
+      Flags := Canvas.TextWidth(Items[I]);
+      if Flags > M then
+      begin
+        M := Flags;
+        Idx := I;
+      end;
+    end;
+    if Idx < 0 then
+      LabelText := Text
+    else begin
+      LabelText := Items[Idx];
+    end;
+  end;
+
+  if LabelText = '' then begin
+    NeededWidth := 1;
+    NeededHeight := 1;
+    Exit;
+  end;
+
+  DC := GetDC(Parent.Handle);
+  try
+    R := Rect(0, 0, MaxWidth, 10000);
+    OldFont := SelectObject(DC, HGDIOBJ(Font.Reference.Handle));
+    Flags := DT_CALCRECT or DT_EXPANDTABS;
+
+    DrawText(DC, PChar(LabelText), Length(LabelText), R, Flags);
+    SelectObject(DC, OldFont);
+    NeededWidth := R.Right - R.Left + GetSystemMetrics(SM_CXVSCROLL) * 2;
+    NeededHeight := R.Bottom - R.Top;
+    // DebugLn(['TComboBoxAutoWidth.CalculatePreferredSize ',DbgSName(Self),' R=',dbgs(R),' MaxWidth=',MaxWidth,' DT_WORDBREAK=',(DT_WORDBREAK and Flags)>0,' LabelText="',LabelText,'"']);
+  finally
+    ReleaseDC(Parent.Handle, DC);
+  end;
 end;
 
 end.
