db9a9ddf87223edeed2f48b89925200ac134df8d
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2015-03-24 19:34:00 +0000

    FIX: Patch [0001053] "Fix build under FreeBSD"

--- trunk/src/platform/uosforms.pas
+++ trunk/src/platform/uosforms.pas
@@ -96,7 +96,7 @@ procedure ShowDriveContextMenu(Parent: TWinControl; ADrive: PDrive; X, Y : Integ
 }
 function ShowOpenIconDialog(Owner: TCustomControl; var sFileName : String) : Boolean;
 
-{$IF DEFINED(LINUX)}
+{$IF DEFINED(UNIX) AND NOT DEFINED(DARWIN)}
 {en
    Show open with dialog
    @param(FileList List of files to open with)
@@ -121,7 +121,7 @@ uses
     {$ELSE}
     , MacOSAll
     {$ENDIF}
-    {$IFDEF LINUX}
+    {$IF NOT DEFINED(DARWIN)}
     , fOpenWith, uKde
     {$ENDIF}
   {$ENDIF};
@@ -555,7 +555,7 @@ begin
     FreeAndNil(opdDialog);
 end;
 
-{$IF DEFINED(LINUX)}
+{$IF DEFINED(UNIX) AND NOT DEFINED(DARWIN)}
 procedure ShowOpenWithDialog(const FileList: TStringList);
 begin
   if not (UseKde and uKde.ShowOpenWithDialog(FileList)) then begin
