d5cb05d4c6bf0fcf79f067640ba7b87360d3bd9c
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2015-03-29 09:36:49 +0000

    FIX: Bug [0001033] "Function key buttons bar: buttons F3-F7 and F9 don't restored the state after click"

--- trunk/src/umaincommands.pas
+++ trunk/src/umaincommands.pas
@@ -107,7 +107,7 @@ type
    // 7. So command is "cm_SrcOpenDrives", so keep writing "cm_SrcOpenDrives" and not "cm_srcopendrives" for example.
    // 8. The only single place to have lowercases is for the icon name which will be "cm_srcopendrives" but it's the only one case.
    // 9. Give an appropriate "caption" name for the command, so for our example "Open drive list"
-   // 10. Set the "GroupIndex" to the same number as the other command of the same category.
+   // 10. Set the "Tag" to the same number as the other command of the same category.
    // 11. In the "uMainCommands", for the type "TMainCommands", add the code for the command.
    // 12. The command name must start with "cm_" and ends with the same name as what we added for the "act".
    // 13. So with our example we add "cm_SrcOpenDrives".
