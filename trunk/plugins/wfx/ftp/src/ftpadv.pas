2eb8165232d7dcee757217fd6027926428c8a705
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2015-05-02 07:49:04 +0000

    FIX: Copy zero size file UPD: License

--- trunk/plugins/wfx/ftp/src/ftpadv.pas
+++ trunk/plugins/wfx/ftp/src/ftpadv.pas
@@ -1,3 +1,25 @@
+{
+   Double commander
+   -------------------------------------------------------------------------
+   WFX plugin for working with File Transfer Protocol
+
+   Copyright (C) 2009-2015 Alexander Koblov (alexx2000@mail.ru)
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+}
+
 unit FtpAdv;
 
 {$mode delphi}
@@ -87,13 +109,13 @@ end;
 function TProgressStream.Read(var Buffer; Count: Longint): Longint;
 begin
   Result:= inherited Read(Buffer, Count);
-  DoProgress(Result);
+  if FileSize > 0 then DoProgress(Result);
 end;
 
 function TProgressStream.Write(const Buffer; Count: Longint): Longint;
 begin
   Result:= inherited Write(Buffer, Count);
-  DoProgress(Result);
+  if FileSize > 0 then DoProgress(Result);
 end;
 
 end.
--- trunk/plugins/wfx/ftp/src/ftputils.pas
+++ trunk/plugins/wfx/ftp/src/ftputils.pas
@@ -8,7 +8,7 @@
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
+   version 2.1 of the License, or (at your option) any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +17,7 @@
 
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 }
 
 unit FtpUtils;
