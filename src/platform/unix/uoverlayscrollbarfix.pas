7ecec3e1f3f24a2d130b268f8237d054ca4e6bb7
Alexander Koblov <alexx2000@mail.ru>
2015-05-09 10:01:36 +0000

    FIX: Workaround for bug [0000875]

--- src/platform/unix/uoverlayscrollbarfix.pas
+++ src/platform/unix/uoverlayscrollbarfix.pas
@@ -6,9 +6,17 @@ interface
 
 implementation
 
-  function setenv(const name, value: pchar; overwrite: longint): longint; cdecl; external 'c' name 'setenv';
+uses
+  BaseUnix;
+
+function setenv(const name, value: pchar; overwrite: longint): longint; cdecl; external 'c' name 'setenv';
 
 initialization
   setenv('LIBOVERLAY_SCROLLBAR', '0', 1);
+  if (fpGetEnv(PAnsiChar('GTK_IM_MODULE')) = 'xim') then
+  begin
+    setenv('GTK_IM_MODULE', '', 1);
+    WriteLn('Warning: Unsupported input method (xim)');
+  end;
 
 end.
