691732ca0275ce7830bf1606726229afa0b05a16
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2015-07-05 13:08:00 +0000

    FIX: Message dialog encoding (OS X Qt 4)

--- trunk/src/platform/unix/uapplemagnifiedmodefix.pas
+++ trunk/src/platform/unix/uapplemagnifiedmodefix.pas
@@ -8,7 +8,7 @@ interface
 implementation
 
 uses
-  CocoaAll;
+  BaseUnix, CocoaAll;
 
 const
   SecondStart = 'SecondStart';
@@ -17,7 +17,29 @@ const
 var
   UserDefaults: NSUserDefaults;
 
+function setenv(const name, value: pchar; overwrite: longint): longint; cdecl; external 'c' name 'setenv';
+
+procedure ExportLanguage;
+var
+  CurrentLocale: NSLocale;
+  Language, Country: String;
+begin
+  if fpGetEnv(PAnsiChar('LANG')) = '' then
+  begin
+    CurrentLocale:= NSLocale.currentLocale();
+    Country:= NSString(CurrentLocale.objectForKey(NSLocaleCountryCode)).UTF8String;
+    Language:= NSString(CurrentLocale.objectForKey(NSLocaleLanguageCode)).UTF8String;
+    if (Length(Language) > 0) and (Length(Country) > 0) then
+    begin
+      Language:= Language + '_' + Country + '.UTF-8';
+      setenv('LANG', PAnsiChar(Language), 1);
+      WriteLn('Export LANG=' + Language);
+    end;
+  end;
+end;
+
 initialization
+  {$IFDEF LCLQT} ExportLanguage; {$ENDIF}
   UserDefaults:= NSUserDefaults.standardUserDefaults;
   if not UserDefaults.boolForKey(NSSTR(SecondStart)) then
   begin
