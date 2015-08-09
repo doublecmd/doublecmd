a90a257b9debdde9f0ed09fd0048a6be9d4dc3ee
Alexander Koblov <alexx2000@mail.ru>
2015-08-09 18:35:26 +0000

    ADD: Feature [0001145] Add support for PSD (Photoshop) files when viewing files as thumbnails in the file view panels

---
+++ src/udcreadpsd.pas
@@ -0,0 +1,77 @@
+{
+   Double Commander
+   -------------------------------------------------------------------------
+   Photoshop Document image class
+
+   Copyright (C) 2015 Alexander Koblov (alexx2000@mail.ru)
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+}
+
+unit uDCReadPSD;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Graphics, FPImage;
+
+type
+
+  { TPhotoshopDocument }
+
+  TPhotoshopDocument = class(TFPImageBitmap)
+  protected
+    class function GetReaderClass: TFPCustomImageReaderClass; override;
+    class function GetSharedImageClass: TSharedRasterImageClass; override;
+  public
+    class function GetFileExtensions: String; override;
+  end;
+
+implementation
+
+uses
+  FPReadPSD;
+
+{ TPhotoshopDocument }
+
+class function TPhotoshopDocument.GetReaderClass: TFPCustomImageReaderClass;
+begin
+  Result:= TFPReaderPSD;
+end;
+
+class function TPhotoshopDocument.GetSharedImageClass: TSharedRasterImageClass;
+begin
+  Result:= TSharedBitmap;
+end;
+
+class function TPhotoshopDocument.GetFileExtensions: String;
+begin
+  Result:= 'psd';
+end;
+
+procedure Initialize;
+begin
+  // Register image handler and format
+  ImageHandlers.RegisterImageReader('Photoshop Document', 'PSD', TFPReaderPSD);
+  TPicture.RegisterFileFormat('psd', 'Photoshop Document', TPhotoshopDocument);
+end;
+
+initialization
+  Initialize;
+
+end.
+
