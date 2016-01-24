27229921716e5f9aa9bd43cb3ae1f4e0c2001dc7
Alexander Koblov <alexx2000@mail.ru>
2016-01-24 16:02:36 +0000

    UPD: Show Lazarus revision only if exists

--- src/fAbout.pas
+++ src/fAbout.pas
@@ -159,7 +159,7 @@ begin
                     'Platform: %s' + LineEnding +
                     'OS version: %s' + LineEnding,
                     [dcVersion, dcRevision, dcBuildDate,
-                    lazVersion + '-' + lazRevision, fpcVersion,
+                    GetLazarusVersion, fpcVersion,
                     TargetCPU + '-' + TargetOS + '-' + TargetWS,
                     OSVersion]);
   if WSVersion <> EmptyStr then
@@ -181,7 +181,7 @@ begin
   lblVersion.Caption         := lblVersion.Caption + #32 + dcVersion;
   lblRevision.Caption        := lblRevision.Caption + #32 + dcRevision;
   lblBuild.Caption           := lblBuild.Caption + #32 + dcBuildDate;
-  lblLazarusVer.Caption      := lblLazarusVer.Caption + #32 + lazVersion + '-' + lazRevision;
+  lblLazarusVer.Caption      := lblLazarusVer.Caption + #32 + GetLazarusVersion;
   lblFreePascalVer.Caption   := lblFreePascalVer.Caption + #32 + fpcVersion;
   lblPlatform.Caption        := TargetCPU + '-' + TargetOS + '-' + TargetWS;
   lblOperatingSystem.Caption := OSVersion;
