c411c670e5968f72d58cc3d917adde6ff916a152
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2016-05-28 13:17:52 +0000

    FIX: GetFileType under OS X

---
+++ trunk/src/platform/unix/umydarwin.pas
@@ -0,0 +1,62 @@
+{
+   Double Commander
+   -------------------------------------------------------------------------
+   This unit contains specific DARWIN functions.
+
+   Copyright (C) 2016 Alexander Koblov (alexx2000@mail.ru)
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+}
+
+unit uMyDarwin;
+
+{$mode objfpc}{$H+}
+{$modeswitch objectivec1}
+
+interface
+
+uses
+  Classes, SysUtils, MacOSAll, CocoaAll;
+
+function GetFileDescription(const FileName: String): String;
+
+implementation
+
+function StringToCFStringRef(const Value: String): CFStringRef;
+begin
+  Result:= CFStringCreateWithCString(nil, PAnsiChar(Value), kCFStringEncodingUTF8)
+end;
+
+function GetFileDescription(const FileName: String): String;
+var
+  Error: NSError;
+  WS: NSWorkspace;
+  FileType: NSString;
+  FileNameRef: CFStringRef;
+begin
+  WS:= NSWorkspace.sharedWorkspace;
+  FileNameRef:= StringToCFStringRef(FileName);
+  if (FileNameRef = nil) then Exit(EmptyStr);
+  FileType:= WS.typeOfFile_error(NSString(FileNameRef), @Error);
+  if (FileType = nil) then
+    Result:= Error.localizedDescription.UTF8String
+  else begin
+    Result:= WS.localizedDescriptionForType(FileType).UTF8String;
+  end;
+  CFRelease(FileNameRef);
+end;
+
+end.
+
