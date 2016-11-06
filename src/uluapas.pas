68e722d680732bf1a14588f03506c424c1373634
Alexander Koblov <alexx2000@mail.ru>
2016-11-06 10:32:12 +0000

    ADD: Execute Lua script via internal command cm_ExecuteScript ADD: Execute DC internal commands from Lua script via DC.ExecuteCommand function

--- src/uluapas.pas
+++ src/uluapas.pas
@@ -30,11 +30,12 @@ uses
   Classes, SysUtils, lua;
 
 procedure RegisterPackages(L : Plua_State);
+function ExecuteScript(const FileName: String; Args: array of String): Boolean;
 
 implementation
 
 uses
-  LazUTF8, DCOSUtils;
+  Forms, Dialogs, LazUTF8, DCOSUtils, fMain, uFormCommands, uOSUtils;
 
 function luaFileGetAttr(L : Plua_State) : Integer; cdecl;
 begin
@@ -54,6 +55,32 @@ begin
   lua_pushboolean(L, mbDirectoryExists(lua_tostring(L, 1)));
 end;
 
+function luaExecuteCommand(L : Plua_State) : Integer; cdecl;
+var
+  Index,
+  Count: Integer;
+  Command: String;
+  Args: array of String;
+  Res: TCommandFuncResult;
+begin
+  Result:= 1;
+  Res:= cfrNotFound;
+  Count:= lua_gettop(L);
+  if Count > 0 then
+  begin
+    // Get command
+    Command:= lua_tostring(L, 1);
+    // Get parameters
+    SetLength(Args, Count - 1);
+    for Index:= 2 to Count do
+     Args[Index - 2]:= lua_tostring(L, Index);
+    // Execute internal command
+    Res:= frmMain.Commands.Commands.ExecuteCommand(Command, Args);
+    Application.ProcessMessages;
+  end;
+  lua_pushboolean(L, Res = cfrSuccess);
+end;
+
 procedure luaP_register(L : Plua_State; n : PChar; f : lua_CFunction);
 begin
   lua_pushcfunction(L, f);
@@ -67,6 +94,60 @@ begin
     luaP_register(L, 'FileGetAttr', @luaFileGetAttr);
     luaP_register(L, 'DirectoryExists', @luaDirectoryExists);
   lua_setglobal(L, 'SysUtils');
+
+  lua_newtable(L);
+    luaP_register(L, 'ExecuteCommand', @luaExecuteCommand);
+  lua_setglobal(L, 'DC');
+end;
+
+function ExecuteScript(const FileName: String; Args: array of String): Boolean;
+var
+  L: Plua_State;
+  Index: Integer;
+  Count: Integer;
+  Script: String;
+  Status: Integer;
+begin
+  Result:= False;
+
+  // Get script file name
+  Script:= mbFileNameToSysEnc(FileName);
+
+  L := lua_open;
+  if Assigned(L) then
+  begin
+    luaL_openlibs(L);
+    RegisterPackages(L);
+
+    // Push arguments
+    Count:= Length(Args);
+    if (Count > 0) then
+    begin
+      for Index := 0 to Count - 1 do begin
+        lua_pushstring(L, PAnsiChar(Args[Index]));
+      end;
+      lua_createtable(L, Count, 0);
+      lua_setglobal(L, 'arg');
+    end;
+
+    // Execute script
+    Status := luaL_loadfile(L, PAnsiChar(Script));
+    lua_insert(L, -(Count + 1));
+    if (Status = 0) then
+      Status := lua_pcall(L, Count, 0, 0)
+    else begin
+      lua_pop(L, Count);
+    end;
+
+    // Check execution result
+    if Status <> 0 then begin
+      MessageDlg(lua_tostring(L, -1), mtError, [mbOK], 0);
+    end;
+
+    lua_close(L);
+
+    Result:= (Status = 0);
+  end;
 end;
 
 end.
