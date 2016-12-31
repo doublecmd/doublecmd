b2bf561463d08dfa1784916bca0c200713bc437a
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2016-12-31 12:40:21 +0000

    ADD: SHA2_224 support (issue 0001666)

--- trunk/src/uhash.pas
+++ trunk/src/uhash.pas
@@ -4,7 +4,7 @@
     General Hash Unit: This unit defines the common types, functions,
     and procedures
 
-    Copyright (C) 2009-2015 Alexander Koblov (alexx2000@mail.ru)
+    Copyright (C) 2009-2016 Alexander Koblov (alexx2000@mail.ru)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -34,22 +34,23 @@ type
   THashContext   = TDCP_hash;
   THashAlgorithm = (HASH_BLAKE2S, HASH_BLAKE2SP, HASH_CRC32, HASH_HAVAL, HASH_MD4,
                     HASH_MD5, HASH_RIPEMD128, HASH_RIPEMD160, HASH_SFV, HASH_SHA1,
-                    HASH_SHA256, HASH_SHA384, HASH_SHA512, HASH_SHA3_224, HASH_SHA3_256,
-                    HASH_SHA3_384, HASH_SHA3_512, HASH_TIGER
+                    HASH_SHA224, HASH_SHA256, HASH_SHA384, HASH_SHA512, HASH_SHA3_224,
+                    HASH_SHA3_256, HASH_SHA3_384, HASH_SHA3_512, HASH_TIGER
                     );
 
 var
   HashFileExt: array[THashAlgorithm] of String = (
                  'blake2s', 'blake2sp', 'crc32', 'haval', 'md4', 'md5',
-                 'ripemd128', 'ripemd160', 'sfv', 'sha', 'sha256', 'sha384',
-                 'sha512', 'sha3', 'sha3', 'sha3', 'sha3', 'tiger'
+                 'ripemd128', 'ripemd160', 'sfv', 'sha', 'sha224', 'sha256',
+                 'sha384', 'sha512', 'sha3', 'sha3', 'sha3', 'sha3', 'tiger'
                );
 
 var
   HashName: array[THashAlgorithm] of String = (
                  'blake2s', 'blake2sp', 'crc32', 'haval', 'md4', 'md5',
-                 'ripemd128', 'ripemd160', 'sfv', 'sha1_160', 'sha2_256', 'sha2_384',
-                 'sha2_512', 'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512', 'tiger'
+                 'ripemd128', 'ripemd160', 'sfv', 'sha1_160', 'sha2_224',
+                 'sha2_256', 'sha2_384', 'sha2_512', 'sha3_224', 'sha3_256',
+                 'sha3_384', 'sha3_512', 'tiger'
                );
 
 procedure HashInit(out Context: THashContext; const Algorithm: THashAlgorithm);
@@ -66,7 +67,7 @@ implementation
 
 uses
   LazUTF8, DCPhaval, DCPmd4, DCPmd5, DCPripemd128, DCPripemd160, DCPcrc32,
-  DCPsha1, DCPsha256, DCPsha512, DCPtiger, DCPblake2, DCPsha3;
+  DCPsha1, DCPsha256, DCPsha512, DCPtiger, DCPblake2, DCPsha3, DCPsha224;
 
 procedure HashInit(out Context: THashContext; const Algorithm: THashAlgorithm);
 begin
@@ -81,6 +82,7 @@ begin
     HASH_RIPEMD160:  Context:= TDCP_ripemd160.Create(nil);
     HASH_SFV:        Context:= TDCP_crc32.Create(nil);
     HASH_SHA1:       Context:= TDCP_sha1.Create(nil);
+    HASH_SHA224:     Context:= TDCP_sha224.Create(nil);
     HASH_SHA256:     Context:= TDCP_sha256.Create(nil);
     HASH_SHA384:     Context:= TDCP_sha384.Create(nil);
     HASH_SHA512:     Context:= TDCP_sha512.Create(nil);
