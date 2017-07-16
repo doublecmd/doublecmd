512c53e182ccd405a65f67eface3c6e9f44d3239
Alexander Koblov <alexx2000@mail.ru>
2017-07-16 07:23:43 +0000

    ADD: Copyright info

--- plugins/wfx/ftp/src/sftp/sftpsend.pas
+++ plugins/wfx/ftp/src/sftp/sftpsend.pas
@@ -1,3 +1,25 @@
+{
+   Double commander
+   -------------------------------------------------------------------------
+   Wfx plugin for working with File Transfer Protocol
+
+   Copyright (C) 2013-2017 Alexander Koblov (alexx2000@mail.ru)
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+}
+
 unit SftpSend;
 
 {$mode delphi}
@@ -224,7 +246,7 @@ end;
 function TSftpSend.Logout: Boolean;
 begin
   Result:= libssh2_sftp_shutdown(FSFTPSession) = 0;
-  libssh2_session_disconnect(FSession, 'Thank you for using sshtest');
+  libssh2_session_disconnect(FSession, 'Logout');
   libssh2_session_free(FSession);
   FSock.CloseSocket;
 end;
