a13674471869e61cd8d3691e7ec11a0798829004
Alexander Koblov <alexx2000@mail.ru>
2017-08-05 10:11:03 +0000

    ADD: HMAC_SHA3-256 function

--- components/dcpcrypt/dcpcrypt.pas
+++ components/dcpcrypt/dcpcrypt.pas
@@ -4,12 +4,14 @@
 
 unit dcpcrypt;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
   DCPbase64, DCPblockciphers, DCPconst, DCPcrypt2, DCPhaval, DCPmd4, DCPmd5, 
   DCPripemd128, DCPripemd160, DCPsha1, DCPsha256, DCPsha512, DCPtiger, 
-  DCPcrc32, DCcrc32, DCblake2, DCPblake2, DCPsha3, DCPsha224;
+  DCPcrc32, DCcrc32, DCblake2, DCPblake2, DCPsha3, DCPsha224, HMAC, SHA3, 
+  SHA3_256;
 
 implementation
 
