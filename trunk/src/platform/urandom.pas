eabc7bdfc0a0452b3abd527415074beddc968f10
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2017-08-06 07:54:35 +0000

    ADD: Use /dev/urandom under Unix to get random bytes

--- trunk/src/platform/urandom.pas
+++ trunk/src/platform/urandom.pas
@@ -17,16 +17,36 @@ uses
 
 var
   RtlGenRandom: function(RandomBuffer: PByte; RandomBufferLength: ULONG): LongBool; stdcall;
+{$ELSEIF DEFINED(UNIX)}
+uses
+  DCOSUtils;
+
+const
+  random_dev = '/dev/urandom';
+
+var
+  HasRandom: Boolean = False;
 {$ENDIF}
 
 procedure Random(ABlock: PByte; ACount: Integer);
 var
-  I: Integer;
+  I: THandle;
   Result: Boolean = False;
 begin
 {$IF DEFINED(MSWINDOWS)}
   Result:= Assigned(RtlGenRandom);
   if Result then Result:= RtlGenRandom(ABlock, ACount);
+{$ELSEIF DEFINED(UNIX)}
+  if HasRandom then
+  begin
+    I:= mbFileOpen(random_dev, fmOpenRead or fmShareDenyNone);
+    Result:= (I <> feInvalidHandle);
+    if Result then
+    begin
+      Result:= (FileRead(I, ABlock^, ACount) = ACount);
+      FileClose(I);
+    end;
+  end;
 {$ENDIF}
   if not Result then for I:= 0 to ACount - 1 do ABlock[I]:= Byte(System.Random(256));
 end;
@@ -34,6 +54,8 @@ end;
 initialization
 {$IF DEFINED(MSWINDOWS)}
   @RtlGenRandom:= GetProcAddress(GetModuleHandle('advapi32.dll'), 'SystemFunction036');
+{$ELSEIF DEFINED(UNIX)}
+  HasRandom:= mbFileAccess(random_dev, fmOpenRead);
 {$ENDIF}
 
 end.
