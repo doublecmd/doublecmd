7552e6177f7186dff12b6f2acedb4d68da44e1f3
Alexander Koblov <alexx2000@mail.ru>
2017-08-24 19:11:50 +0000

    ADD: Display unmounted drive icons half-transparent

--- src/platform/upixmapmanager.pas
+++ src/platform/upixmapmanager.pas
@@ -324,7 +324,7 @@ implementation
 uses
   GraphType, LCLIntf, LCLType, LCLProc, Forms, uGlobsPaths, WcxPlugin,
   DCStrUtils, uDCUtils, uFileSystemFileSource, uReSample, uDebug,
-  DCOSUtils, DCClassesUtf8, LazUTF8
+  DCOSUtils, DCClassesUtf8, LazUTF8, uGraphics
   {$IFDEF LCLGTK2}
     , uPixMapGtk, gdk2pixbuf, gdk2, glib2
   {$ENDIF}
@@ -1988,6 +1988,11 @@ begin
     begin
       Result := GetBuiltInDriveIcon(Drive, IconSize, clBackColor);
     end;
+
+  if Assigned(Result) and (not Drive^.IsMounted) then
+  begin
+    BitmapAlpha(Result, 0.5);
+  end;
 end;
 
 function TPixMapManager.GetBuiltInDriveIcon(Drive : PDrive; IconSize : Integer; clBackColor : TColor) : Graphics.TBitmap;
