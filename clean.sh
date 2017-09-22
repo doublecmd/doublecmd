bd37abb1a0fe1b98dd7d7cf8b618b5c55677eec7
Alexander Koblov <alexx2000@mail.ru>
2017-09-22 12:18:40 +0000

    FIX: Clean script (clearing all subdirectories)

--- clean.sh
+++ clean.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-rm -f units/i386-linux-gtk2/*
-rm -f units/i386-linux-qt/*
-rm -f units/x86_64-linux-gtk2/*
-rm -f units/x86_64-linux-qt/*
+# Clean up output directories
+rm -f units/*/*
 
 # Clean up components output directories
 rm -rf components/chsdet/lib/*
