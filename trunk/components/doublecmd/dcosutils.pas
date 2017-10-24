221677b425afe711e1dd9ec1a6b3b8414d7d6de0
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2017-10-24 18:04:53 +0000

    UPD: Move sym/hard link code to doublecmd_common package

--- trunk/components/doublecmd/dcosutils.pas
+++ trunk/components/doublecmd/dcosutils.pas
@@ -192,11 +192,33 @@ function mbGetModuleName(Address: Pointer = nil): String;
 function mbLoadLibrary(const Name: String): TLibHandle;
 function SafeGetProcAddress(Lib: TLibHandle; const ProcName: AnsiString): Pointer;
 
+{en
+   Create a hard link to a file
+   @param(Path Name of file)
+   @param(LinkName Name of hard link)
+   @returns(The function returns @true if successful, @false otherwise)
+}
+function CreateHardLink(const Path, LinkName: String) : Boolean;
+{en
+   Create a symbolic link
+   @param(Path Name of file)
+   @param(LinkName Name of symbolic link)
+   @returns(The function returns @true if successful, @false otherwise)
+}
+function CreateSymLink(const Path, LinkName: string) : Boolean;
+{en
+   Read destination of symbolic link
+   @param(LinkName Name of symbolic link)
+   @returns(The file name/path the symbolic link name is pointing to.
+            The path may be relative to link's location.)
+}
+function ReadSymLink(const LinkName : String) : String;
+
 implementation
 
 uses
 {$IF DEFINED(MSWINDOWS)}
-  Windows, JwaWinNetWk, DCDateTimeUtils, DCWindows,
+  Windows, JwaWinNetWk, DCDateTimeUtils, DCWindows, DCNtfsLinks,
 {$ENDIF}
 {$IF DEFINED(UNIX)}
   BaseUnix, Unix, dl, DCUnix,
@@ -1383,5 +1405,52 @@ begin
   Result:= GetProcedureAddress(Lib, ProcName);
   if (Result = nil) then raise Exception.Create(ProcName);
 end;
+
+function CreateHardLink(const Path, LinkName: String) : Boolean;
+{$IFDEF MSWINDOWS}
+var
+  wsPath, wsLinkName: UnicodeString;
+begin
+  wsPath:= UTF16LongName(Path);
+  wsLinkName:= UTF16LongName(LinkName);
+  Result:= DCNtfsLinks.CreateHardlink(wsPath, wsLinkName);
+end;
+{$ELSE}
+begin
+  Result := (fplink(PAnsiChar(CeUtf8ToSys(Path)),PAnsiChar(CeUtf8ToSys(LinkName)))=0);
+end;
+{$ENDIF}
+
+function CreateSymLink(const Path, LinkName: string) : Boolean;
+{$IFDEF MSWINDOWS}
+var
+  wsPath, wsLinkName: UnicodeString;
+begin
+  wsPath:= UTF8Decode(Path);
+  wsLinkName:= UTF16LongName(LinkName);
+  Result:= DCNtfsLinks.CreateSymlink(wsPath, wsLinkName);
+end;
+{$ELSE}
+begin
+  Result := (fpsymlink(PAnsiChar(CeUtf8ToSys(Path)), PAnsiChar(CeUtf8ToSys(LinkName)))=0);
+end;
+{$ENDIF}
+
+function ReadSymLink(const LinkName : String) : String;
+{$IFDEF MSWINDOWS}
+var
+  wsLinkName, wsTarget: UnicodeString;
+begin
+  wsLinkName:= UTF16LongName(LinkName);
+  if DCNtfsLinks.ReadSymLink(wsLinkName, wsTarget) then
+    Result := UTF16ToUTF8(wsTarget)
+  else
+    Result := EmptyStr;
+end;
+{$ELSE}
+begin
+  Result := SysToUTF8(fpReadlink(UTF8ToSys(LinkName)));
+end;
+{$ENDIF}
 
 end.
