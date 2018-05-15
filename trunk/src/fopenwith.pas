ab99519047d808563bc8dc5e630f1e75e601036d
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2018-05-15 19:11:13 +0000

    ADD: Open with dialog - group applications in categories

--- trunk/src/fopenwith.pas
+++ trunk/src/fopenwith.pas
@@ -68,6 +68,7 @@ type
     FMimeType: String;
     FFileList: TStringList;
     procedure LoadApplicationList;
+    function TreeNodeCompare(Node1, Node2: TTreeNode): Integer;
   public
     constructor Create(TheOwner: TComponent; AFileList: TStringList); reintroduce;
   end;
@@ -80,7 +81,29 @@ implementation
 
 uses
   LCLProc, DCStrUtils, uOSUtils, uPixMapManager, uGlobs, uMimeActions,
-  uMimeType, uLng, LazUTF8;
+  uMimeType, uLng, LazUTF8, Math;
+
+const
+  CATEGORY_OTHER = 11; // 'Other' category index
+
+const
+  // See https://specifications.freedesktop.org/menu-spec/latest
+  CATEGORIES: array[0..12] of String =
+  (
+    'AudioVideo',
+    'Audio',
+    'Video',
+    'Development',
+    'Education',
+    'Game',
+    'Graphics',
+    'Network',
+    'Office',
+    'Science',
+    'Settings',
+    'System',
+    'Utility'
+  );
 
 procedure ShowOpenWithDlg(const FileList: TStringList);
 begin
@@ -97,6 +120,19 @@ begin
   FFileList:= AFileList;
   inherited Create(TheOwner);
   InitPropStorage(Self);
+
+  tvApplications.Items.AddChild(nil, rsOpenWithMultimedia);
+  tvApplications.Items.AddChild(nil, rsOpenWithDevelopment);
+  tvApplications.Items.AddChild(nil, rsOpenWithEducation);
+  tvApplications.Items.AddChild(nil, rsOpenWithGames);
+  tvApplications.Items.AddChild(nil, rsOpenWithGraphics);
+  tvApplications.Items.AddChild(nil, rsOpenWithNetwork);
+  tvApplications.Items.AddChild(nil, rsOpenWithOffice);
+  tvApplications.Items.AddChild(nil, rsOpenWithScience);
+  tvApplications.Items.AddChild(nil, rsOpenWithSettings);
+  tvApplications.Items.AddChild(nil, rsOpenWithSystem);
+  tvApplications.Items.AddChild(nil, rsOpenWithUtility);
+  tvApplications.Items.AddChild(nil, rsOpenWithOther);
 end;
 
 procedure TfrmOpenWith.FormCreate(Sender: TObject);
@@ -106,7 +142,7 @@ begin
   FMimeType:= GetFileMimeType(FFileList[0]);
   lblMimeType.Caption:= Format(lblMimeType.Caption, [FMimeType]);
   LoadApplicationList;
-  tvApplications.AlphaSort;
+  tvApplications.CustomSort(@TreeNodeCompare);
 end;
 
 procedure TfrmOpenWith.chkCustomCommandChange(Sender: TObject);
@@ -158,8 +194,11 @@ begin
     end
   else if tvApplications.SelectionCount > 0 then
     begin
-      DesktopFile:= PDesktopFileEntry(tvApplications.Selected.Data);
-      fneCommand.Text:= DesktopFile^.DesktopFilePath;
+      if Assigned(tvApplications.Selected.Data) then
+      begin
+        DesktopFile:= PDesktopFileEntry(tvApplications.Selected.Data);
+        fneCommand.Text:= DesktopFile^.DesktopFilePath;
+      end;
     end;
   if Assigned(DesktopFile) then
   begin
@@ -180,8 +219,11 @@ procedure TfrmOpenWith.tvApplicationsDeletion(Sender: TObject; Node: TTreeNode);
 var
   DesktopFile: PDesktopFileEntry;
 begin
-  DesktopFile:= PDesktopFileEntry(Node.Data);
-  Dispose(DesktopFile);
+  if Assigned(Node.Data) then
+  begin
+    DesktopFile:= PDesktopFileEntry(Node.Data);
+    Dispose(DesktopFile);
+  end;
 end;
 
 procedure TfrmOpenWith.tvApplicationsSelectionChanged(Sender: TObject);
@@ -191,8 +233,15 @@ begin
   if tvApplications.SelectionCount > 0 then
   begin
     chkCustomCommand.Checked:= False;
-    DesktopFile:= PDesktopFileEntry(tvApplications.Selected.Data);
-    fneCommand.Text:= DesktopFile^.ExecWithParams;
+    if (tvApplications.Selected.Data = nil) then
+    begin
+      DesktopFile:= nil;
+      fneCommand.Text:= EmptyStr;
+    end
+    else begin
+      DesktopFile:= PDesktopFileEntry(tvApplications.Selected.Data);
+      fneCommand.Text:= DesktopFile^.ExecWithParams;
+    end;
   end;
 end;
 
@@ -201,12 +250,33 @@ const
   Folders: array [1..2] of String = ('/.local/share/applications',
                                          '/usr/share/applications');
 var
-  I, J: Integer;
   Bitmap: TBitmap;
+  I, J, K: Integer;
   ImageIndex: PtrInt;
   TreeNode: TTreeNode;
+  Index, Count: Integer;
   Applications: TStringList;
   DesktopFile: PDesktopFileEntry;
+
+  function GetCategoryIndex(const Category: String): Integer;
+  var
+    Index: Integer;
+  begin
+    Result:= CATEGORY_OTHER; // Default 'other' category
+    for Index:= Low(CATEGORIES) to High(CATEGORIES) do
+    begin
+      if SameText(CATEGORIES[Index], Category) then
+      begin
+        if Index < 3 then
+          Result:= 0
+        else begin
+          Result:= Index - 2;
+        end;
+        Break;
+      end;
+    end;
+  end;
+
 begin
   Folders[1]:= GetHomeDir + Folders[1];
   for I:= Low(Folders) to High(Folders) do
@@ -217,7 +287,7 @@ begin
       DesktopFile:= GetDesktopEntry(Applications[J]);
       if Assigned(DesktopFile) then
       begin
-        if DesktopFile^.Hidden or (Pos('Screensaver', DesktopFile^.Categories) > 0) then
+        if DesktopFile^.Hidden or Contains(DesktopFile^.Categories, 'Screensaver') then
         begin
           Dispose(DesktopFile);
           Continue;
@@ -227,8 +297,23 @@ begin
           DesktopFilePath:= ExtractDirLevel(Folders[I] + PathDelim, DesktopFilePath);
           DesktopFilePath:= StringReplace(DesktopFilePath, PathDelim, '-', [rfReplaceAll]);
         end;
-        TreeNode:= tvApplications.Items.AddChild(nil, DesktopFile^.DisplayName);
+
+        // Determine application category
+        Count:= Min(3, Length(DesktopFile^.Categories));
+        if Count = 0 then
+          Index:= CATEGORY_OTHER
+        else begin
+          for K:= 0 to Count - 1 do
+          begin
+            Index:= GetCategoryIndex(DesktopFile^.Categories[K]);
+            if Index <> CATEGORY_OTHER then Break;
+          end;
+        end;
+
+        TreeNode:= tvApplications.Items.TopLvlItems[Index];
+        TreeNode:= tvApplications.Items.AddChild(TreeNode, DesktopFile^.DisplayName);
         TreeNode.Data:= DesktopFile;
+
         ImageIndex:= PixMapManager.GetIconByName(DesktopFile^.IconName);
         if ImageIndex >= 0 then
         begin
@@ -245,6 +330,22 @@ begin
     end;
     Applications.Free;
   end;
+  // Hide empty categories
+  for Index:= 0 to tvApplications.Items.TopLvlCount - 1 do
+  begin
+    if tvApplications.Items.TopLvlItems[Index].Count = 0 then
+      tvApplications.Items.TopLvlItems[Index].Visible:= False;
+  end;
+end;
+
+function TfrmOpenWith.TreeNodeCompare(Node1, Node2: TTreeNode): Integer;
+begin
+  if SameText(Node1.Text, rsOpenWithOther) then
+    Result:= +1
+  else if SameText(Node2.Text, rsOpenWithOther) then
+    Result:= -1
+  else
+    Result := LazUTF8.Utf8CompareStr(Node1.Text, Node2.Text);
 end;
 
 end.
