a817a8226243ff99c2ca485a3c87c6ef3fc8dc51
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2018-10-21 08:08:54 +0000

    FIX: Bug [0002141] Link failure on ARM #2

--- trunk/src/platform/unix/uqt5workaround.pas
+++ trunk/src/platform/unix/uqt5workaround.pas
@@ -9,17 +9,8 @@ implementation
 uses
   InitC, BaseUnix;
 
-var
-  __dso_handle: pointer = nil; cvar; public;
-
 procedure _exit(status: cint); cdecl; external clib;
-function atexit(func: pointer): cint; cdecl; external clib;
 function setenv(const name, value: pchar; overwrite: cint): cint; cdecl; external clib;
-
-procedure DoExit; cdecl;
-begin
-  _exit(ExitCode);
-end;
 
 initialization
   if (LowerCase(fpGetEnv(PAnsiChar('XDG_SESSION_TYPE'))) = 'wayland') then
@@ -29,8 +20,8 @@ finalization
   // Workaround: https://doublecmd.sourceforge.io/mantisbt/view.php?id=2079
   if (UpCase(fpGetEnv(PAnsiChar('XDG_CURRENT_DESKTOP'))) = 'KDE') then
   begin
-    atexit(@DoExit);
     WriteLn('Warning: Skip libKF5IconThemes exit handler');
+    _exit(ExitCode);
   end;
 
 end.
