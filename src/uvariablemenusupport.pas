ea4340a1aeeb0aa3a9991b32698ac202013decab
Denis Bisson <16710801+denis-bisson@users.noreply.github.com>
2019-01-01 21:50:17 +0000

    FIX: Add unit "uvariablemenusupport" in project files list.

--- src/uvariablemenusupport.pas
+++ src/uvariablemenusupport.pas
@@ -3,7 +3,7 @@
     -------------------------------------------------------------------------
     Support for popup menu to help to enter variable parameters.
 
-	Copyright (C) 2015-2017  Alexander Koblov (alexx2000@mail.ru)
+    Copyright (C) 2015-2018  Alexander Koblov (alexx2000@mail.ru)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,8 +16,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 }
 
 unit uVariableMenuSupport;
