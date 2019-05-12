c0f7413b42103a8a94ede8f97f1c0bb54f982fa8
alexx2000 <alexx2000@10bb5c37-c6d6-4c74-9a2a-30fc9b6e22f7>
2019-05-12 13:00:39 +0000

    ADD: Multi Rename -  allow counter start value after C, e.g. C12

--- trunk/src/fmultirename.pas
+++ trunk/src/fmultirename.pas
@@ -825,7 +825,7 @@ end;
 function TfrmMultiRename.sHandleFormatString(const sFormatStr: string; ItemNr: Integer): string;
 var
   aFile: TFile;
-  Index: Integer;
+  Index: Int64;
   Counter: Int64;
   Dirs: TStringArray;
 begin
@@ -848,8 +848,10 @@ begin
         end;
       'C':
         begin
-          Counter := StrToInt64Def(edPoc.Text, 1) +
-                     StrToInt64Def(edInterval.Text, 1) * ItemNr;
+          // Check for start value after C, e.g. C12
+          if not TryStrToInt64(Copy(sFormatStr, 2, MaxInt), Index) then
+            Index := StrToInt64Def(edPoc.Text, 1);
+          Counter := Index + StrToInt64Def(edInterval.Text, 1) * ItemNr;
           Result := Format('%.' + cmbxWidth.Items[cmbxWidth.ItemIndex] + 'd', [Counter]);
         end;
       'A':  // full path
