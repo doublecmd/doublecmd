92632a443f7a8002bbb20d0d84ddc27cf8ab8c0e
Alexander Koblov <alexx2000@mail.ru>
2019-09-18 18:02:57 +0000

    UPD: Reset elevate before CreateHardLink and CreateSymLink action

--- src/fhardlink.pas
+++ src/fhardlink.pas
@@ -69,6 +69,8 @@ begin
   sSrc := GetAbsoluteFileName(FCurrentPath, sSrc);
   sDst := GetAbsoluteFileName(FCurrentPath, sDst);
 
+  ElevateAction:= False;
+
   if CreateHardLinkUAC(sSrc, sDst) then
     begin
       // write log
