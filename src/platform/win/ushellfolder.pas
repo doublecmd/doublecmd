53373374f58075da8c018c7cceddb2f232d1df6c
Alexander Koblov <alexx2000@mail.ru>
2020-04-05 16:25:28 +0000

    FIX: Get Windows network computers listing without SMB1

---
+++ src/platform/win/ushellfolder.pas
@@ -0,0 +1,56 @@
+unit uShellFolder;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Windows, ShlObj, ActiveX, ComObj;
+
+function GetDisplayName(AFolder: IShellFolder; PIDL: PItemIDList;
+                        Flags: DWORD): String;
+
+implementation
+
+uses
+  DCConvertEncoding;
+
+function StrRetToString(PIDL: PItemIDList; StrRet: TStrRet): String;
+var
+  P: PWideChar;
+  S: UnicodeString;
+begin
+  case StrRet.uType of
+    STRRET_CSTR:
+      Result := CeSysToUtf8(StrRet.cStr);
+    STRRET_WSTR:
+      begin
+        if (StrRet.pOleStr = nil) then
+          Result := EmptyStr
+        else
+          Result := UTF8Encode(UnicodeString(StrRet.pOleStr));
+      end;
+    STRRET_OFFSET:
+      begin
+        P := PWideChar(@PIDL^.mkid.abID[StrRet.uOffset - SizeOf(PIDL^.mkid.cb)]);
+        SetString(S, P, PIDL^.mkid.cb - StrRet.uOffset);
+        Result:= UTF8Encode(S);
+      end;
+  end;
+end;
+
+function GetDisplayName(AFolder: IShellFolder; PIDL: PItemIDList;
+                        Flags: DWORD): String;
+var
+  StrRet: TStrRet;
+begin
+  Result:= EmptyStr;
+  StrRet:= Default(TStrRet);
+  if Succeeded(AFolder.GetDisplayNameOf(PIDL, Flags, StrRet)) then
+    Result := StrRetToString(PIDL, StrRet);
+  if (Length(Result) = 0) and (Flags <> SHGDN_NORMAL) then
+    Result := GetDisplayName(AFolder, PIDL, SHGDN_NORMAL);
+end;
+
+end.
+
