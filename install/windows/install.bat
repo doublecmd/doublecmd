38fd19d8140158e52347d0658d31135e81c0b7df
Alexander Koblov <alexx2000@mail.ru>
2020-11-04 14:12:31 +0000

    UPD: Plugins output directory

--- install/windows/install.bat
+++ install/windows/install.bat
@@ -40,15 +40,15 @@ copy *.dll                          %DC_INSTALL_DIR%\
 
 rem copy plugins
 rem WCX
-copy  plugins\wcx\rpm\lib\rpm.wcx         %DC_INSTALL_DIR%\plugins\wcx\rpm\
+copy  plugins\wcx\rpm\rpm.wcx             %DC_INSTALL_DIR%\plugins\wcx\rpm\
 copy  plugins\wcx\sevenzip\sevenzip.wcx   %DC_INSTALL_DIR%\plugins\wcx\sevenzip\
 copy  plugins\wcx\unrar\unrar.wcx         %DC_INSTALL_DIR%\plugins\wcx\unrar\
 xcopy /E plugins\wcx\unrar\language       %DC_INSTALL_DIR%\plugins\wcx\unrar\language\
 copy  plugins\wcx\zip\zip.wcx             %DC_INSTALL_DIR%\plugins\wcx\zip\
 xcopy /E plugins\wcx\zip\language         %DC_INSTALL_DIR%\plugins\wcx\zip\language\
 rem WDX
-copy  plugins\wdx\rpm_wdx\lib\rpm_wdx.wdx %DC_INSTALL_DIR%\plugins\wdx\rpm_wdx\
-copy  plugins\wdx\deb_wdx\lib\deb_wdx.wdx %DC_INSTALL_DIR%\plugins\wdx\deb_wdx\
+copy  plugins\wdx\rpm_wdx\rpm_wdx.wdx     %DC_INSTALL_DIR%\plugins\wdx\rpm_wdx\
+copy  plugins\wdx\deb_wdx\deb_wdx.wdx     %DC_INSTALL_DIR%\plugins\wdx\deb_wdx\
 copy  plugins\wdx\scripts\*               %DC_INSTALL_DIR%\plugins\wdx\scripts\
 copy  plugins\wdx\audioinfo\audioinfo.wdx %DC_INSTALL_DIR%\plugins\wdx\audioinfo\
 copy  plugins\wdx\audioinfo\audioinfo.lng %DC_INSTALL_DIR%\plugins\wdx\audioinfo\
