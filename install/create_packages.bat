dcc6f84258a6ac5693bbff2ecfd7a67f88d9407f
Alexander Koblov <alexx2000@mail.ru>
2021-09-25 23:38:13 +0300

    FIX: Script create_packages.bat (use git)

--- install/create_packages.bat
+++ install/create_packages.bat
@@ -3,7 +3,7 @@ rem Set Double Commander version
 set DC_VER=1.0.0
 
 rem Path to subversion
-set SVN_EXE="c:\Program Files\SlikSvn\bin\svn.exe"
+set GIT_EXE="C:\Program Files\Git\bin\git.exe"
 
 rem Path to Inno Setup compiler
 set ISCC_EXE="c:\Program Files\Inno Setup 5\ISCC.exe"
@@ -17,20 +17,8 @@ set PACK_DIR=%CD%\windows\release
 rem Create temp dir for building
 set BUILD_DC_TMP_DIR=%TEMP%\doublecmd-%DC_VER%
 rm -rf %BUILD_DC_TMP_DIR%
-%SVN_EXE% export ..\ %BUILD_DC_TMP_DIR%
-
-rem Save revision number
-mkdir %BUILD_DC_TMP_DIR%\.svn
-copy ..\.svn\entries %BUILD_DC_TMP_DIR%\.svn\
-
-rem Prepare package build dir
-rm -rf %BUILD_PACK_DIR%
-mkdir %BUILD_PACK_DIR%
-mkdir %BUILD_PACK_DIR%\release
-
-rem Copy needed files
-copy windows\doublecmd.iss %BUILD_PACK_DIR%\
-copy windows\portable.diff %BUILD_PACK_DIR%\
+mkdir %BUILD_DC_TMP_DIR%
+%GIT_EXE% -C ..\ checkout-index -a -f --prefix=%BUILD_DC_TMP_DIR%\
 
 rem Get processor architecture
 if "%CPU_TARGET%" == "" (
@@ -42,6 +30,19 @@ if "%CPU_TARGET%" == "" (
     set OS_TARGET=win64
   )
 )
+
+rem Save revision number
+set OUT=..\units\%CPU_TARGET%-%OS_TARGET%-win32
+call ..\src\platform\git2revisioninc.exe.cmd %OUT%
+copy %OUT%\dcrevision.inc %BUILD_DC_TMP_DIR%\units\
+
+rem Prepare package build dir
+rm -rf %BUILD_PACK_DIR%
+mkdir %BUILD_PACK_DIR%
+mkdir %BUILD_PACK_DIR%\release
+
+rem Copy needed files
+copy windows\doublecmd.iss %BUILD_PACK_DIR%\
 
 rem Copy libraries
 copy windows\lib\%CPU_TARGET%\*.dll    %BUILD_DC_TMP_DIR%\
@@ -63,15 +64,7 @@ move release\*.exe %PACK_DIR%
 
 rem Create *.zip package
 copy NUL doublecmd\doublecmd.inf
-zip -9 -Dr %PACK_DIR%\doublecmd-%DC_VER%.%CPU_TARGET%-%OS_TARGET%.zip doublecmd 
-
-rem Create help packages
-cd /D %BUILD_DC_TMP_DIR%
-rem Copy help files
-call %BUILD_DC_TMP_DIR%\install\windows\install-help.bat
-rem Create help package for each language
-cd %BUILD_PACK_DIR%\doublecmd
-for /D %%f in (doc\*) do zip -9 -Dr %PACK_DIR%\doublecmd-help-%%~nf-%DC_VER%.noarch.zip %%f
+zip -9 -Dr %PACK_DIR%\doublecmd-%DC_VER%.%CPU_TARGET%-%OS_TARGET%.zip doublecmd
 
 rem Clean temp directories
 cd \
