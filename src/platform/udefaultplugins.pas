e38d7eeadf9358de93e627c5e90fbcd698a9b761
Alexander Koblov <alexx2000@mail.ru>
2021-10-16 12:47:07 +0300

    UPD: Default plugins

--- src/platform/udefaultplugins.pas
+++ src/platform/udefaultplugins.pas
@@ -162,6 +162,10 @@ begin
   if I < 0 then
     gWCXPlugins.Add('xz', 91, Folder + 'zip' + PathDelim + 'zip.wcx');
 
+  I:= gWCXPlugins.IndexOfName('zst');
+  if I < 0 then
+    gWCXPlugins.Add('zst', 91, Folder + 'zip' + PathDelim + 'zip.wcx');
+
   I:= gWCXPlugins.IndexOfName('txz');
   if I < 0 then
     gWCXPlugins.Add('txz', 95, Folder + 'zip' + PathDelim + 'zip.wcx');
@@ -278,12 +282,8 @@ begin
   I:= gWCXPlugins.IndexOfName('rar');
   if I < 0 then
     gWCXPlugins.Add('rar', 607, Folder + 'unrar' + PathDelim + 'unrar.wcx')
-  {$IF DEFINED(MSWINDOWS)}
   else
     gWCXPlugins.Flags[I]:= 607;
-  {$ELSE}
-  ;
-  {$ENDIF}
 
   // Wdx plugins
   Folder:= '%commander_path%' + PathDelim + 'plugins' + PathDelim + 'wdx' + PathDelim;
