4d4d8d992a8529d8b1f0c50ab7ea344d669adb9c
Alexander Koblov <alexx2000@mail.ru>
2021-12-15 21:45:06 +0300

    FIX: WfxPlugins - copy out file with invalid time (Linux)

--- src/filesources/wfxplugin/uwfxpluginutil.pas
+++ src/filesources/wfxplugin/uwfxpluginutil.pas
@@ -119,18 +119,18 @@ uses
 
 function WfxRenameFile(aFileSource: IWfxPluginFileSource; const aFile: TFile; const NewFileName: String): Boolean;
 var
+  ASize: Int64;
   RemoteInfo: TRemoteInfo;
-  iTemp: TInt64Rec;
 begin
   with aFileSource do
   begin
     with RemoteInfo do
     begin
-      iTemp.Value := (aFile.Properties[fpSize] as TFileSizeProperty).Value;
-      SizeLow := iTemp.Low;
-      SizeHigh := iTemp.High;
-      LastWriteTime := DateTimeToWfxFileTime((aFile.Properties[fpModificationTime] as TFileModificationDateTimeProperty).Value);
-      Attr := LongInt((aFile.Properties[fpAttributes] as TFileAttributesProperty).Value);
+      ASize := aFile.Size;
+      Attr := LongInt(aFile.Attributes);
+      SizeLow := LongInt(Int64Rec(ASize).Lo);
+      SizeHigh := LongInt(Int64Rec(ASize).Hi);
+      LastWriteTime := DateTimeToWfxFileTime(aFile.ModificationTime);
     end;
     Result := (WfxCopyMove(aFile.Path + aFile.Name, aFile.Path + NewFileName, FS_COPYFLAGS_MOVE, @RemoteInfo, True, True) = FS_FILE_OK);
   end;
@@ -138,7 +138,7 @@ end;
 
 function WfxFileTimeToDateTime(FileTime: TWfxFileTime): TDateTime;
 const
-  NULL_DATE_TIME = TDateTime(1.7e+308);
+  NULL_DATE_TIME = TDateTime(2958466.0);
 begin
   if (FileTime.dwLowDateTime = $FFFFFFFE) and (FileTime.dwHighDateTime = $FFFFFFFF) then
     Result:= NULL_DATE_TIME
@@ -150,7 +150,12 @@ end;
 
 function DateTimeToWfxFileTime(DateTime: TDateTime): TWfxFileTime;
 begin
-  Result:= TWfxFileTime(DateTimeToWinFileTime(DateTime));
+  if (DateTime <= SysUtils.MaxDateTime) then
+    Result:= TWfxFileTime(DateTimeToWinFileTime(DateTime))
+  else begin
+    Result.dwLowDateTime:= $FFFFFFFE;
+    Result.dwHighDateTime:= $FFFFFFFF;
+  end;
 end;
 
 { TWfxTreeBuilder }
