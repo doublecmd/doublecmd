404518d428e509b46b24f04946d5408c31bf271d
rich2014 <rich2014.git@outlook.com>
2022-12-22 23:49:16 +0800

    FIX: COCOA: cursor in MainControl(Grid) may be always in waiting state (fixes #699)

--- src/fileviews/ufileviewwithmainctrl.pas
+++ src/fileviews/ufileviewwithmainctrl.pas
@@ -36,7 +36,11 @@ uses
   uFileView,
   uDragDropEx,
   uFileViewNotebook,
-  uDebug;
+  uDebug
+{$IFDEF LCLCOCOA}
+  ,uMyDarwin
+{$ENDIF}
+  ;
 
 type
 
@@ -1630,6 +1634,9 @@ procedure TFileViewWithMainCtrl.WorkerFinished(const Worker: TFileViewWorker);
 begin
   inherited WorkerFinished(Worker);
   MainControl.Cursor := crDefault;
+  {$IFDEF LCLCOCOA}
+  cocoaInvalidControlCursor( MainControl );
+  {$ENDIF}
   // Update status line only
   if not (csDestroying in ComponentState) then UpdateInfoPanel;
 end;
@@ -1798,6 +1805,9 @@ procedure TFileViewWithMainCtrl.WorkerStarting(const Worker: TFileViewWorker);
 begin
   inherited WorkerStarting(Worker);
   MainControl.Cursor := crHourGlass;
+  {$IFDEF LCLCOCOA}
+  cocoaInvalidControlCursor( MainControl );
+  {$ENDIF}
   UpdateInfoPanel; // Update status line only
 end;
 
