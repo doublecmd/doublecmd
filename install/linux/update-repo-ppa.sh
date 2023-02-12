adce914beba3d845ebe7c1bef1844e371d427a14
Alexander Koblov <alexx2000@mail.ru>
2023-02-12 19:58:17 +0300

    UPD: Version number

--- install/linux/update-repo-ppa.sh
+++ install/linux/update-repo-ppa.sh
@@ -3,7 +3,7 @@
 # This script updates Double Commander Personal Package Archive (PPA) repository
 
 # Set Double Commander version
-DC_VER=1.0.10
+DC_VER=1.0.11
 # Set Ubuntu series
 DISTRO=( xenial zesty artful )
 
