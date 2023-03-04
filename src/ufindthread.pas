7490fb959d8c0a371510d5549dac0b0480b57fe7
Alexander Koblov <alexx2000@mail.ru>
2023-03-04 18:13:56 +0300

    FIX: Find files - find text in the drive root (issue #839)
    (cherry picked from commit 25ec33e563accc486dab84145eb507eebdc2a701)

--- src/ufindthread.pas
+++ src/ufindthread.pas
@@ -737,7 +737,7 @@ var
   end;
 
 begin
-  AFileName:= Folder + PathDelim + sr.Name;
+  AFileName:= IncludeTrailingBackslash(Folder) + sr.Name;
 
   if (FPS_ISDIR(sr.Attr) or FileIsLinkToDirectory(AFileName, sr.Attr)) then
     Exit(False);
@@ -836,10 +836,10 @@ begin
         Exit(False);
 
       try
-        Result := FindInFile(Folder + PathDelim + sr.Name, FindText, CaseSensitive, TextRegExp);
+        Result := FindInFile(IncludeTrailingBackslash(Folder) + sr.Name, FindText, CaseSensitive, TextRegExp);
 
         if (Result and IsReplaceText) then
-          FileReplaceString(Folder + PathDelim + sr.Name, FindText, ReplaceText, CaseSensitive, TextRegExp);
+          FileReplaceString(IncludeTrailingBackslash(Folder) + sr.Name, FindText, ReplaceText, CaseSensitive, TextRegExp);
 
         if NotContainingText then
           Result := not Result;
@@ -851,7 +851,7 @@ begin
           if (log_errors in gLogOptions) then
           begin
             logWrite(Self, rsMsgLogError + E.Message + ' (' +
-                     Folder + PathDelim + sr.Name + ')', lmtError);
+                     IncludeTrailingBackslash(Folder) + sr.Name + ')', lmtError);
           end;
         end;
       end;
