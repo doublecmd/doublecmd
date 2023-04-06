344413a64e9a616f3b6e4e36b58c8a22b015a677
rich2014 <rich2014.git@outlook.com>
2023-04-06 23:12:41 +0800

    UPD: DARWIN: unmount driver

--- src/platform/unix/darwin/umydarwin.pas
+++ src/platform/unix/darwin/umydarwin.pas
@@ -3,7 +3,7 @@
    -------------------------------------------------------------------------
    This unit contains specific DARWIN functions.
 
-   Copyright (C) 2016-2021 Alexander Koblov (alexx2000@mail.ru)
+   Copyright (C) 2016-2023 Alexander Koblov (alexx2000@mail.ru)
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -54,6 +54,8 @@ function NSGetFolderPath(Folder: NSSearchPathDirectory): String;
 
 function GetFileDescription(const FileName: String): String;
 function MountNetworkDrive(const serverAddress: String): Boolean;
+
+function unmountAndEject(const path: String): Boolean;
 
 // Workarounds for FPC RTL Bug
 // copied from ptypes.inc and modified fstypename only
@@ -343,6 +345,10 @@ begin
   CFRelease(FileNameRef);
 end;
 
+function unmountAndEject(const path: String): Boolean;
+begin
+  Result:= NSWorkspace.sharedWorkspace.unmountAndEjectDeviceAtPath( StringToNSString(path) );
+end;
 
 procedure cocoaInvalidControlCursor( const control:TWinControl );
 var
