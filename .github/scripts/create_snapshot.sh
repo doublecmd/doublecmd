d1d603bcdf2684b5643e537de2b6a6e7d9a49989
Alexander Koblov <alexx2000@mail.ru>
2023-08-18 22:02:06 +0300

    ADD: Build snapshots with unrar library

--- .github/scripts/create_snapshot.sh
+++ .github/scripts/create_snapshot.sh
@@ -15,6 +15,13 @@ DC_MINOR=$(grep 'MinorVersionNr' src/doublecmd.lpi | grep -o '[0-9.]\+' || echo 
 DC_MICRO=$(grep 'RevisionNr' src/doublecmd.lpi | grep -o '[0-9.]\+' || echo 0)
 DC_VER=$DC_MAJOR.$DC_MINOR.$DC_MICRO
 
+# Get libraries
+pushd install
+wget https://github.com/doublecmd/snapshots/raw/main/darwin.tar.gz
+tar xzf darwin.tar.gz
+rm -f darwin.tar.gz
+popd
+
 # Set widgetset
 export lcl=cocoa
 
