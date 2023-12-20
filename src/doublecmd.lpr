doublecmd/doublecmd

commit 919f3a4e1893e63541a4632608b8a7ae34eb7771

UPD: Initialize error mode early (issue #473)

src/doublecmd.lpr
@@ -5,6 +5,7 @@ program doublecmd;
 {$ENDIF}
 
 uses
+  uSystem,
   {$IFDEF MSWINDOWS}
   uElevation,
   {$IFDEF LCLQT5}
@@ -106,13 +107,13 @@ var
 {$ENDIF}
 
 begin
+  // Initialize again
+  uSystem.Initialize;
+
   DCDebug('Starting Double Commander');
 
   // Initialize random number generator
   Randomize;
-
-  // Disable invalid floating point operation exception
-  SetExceptionMask(GetExceptionMask + [exInvalidOp, exZeroDivide]);
 
   {$IF DEFINED(NIGHTLY_BUILD)}
   InitLineInfo;
@@ -126,13 +127,10 @@ begin
   {$ENDIF}
 
   {$IFDEF MSWINDOWS}
-  uMyWindows.InitErrorMode;
   uMyWindows.FixCommandLineToUTF8;
   {$ENDIF}
 
-  {$if lcl_fullversion >= 1070000}
   Application.Scaled:= True;
-  {$endif}
 
   // Fix default BidiMode
   // see http://bugs.freepascal.org/view.php?id=22044
@@ -160,14 +158,12 @@ begin
 
   // Use only current directory separator
   AllowDirectorySeparators:= [DirectorySeparator];
-  {$IF lcl_fullversion >= 093100}
   // Disable because we set a few of our own format settings and we don't want
   // them to be changed. There's no way currently to react to Application.IntfSettingsChange.
   // If in future we move to a Unicode RTL this could be removed.
   {$PUSH}{$WARN SYMBOL_PLATFORM OFF}
   Application.UpdateFormatSettings := False;
   {$POP}
-  {$ENDIF}
   DefaultFormatSettings.ThousandSeparator:= ' ';
   {$IFDEF UNIX}
   uMyUnix.FixDateTimeSeparators;
@@ -223,7 +219,7 @@ begin
       Application.CreateForm(TdmComData, dmComData); // common data
       Application.CreateForm(TdmHelpManager, dmHelpMgr); // help manager
 
-      {$IF DEFINED(LCLGTK2) AND (lcl_fullversion >= 093100)}
+      {$IF DEFINED(LCLGTK2)}
       // LCLGTK2 uses Application.MainForm as the clipboard widget, however our
       // MainForm is TfrmHackForm and it never gets realized. GTK2 doesn't
       // seem to allow a not realized widget to have clipboard ownership.

src/platform/usystem.pas
@@ -1,0 +1,31 @@
+unit uSystem;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Math
+{$IF DEFINED(MSWINDOWS)}
+  , Windows
+{$ENDIF}
+  ;
+
+procedure Initialize;
+
+implementation
+
+procedure Initialize;
+begin
+  // Disable invalid floating point operation exception
+  SetExceptionMask(GetExceptionMask + [exInvalidOp, exZeroDivide]);
+{$IF DEFINED(MSWINDOWS)}
+  SetErrorMode(SEM_FAILCRITICALERRORS or SEM_NOOPENFILEERRORBOX);
+{$ENDIF}
+end;
+
+initialization
+  Initialize;
+
+end.
+

src/platform/win/umywindows.pas
@@ -176,8 +176,6 @@ procedure CreateShortcut(const Target, Shortcut: String);
 }
 function ExtractFileAttributes(const FindData: TWin32FindDataW): DWORD;
 
-procedure InitErrorMode;
-
 procedure UpdateEnvironment;
 
 procedure FixCommandLineToUTF8;
@@ -1130,11 +1128,6 @@ begin
     Result:= FindData.dwFileAttributes - FILE_ATTRIBUTE_REPARSE_POINT
   else
     Result:= FindData.dwFileAttributes;
-end;
-
-procedure InitErrorMode;
-begin
-  SetErrorMode(SEM_FAILCRITICALERRORS or SEM_NOOPENFILEERRORBOX);
 end;
 
 procedure UpdateEnvironment;

commit 2e5b4845de7f184691e9cd953ae2b05260f7fd43

Fix: crash on startup, introduced by 919f3a4, cthreads/cmem should be the first unit

src/doublecmd.lpr
@@ -5,7 +5,6 @@ program doublecmd;
 {$ENDIF}
 
 uses
-  uSystem,
   {$IFDEF MSWINDOWS}
   uElevation,
   {$IFDEF LCLQT5}
@@ -41,6 +40,7 @@ uses
   uQt5Workaround,
   {$ENDIF}
   {$ENDIF}
+  uSystem,
   uMoveConfig,
   uEarlyConfig,
   DCConvertEncoding,