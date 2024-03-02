1489dc73c77c562f9bf975f09618e1659ef941c3
Graham Inggs <ginggs@debian.org>
2024-03-02 12:43:26 -0100

    Update clean.sh
    Clean temporary files plugins/wcx/unrar/lib/rarlng.rsj and plugins/wfx/ftp/lib/ftppropdlg.lfm

--- clean.sh
+++ clean.sh
@@ -27,10 +27,12 @@ find plugins -iname '*.a'  -delete
 rm -f src/doublecmd.res doublecmd
 rm -f tools/extractdwrflnfo
 rm -f plugins/wcx/unrar/lib/rarconfdlg.lfm
+rm -f plugins/wcx/unrar/lib/rarlng.rsj
 rm -f plugins/wcx/zip/lib/ZipConfDlg.lfm
 rm -f plugins/wcx/zip/lib/ZipLng.rsj
 rm -f plugins/wcx/zip/lib/abresstring.rs?
 rm -f plugins/wfx/ftp/lib/FtpConfDlg.lfm
+rm -f plugins/wfx/ftp/lib/ftppropdlg.lfm
 rm -f plugins/wfx/samba/lib/smbauthdlg.lfm
 
 # Remove debug files
