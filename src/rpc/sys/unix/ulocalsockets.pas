2626a317f0b880d42e47b53c9c6405cec3678c3b
Alexander Koblov <alexx2000@mail.ru>
2025-01-12 13:43:04 +0300

    FIX: Unable to make a privileged operation if app's executable has non-default file name in linux (fixes #2071)
    (cherry picked from commit 4fc93d275c5494ea5655295950f65ca43ec51422)

--- src/rpc/sys/unix/ulocalsockets.pas
+++ src/rpc/sys/unix/ulocalsockets.pas
@@ -366,7 +366,7 @@ begin
   if UserID = 0 then begin
     UserID:= GetProcessUserId(StrToInt(ParamStr(2)));
   end;
-  Result:= '/tmp/' + ApplicationName + '-' + IntToStr(UserID);
+  Result:= '/tmp/doublecmd' + '-' + IntToStr(UserID);
   // Verify directory owner
   if not DirectoryExists(Result) then
   begin
