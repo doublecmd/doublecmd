05f3ab844659d8879a5835bc4b752d0afdd51b9b
Alexander Koblov <alexx2000@mail.ru>
2025-01-17 18:34:01 +0300

    ADD: CloseRange function

--- components/doublecmd/dcdarwin.pas
+++ components/doublecmd/dcdarwin.pas
@@ -1,17 +1,72 @@
 unit DCDarwin;
 
 {$mode delphi}
+{$packrecords c}
+{$pointermath on}
 {$modeswitch objectivec1}
 
 interface
 
 uses
-  Classes, SysUtils, DCBasicTypes, CocoaAll;
+  Classes, SysUtils, DCBasicTypes, CocoaAll, BaseUnix;
+
+const
+  CLOSE_RANGE_CLOEXEC = (1 << 2);
+
+function CloseRange(first: cuint; last: cuint; flags: cint): cint; cdecl;
 
 // MacOS File Utils
 function MacosFileSetCreationTime( const path:String; const birthtime:TFileTimeEx ): Boolean;
 
 implementation
+
+uses
+  DCUnix;
+
+type
+  proc_fdinfo = record
+    proc_fd: Int32;
+    proc_fdtype: UInt32;
+  end;
+  Pproc_fdinfo = ^proc_fdinfo;
+
+const
+  PROC_PIDLISTFDS = 1;
+  PROC_PIDLISTFD_SIZE = SizeOf(proc_fdinfo);
+
+function proc_pidinfo(pid: cint; flavor: cint; arg: cuint64; buffer: pointer; buffersize: cint): cint; cdecl; external 'proc';
+
+function CloseRange(first: cuint; last: cuint; flags: cint): cint; cdecl;
+var
+  I: cint;
+  Handle: cint;
+  ProcessId: TPid;
+  bufferSize: cint;
+  pidInfo: Pproc_fdinfo;
+begin
+  Result:= -1;
+  ProcessId:= FpGetpid;
+  bufferSize:= proc_pidinfo(ProcessId, PROC_PIDLISTFDS, 0, nil, 0);
+  pidInfo:= GetMem(bufferSize);
+  if Assigned(pidInfo) then
+  begin
+    bufferSize:= proc_pidinfo(ProcessId, PROC_PIDLISTFDS, 0, pidInfo, bufferSize);
+    for I:= 0 to (bufferSize div PROC_PIDLISTFD_SIZE) - 1 do
+    begin
+      Handle:= pidInfo[I].proc_fd;
+      if (Handle >= first) and (Handle <= last) then
+      begin
+        if (flags and CLOSE_RANGE_CLOEXEC <> 0) then
+          FileCloseOnExec(Handle)
+        else begin
+          FileClose(Handle);
+        end;
+      end;
+    end;
+    Result:= 0;
+    FreeMem(pidInfo);
+  end;
+end;
 
 function StringToNSString(const S: String): NSString;
 begin
