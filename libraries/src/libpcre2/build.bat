5ca7cc68c275e0861a8561bb87159e4d0c984b8b
Alexander Koblov <alexx2000@mail.ru>
2025-03-26 20:50:39 +0300

    UPD: libpcre2 build script

--- libraries/src/libpcre2/build.bat
+++ libraries/src/libpcre2/build.bat
@@ -1,4 +1,8 @@
+set PATH=W:\Prog\cmake-3.16.2-win32-x86\bin;W:\Prog\MinGW\mingw64\bin;%PATH%
+
 rem Build libpcre2 library
 
 cmake -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=ON -DPCRE2_NEWLINE=CRLF
 mingw32-make
+
+strip --strip-debug --strip-unneeded libpcre2-8.dll
