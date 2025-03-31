9fabdb6e990bc9a79a3d964f49e1d664224496ea
Alexander Koblov <alexx2000@mail.ru>
2025-03-31 20:58:50 +0300

    FIX: Enable resource sharing between the OpenGL contexts (needs by plugins)
    (cherry picked from commit caf1d3f0800b7a7142df6c4eb4d9b170f2123833)

--- src/platform/usystem.pas
+++ src/platform/usystem.pas
@@ -9,6 +9,9 @@ uses
 {$IF DEFINED(MSWINDOWS)}
   , Windows
 {$ENDIF}
+{$IF DEFINED(LCLQT6)}
+  , Qt6
+{$ENDIF}
   ;
 
 procedure Initialize;
@@ -21,6 +24,9 @@ begin
   SetExceptionMask(GetExceptionMask + [exInvalidOp, exZeroDivide]);
 {$IF DEFINED(MSWINDOWS)}
   SetErrorMode(SEM_FAILCRITICALERRORS or SEM_NOOPENFILEERRORBOX);
+{$ENDIF}
+{$IF DEFINED(LCLQT6)}
+  QCoreApplication_setAttribute(QtAA_ShareOpenGLContexts, True);
 {$ENDIF}
 end;
 
