b8fc7d03bb6d818b14b38c66fc000a1ac7cf53f1
Alexander Koblov <alexx2000@mail.ru>
2025-06-01 01:04:14 +0300

    FIX: Windows XP compatibility
    (cherry picked from commit 1af6bd6f7d8d319762bcf5b98a0e2aedbb259ddc)

--- src/platform/upixmapmanager.pas
+++ src/platform/upixmapmanager.pas
@@ -2732,11 +2732,11 @@ procedure LoadPixMapManager;
 var
   Q: QWord;
 begin
-  Q:= GetTickCount64;
+  Q:= SysUtils.GetTickCount64;
   DCDebug('Creating PixmapManager');
   PixMapManager:=TPixMapManager.Create;
   PixMapManager.Load(gpCfgDir + 'pixmaps.txt');
-  DCDebug('Creating PixmapManager done '+ IntToStr(GetTickCount64 - Q));
+  DCDebug('Creating PixmapManager done '+ IntToStr(SysUtils.GetTickCount64 - Q));
 end;
 
 initialization
