25aa9b6549ff8821d598ab507a820f62aade4051
Alexander Koblov <alexx2000@mail.ru>
2025-07-16 21:36:39 +0300

    UPD: Use Wayland under Qt6 >= 6.9.1

--- src/platform/unix/uqtworkaround.pas
+++ src/platform/unix/uqtworkaround.pas
@@ -7,7 +7,11 @@ interface
 implementation
 
 uses
-  InitC, BaseUnix, LCLVersion;
+  InitC, BaseUnix, LCLVersion
+{$IF DEFINED(LCLQT6)}
+  , SysUtils, qt6
+{$ENDIF}
+  ;
 
 {$IF DEFINED(LCLQT5)}
 procedure _exit(status: cint); cdecl; external clib;
@@ -16,10 +20,28 @@ function setenv(const name, value: pchar; overwrite: cint): cint; cdecl; externa
 {$ENDIF}
 
 {$IF DEFINED(LCLQT6)}
+var
+  Minor, Micro: Integer;
+  AVersion: TStringArray;
 initialization
   // Workaround: https://github.com/doublecmd/doublecmd/issues/2290
   if (LowerCase(fpGetEnv(PAnsiChar('XDG_SESSION_TYPE'))) = 'wayland') then
-    setenv('QT_QPA_PLATFORM', 'xcb', 1);
+  begin
+    AVersion:= StrPas(QtVersion).Split(['.']);
+    if (Length(AVersion) > 1) then
+    begin
+      Minor:= StrToIntDef(AVersion[1], 0);
+      if (Length(AVersion) > 2) then
+        Micro:= StrToIntDef(AVersion[2], 0)
+      else begin
+        Micro:= 0;
+      end;
+    end;
+    if (Minor < 9) or ((Minor = 9) and (Micro = 0)) then
+    begin
+      setenv('QT_QPA_PLATFORM', 'xcb', 1);
+    end;
+  end;
 {$ENDIF}
 
 {$IF DEFINED(LCLQT5)}
