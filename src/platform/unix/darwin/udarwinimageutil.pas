babc01608cc272bec23e706717b9435fa46159f0
rich2014 <rich2014.git@outlook.com>
2025-12-08 22:47:38 +0800

    ADD: uDarwinImageUtil on macOS

---
+++ src/platform/unix/darwin/udarwinimageutil.pas
@@ -0,0 +1,51 @@
+unit uDarwinImageUtil;
+
+{$mode delphi}
+{$modeswitch objectivec2}
+
+interface
+
+uses
+  Classes, SysUtils,
+  CocoaAll, CocoaUtils;
+
+type
+  
+  { TDarwinImageUtil }
+
+  TDarwinImageUtil = class
+  public
+    class function filt( const filterName: NSString; const sourceImage: NSImage ): NSImage;
+    class function invertColor( const sourceImage: NSImage ): NSImage;
+  end;
+
+implementation
+
+class function TDarwinImageUtil.filt(
+  const filterName: NSString;
+  const sourceImage: NSImage ): NSImage;
+var
+  tiffData: NSData;
+  inputImage: CIImage;
+  outputImage: CIImage;
+  filter: CIFilter;
+  rep: NSCIImageRep;
+begin
+  tiffData:= sourceImage.TIFFRepresentation;
+  inputImage:= CIImage.imageWithData( tiffData );
+  filter:= CIFilter.filterWithName( filterName );
+  filter.setValue_forKey( inputImage, kCIInputImageKey );
+  outputImage:= filter.valueForKey( kCIOutputImageKey );
+  rep:= NSCIImageRep.imageRepWithCIImage( outputImage );
+  Result:= NSImage.alloc.initWithSize( sourceImage.size );
+  Result.addRepresentation( rep );
+  Result.autorelease;
+end;
+
+class function TDarwinImageUtil.invertColor( const sourceImage: NSImage ): NSImage;
+begin
+  Result:= filt( NSSTR('CIColorInvert'), sourceImage );
+end;
+
+end.
+
