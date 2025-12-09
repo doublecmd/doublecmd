7a76524516c46e1aa93360e62a27438be25b355d
rich2014 <rich2014.git@outlook.com>
2025-12-09 19:40:10 +0800

    UPD: refactoring uDarwinSimpleFSWatch from uMyDarwin

--- src/platform/unix/darwin/umydarwin.pas
+++ src/platform/unix/darwin/umydarwin.pas
@@ -100,27 +100,6 @@ type TDarwinStatfs = TStatFs;
 
 {$endif}
 
-// MacOS Simple File Sytem Watcher (only one watchPath)
-
-{ TSimpleDarwinFSWatcher }
-
-TSimpleDarwinFSWatcher = class( TThread )
-private
-  _monitor: TDarwinFSWatcher;
-  _callback: TDarwinFSWatchCallBack;
-  _event: TDarwinFSWatchEvent;
-protected
-  procedure Execute; override;
-  procedure handleEvent( event:TDarwinFSWatchEvent );
-  procedure doSyncCallback;
-public
-  procedure stop();
-  constructor Create( const path:String; const callback:TDarwinFSWatchCallBack );
-  destructor Destroy; override;
-public
-  property monitor: TDarwinFSWatcher read _monitor;
-end;
-
 type
 
   { TMacosServiceMenuHelper }
@@ -190,48 +169,6 @@ begin
   if (lclForm=nil) or (lclForm.ClassName='TfrmMain') then
     AttachEditMenu( menu, menu.numberOfItems, CocoaConst.NSSTR_EDIT_MENU );
 end;
-
-{ TSimpleDarwinFSWatcher }
-
-procedure TSimpleDarwinFSWatcher.Execute;
-begin
-  _monitor.start();
-end;
-
-procedure TSimpleDarwinFSWatcher.handleEvent( event:TDarwinFSWatchEvent );
-begin
-  _event:= event;
-  Synchronize( doSyncCallback );
-end;
-
-procedure TSimpleDarwinFSWatcher.doSyncCallback;
-begin
-  _callback( _event );
-  _event:= nil;
-end;
-
-procedure TSimpleDarwinFSWatcher.stop();
-begin
-  _monitor.terminate();
-end;
-
-constructor TSimpleDarwinFSWatcher.Create(
-  const path:String;
-  const callback:TDarwinFSWatchCallBack );
-begin
-  Inherited Create( false );
-  _callback:= callback;
-  _monitor:= TDarwinFSWatcher.create( handleEvent );
-  _monitor.addPath( path );
-end;
-
-destructor TSimpleDarwinFSWatcher.Destroy;
-begin
-  _monitor.terminate;
-  FreeAndNil( _monitor );
-  inherited;
-end;
-
 
 procedure TMacosServiceMenuHelper.attachSystemMenu(Sender: TObject);
 begin
