b75ac1e82f1af8dd7a757c58b47cc2e04a35adf7
rich2014 <rich2014.git@outlook.com>
2025-12-10 22:28:06 +0800

    UPD: refactoring Panel from uMyDarwin to uDarwinPanel

---
+++ src/platform/unix/darwin/udarwinpanel.pas
@@ -0,0 +1,219 @@
+unit uDarwinPanel;
+
+{$mode delphi}
+{$modeswitch objectivec2}
+
+interface
+
+uses
+  Classes, SysUtils,
+  Controls, Forms,
+  CocoaAll, QuickLookUI, CocoaUtils,
+  uDarwinApplication, uDarwinFinderModel, uDarwinFinder;
+
+type
+
+  { TDarwinPanelUtil }
+
+  TDarwinPanelUtil = class
+  public
+    class procedure showQuickLook;
+    class procedure showEditFinderTags( const Sender: id; const control: TWinControl );
+    class procedure showSharingService;
+    class procedure showAirDrop;
+  end;
+
+implementation
+
+type
+
+  { TQLPreviewPanelMate }
+
+  TQLPreviewPanelMate = objcclass( NSObject, QLPreviewPanelDataSourceProtocol )
+  private
+    _urlArray: NSArray;
+  public
+    function numberOfPreviewItemsInPreviewPanel (panel: QLPreviewPanel): NSInteger;
+    function previewPanel_previewItemAtIndex (panel: QLPreviewPanel; index: NSInteger): QLPreviewItemProtocol;
+  public
+    function initWithItems( urlArray: NSArray ): id; message 'setUrlArray:';
+    procedure dealloc; override;
+  end;
+
+function TQLPreviewPanelMate.numberOfPreviewItemsInPreviewPanel(
+  panel: QLPreviewPanel): NSInteger;
+begin
+  Result:= _urlArray.count;
+end;
+
+function TQLPreviewPanelMate.previewPanel_previewItemAtIndex(panel: QLPreviewPanel;
+  index: NSInteger): QLPreviewItemProtocol;
+begin
+  Result:= QLPreviewItemProtocol( _urlArray.objectAtIndex(index) );
+end;
+
+function TQLPreviewPanelMate.initWithItems(urlArray: NSArray): id;
+begin
+  Result:= Inherited init;
+  _urlArray:= urlArray;
+  _urlArray.retain;
+end;
+
+procedure TQLPreviewPanelMate.dealloc;
+begin
+  _urlArray.release;
+  Inherited;
+end;
+
+class procedure TDarwinPanelUtil.showQuickLook;
+var
+  lclArray: TStringArray;
+  mate: TQLPreviewPanelMate;
+  panel: QLPreviewPanel;
+begin
+  lclArray:= TDCCocoaApplication(NSApp).serviceMenuGetFilenames;
+  if lclArray = nil then
+    Exit;
+
+  mate:= TQLPreviewPanelMate.alloc.initWithItems( UrlArrayFromLCLToNS(lclArray) );
+  panel:= QLPreviewPanel.sharedPreviewPanel;
+  panel.setDataSource( mate );
+  panel.makeKeyAndOrderFront( nil );
+  mate.release;
+end;
+
+type
+
+  { TFinderTagsEditorPanelHandler }
+
+  TFinderTagsEditorPanelHandler = class
+  private
+    _urls: NSArray;
+    _oldTagNames: NSArray;
+  public
+    constructor Create( const paths: TStringArray );
+    destructor Destroy; override;
+    procedure onClose( const cancel: Boolean; const newTagNames: NSArray );
+  end;
+
+constructor TFinderTagsEditorPanelHandler.Create( const paths: TStringArray );
+begin
+  _urls:= UrlArrayFromLCLToNS( paths );
+  _urls.retain;
+  _oldTagNames:= uDarwinFinderModelUtil.getTagNamesOfFiles( _urls );
+  _oldTagNames.retain;
+end;
+
+destructor TFinderTagsEditorPanelHandler.Destroy;
+begin
+  _oldTagNames.release;
+  _urls.release;
+end;
+
+procedure TFinderTagsEditorPanelHandler.onClose( const cancel: Boolean; const newTagNames: NSArray );
+
+  procedure processRemovedTags;
+  var
+    removedTagNames: NSMutableSet;
+    tagName: NSString;
+  begin
+    removedTagNames:= NSMutableSet.setWithArray( _oldTagNames );
+    removedTagNames.minusSet( NSSet.setWithArray(newTagNames) );
+    for tagName in removedTagNames do begin
+      uDarwinFinderModelUtil.removeTagForFiles( _urls, tagName );
+    end;
+  end;
+
+  procedure processAddedTags;
+  var
+    addedTagNames: NSMutableSet;
+    tagName: NSString;
+  begin
+    addedTagNames:= NSMutableSet.setWithArray( newTagNames );
+    addedTagNames.minusSet( NSSet.setWithArray(_oldTagNames) );
+    for tagName in addedTagNames do begin
+      uDarwinFinderModelUtil.addTagForFiles( _urls, tagName );
+    end;
+  end;
+
+begin
+  if cancel then
+    Exit;
+  if _urls.count = 1 then begin
+    uDarwinFinderModelUtil.setTagNamesOfFile( NSURL(_urls.objectAtIndex(0)), newTagNames );
+  end else begin
+    processRemovedTags;
+    processAddedTags;
+  end;
+end;
+
+class procedure TDarwinPanelUtil.showEditFinderTags(const Sender: id;
+  const control: TWinControl);
+var
+  tagItem: NSToolBarItem absolute Sender;
+  filenames: TStringArray;
+  view: NSView;
+  handler: TFinderTagsEditorPanelHandler;
+begin
+  filenames:= TDCCocoaApplication(NSApp).serviceMenuGetFilenames;
+  if length(filenames) = 0 then
+    Exit;
+
+  view:= nil;
+  if Assigned(tagItem) then
+    view:= tagItem.valueForKey( NSSTR('_itemViewer') );
+  if (view=nil) or (view.window=nil) then
+    view:= NSView( control.Handle );
+
+  handler:= TFinderTagsEditorPanelHandler.Create( filenames );
+  uDarwinFinderUtil.popoverFileTagsEditor( filenames, handler.onClose, view , NSMaxYEdge );
+end;
+
+// Sharing Service
+class procedure TDarwinPanelUtil.showSharingService;
+var
+  picker: NSSharingServicePicker;
+  cocoaArray: NSArray;
+  lclArray: TStringArray;
+  point: TPoint;
+  popupNSRect: NSRect;
+  control: TWinControl;
+begin
+  if not TDCCocoaApplication(NSApp).serviceMenuIsReady then
+    exit;
+
+  lclArray:= TDCCocoaApplication(NSApp).serviceMenuGetFilenames;
+  if lclArray = nil then
+    Exit;
+
+  cocoaArray:= UrlArrayFromLCLToNS( lclArray );
+
+  control:= Screen.ActiveControl;
+  point:= control.ScreenToClient( Mouse.CursorPos );
+  popupNSRect.origin.x:= point.X;
+  popupNSRect.origin.y:= point.Y;
+  popupNSRect.size:= NSMakeSize( 1, 1 );
+
+  picker:= NSSharingServicePicker.alloc.initWithItems( cocoaArray );
+  picker.showRelativeToRect_ofView_preferredEdge( popupNSRect, NSView(control.handle) , NSMaxXEdge );
+  picker.release;
+end;
+
+class procedure TDarwinPanelUtil.showAirDrop;
+var
+  service: NSSharingService;
+  lclArray: TStringArray;
+  cocoaArray: NSArray;
+begin
+  lclArray:= TDCCocoaApplication(NSApp).serviceMenuGetFilenames;
+  if lclArray = nil then
+    Exit;
+
+  cocoaArray:= UrlArrayFromLCLToNS( lclArray );
+  service:= NSSharingService.sharingServiceNamed( NSSharingServiceNameSendViaAirDrop );
+  service.performWithItems( cocoaArray );
+end;
+
+
+end.
+
