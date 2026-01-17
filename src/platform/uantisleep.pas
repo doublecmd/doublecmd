88ca9678878ac90f083e57551ad8b6a29a56e179
Alexander Koblov <alexx2000@mail.ru>
2026-01-18 00:25:07 +0300

    ADD: Anti-sleep - Linux support

--- src/platform/uantisleep.pas
+++ src/platform/uantisleep.pas
@@ -13,6 +13,8 @@ implementation
 
 {$IF DEFINED(MSWINDOWS)}
 uses JwaWinNT, JwaWinBase;
+{$ELSEIF DEFINED(LINUX)}
+uses dbus, LazLogger, BaseUnix;
 {$ELSEIF DEFINED(DARWIN)}
 uses MacOSAll;
 {$ENDIF}
@@ -39,10 +41,13 @@ end;
 
 procedure EnableSleep;
 begin
-  Dec(Count);
-  if (Count = 0) then
+  if (Count > 0) then
   begin
-    SetThreadExecutionState(ES_CONTINUOUS);
+    Dec(Count);
+    if (Count = 0) then
+    begin
+      SetThreadExecutionState(ES_CONTINUOUS);
+    end;
   end;
 end;
 {$ELSEIF DEFINED(DARWIN)}
@@ -94,18 +99,163 @@ procedure EnableSleep;
 begin
   System.EnterCriticalSection(Mutex);
   try
-    Dec(Count);
-    if (Count = 0) then
+    if (Count > 0) then
     begin
-      if (AssertionID <> kIOPMNullAssertionID) then
+      Dec(Count);
+      if (Count = 0) then
       begin
-        IOPMAssertionRelease(AssertionID);
-        AssertionID:= kIOPMNullAssertionID;
+        if (AssertionID <> kIOPMNullAssertionID) then
+        begin
+          IOPMAssertionRelease(AssertionID);
+          AssertionID:= kIOPMNullAssertionID;
+        end;
       end;
     end;
   finally
     System.LeaveCriticalSection(Mutex);
   end;
+end;
+
+procedure Initialize;
+begin
+  System.InitCriticalSection(Mutex);
+end;
+
+procedure Finalize;
+begin
+  System.DoneCriticalSection(Mutex);
+end;
+{$ELSEIF DEFINED(LINUX)}
+const
+  LOGIN1_ADDRESS = 'org.freedesktop.login1';
+  LOGIN1_PATH = '/org/freedesktop/login1';
+  LOGIN1_INTERFACE = 'org.freedesktop.login1.Manager';
+
+var
+  conn: PDBusConnection = nil;
+  inhibit_fd: THandle = feInvalidHandle;
+
+var
+  Count: Integer = 0;
+  Mutex: System.TRTLCriticalSection;
+
+procedure Print(const sMessage: String);
+begin
+  DebugLn('Inhibit: ', sMessage);
+end;
+
+function CheckError(const sMessage: String; pError: PDBusError): Boolean;
+begin
+  if (dbus_error_is_set(pError) = 0) then
+    Result := False
+  else begin
+    Print(sMessage + ': ' + pError^.name + ' ' + pError^.message);
+    dbus_error_free(pError);
+    Result := True;
+  end;
+end;
+
+procedure DisableSleep;
+var
+  res: Boolean;
+  err: DBusError;
+  iter: DBusMessageIter;
+  message, reply: PDBusMessage;
+  what, who, why, mode: PAnsiChar;
+begin
+  System.EnterCriticalSection(Mutex);
+  try
+    Inc(Count);
+    if (Count = 1) then
+    begin
+      dbus_error_init(@err);
+
+      message := dbus_message_new_method_call(LOGIN1_ADDRESS, LOGIN1_PATH, LOGIN1_INTERFACE, 'Inhibit');
+
+      if message = nil then
+      begin
+        Print('Cannot create message');
+        Exit;
+      end;
+
+      try
+        what := 'idle';
+        who := 'Double Commander';
+        why := 'Operation in progress';
+        mode := 'block';
+
+        dbus_message_iter_init_append(message, @iter);
+        res:= dbus_message_iter_append_basic(@iter, DBUS_TYPE_STRING, @what) <> 0;
+        res:= res and (dbus_message_iter_append_basic(@iter, DBUS_TYPE_STRING, @who) <> 0);
+        res:= res and (dbus_message_iter_append_basic(@iter, DBUS_TYPE_STRING, @why) <> 0);
+        res:= res and (dbus_message_iter_append_basic(@iter, DBUS_TYPE_STRING, @mode) <> 0);
+
+        if not res then
+        begin
+          Print('Cannot append arguments');
+          Exit;
+        end;
+
+        reply:= dbus_connection_send_with_reply_and_block(conn, message, 1000, @err);
+
+        if CheckError('Send message', @err) then Exit;
+
+        dbus_message_iter_init(reply, @iter);
+        dbus_message_iter_get_basic(@iter, @inhibit_fd);
+        dbus_message_unref(reply);
+      finally
+        dbus_message_unref(message);
+      end;
+    end;
+  finally
+    System.LeaveCriticalSection(Mutex);
+  end;
+end;
+
+procedure EnableSleep;
+var
+  Res: Integer;
+begin
+  System.EnterCriticalSection(Mutex);
+  try
+    if (Count > 0) then
+    begin
+      Dec(Count);
+      if (Count = 0) then
+      begin
+        if inhibit_fd <> feInvalidHandle then
+        begin
+          repeat
+            Res:= fpClose(inhibit_fd);
+          until (Res <> -1) or (fpGetErrNo <> ESysEINTR);
+
+          if (Res < 0) then
+          begin
+            Print(SysErrorMessage(fpGetErrNo));
+          end;
+          inhibit_fd:= feInvalidHandle;
+        end;
+      end;
+    end;
+  finally
+    System.LeaveCriticalSection(Mutex);
+  end;
+end;
+
+procedure Initialize;
+var
+  error: DBusError;
+begin
+  dbus_error_init(@error);
+  System.InitCriticalSection(Mutex);
+  conn:= dbus_bus_get(DBUS_BUS_SYSTEM, @error);
+  CheckError('Cannot acquire connection to DBUS system bus', @error);
+end;
+
+procedure Finalize;
+begin
+  System.DoneCriticalSection(Mutex);
+  if Assigned(conn) then dbus_connection_unref(conn);
 end;
 {$ELSE}
 procedure DisableSleep;
@@ -126,12 +276,12 @@ begin
   end;
 end;
 
-{$IF DEFINED(DARWIN)}
+{$IF DEFINED(DARWIN) OR DEFINED(LINUX)}
 initialization
-  System.InitCriticalSection(Mutex);
+  Initialize;
 
 finalization
-  System.DoneCriticalSection(Mutex);
+  Finalize;
 {$ENDIF}
 
 end.
