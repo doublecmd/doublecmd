4f8ab152bf33493796f29134da3a808e722d1709
rich2014 <rich2014.git@outlook.com>
2026-01-25 17:06:03 +0800

    FIX: the issue of frmMain losing focus in extreme cases, it can now recover focus automatically
    (cherry picked from commit 8a36e69eba0bc5ab6c6491f214d8c149fcac16d1)

--- src/fmain.pas
+++ src/fmain.pas
@@ -4224,6 +4224,9 @@ var
 begin
   SetDragCursor(Shift);
 
+  if ActiveControl = nil then
+    ActiveFrame.SetFocus;
+
   // Either left or right panel has to be focused.
   if not FrameLeft.Focused and
      not FrameRight.Focused then
