ace234fb79cb63ec0c55b57f6a49fd3df63508bb
Alexander Koblov <alexx2000@mail.ru>
2026-07-11 19:44:08 +0300

    ADD: Find files - cm_GoToFile parameter 'close=0/1'

--- src/fFindDlg.pas
+++ src/fFindDlg.pas
@@ -1976,14 +1976,21 @@ end;
 { TfrmFindDlg.cm_GoToFile }
 procedure TfrmFindDlg.cm_GoToFile(const Params: array of string);
 var
+  AValue: String;
   AFile: TFile = nil;
   TargetFile: string;
   ArchiveFile: string;
+  AClose: Boolean = True;
   FileSource: IFileSource;
 begin
   if lsFoundedFiles.ItemIndex <> -1 then
     try
-      StopSearch;
+      if (Length(Params) > 0) then
+      begin
+        if GetParamValue(Params, 'close', AValue) then
+          GetBoolValue(AValue, AClose);
+      end;
+      if AClose then StopSearch;
       TargetFile := lsFoundedFiles.Items[lsFoundedFiles.ItemIndex];
       if (ObjectType(lsFoundedFiles.ItemIndex) = cbChecked) then
       begin
@@ -2011,7 +2018,7 @@ begin
         frmMain.ActiveFrame.SetActiveFile(ExtractFileName(TargetFile));
       end;
       frmMain.RestoreWindow;
-      Close;
+      if AClose then Close;
     except
       on E: Exception do MessageDlg(E.Message, mtError, [mbOK], 0);
     end;
@@ -2568,6 +2575,12 @@ begin
           cm_GotoFile([]);
           Key := 0;
         end;
+      end;
+
+      VK_SPACE:
+      begin
+        cm_GotoFile(['close=0']);
+        Key := 0;
       end;
 
       VK_RIGHT, VK_LEFT:
