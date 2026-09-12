96445dec1a4378f1f82e8ca190df70aca67f3fab
Demetrius flavious <lahavlior@gmail.com>
2026-09-12 14:34:20 +0300

    FIX: Navigate to correct file source when opening path from command line (#2902)
    Co-authored-by: Alexander Koblov <alexx2000@mail.ru>

--- src/fmain.pas
+++ src/fmain.pas
@@ -977,7 +977,7 @@ uses
   Laz2_XMLRead, DCOSUtils, DCStrUtils, fOptions, fOptionsFrame, fOptionsToolbar, uClassesEx,
   uHotDir, uFileSorting, DCBasicTypes, foptionsDirectoryHotlist, uConnectionManager,
   fOptionsToolbarBase, fOptionsToolbarMiddle, fEditor, uColumns, StrUtils, uSysFolders,
-  uColumnsFileView, dmHigh, uFileSourceOperationMisc
+  uColumnsFileView, dmHigh, uFileSourceOperationMisc, uVfsModule
 {$IFDEF MSWINDOWS}
   , uShellFileSource, uNetworkThread
 {$ENDIF}
@@ -6409,18 +6409,31 @@ end;
 procedure TfrmMain.LoadTabsCommandLine(Params: TCommandLineParams);
 
   procedure LoadPanel(aNoteBook: TFileViewNotebook; aPath: String);
+  var
+    AFileView: TFileView;
   begin
     if Length(aPath) <> 0 then
     begin
       aPath:= ReplaceEnvVars(ReplaceTilde(aPath));
-      if not mbFileSystemEntryExists(aPath) then
-        aPath:= GetDeepestExistingPath(aPath);
+      if (gVfsModuleList.GetFileSource(aPath) = nil) then
+      begin
+        if not mbFileSystemEntryExists(aPath) then
+          aPath:= GetDeepestExistingPath(aPath);
+      end;
       if Length(aPath) <> 0 then
       begin
         if Params.NewTab then
           AddTab(aNoteBook, aPath)
-        else
-          aNoteBook.ActivePage.FileView.ChangePathAndSetActiveFile(aPath)
+        else begin
+          AFileView:= aNoteBook.ActivePage.FileView;
+          if mbFileExists(aPath) then
+          begin
+            if ChooseFileSource(AFileView, ExtractFileDir(aPath)) then
+              AFileView.SetActiveFile(ExtractFileName(aPath));
+          end
+          else
+            ChooseFileSource(AFileView, aPath);
+        end;
       end;
     end;
   end;
