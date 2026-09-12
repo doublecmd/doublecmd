032c95e41310650b1a248922a517b1ceec2d914c
Alexander Koblov <alexx2000@mail.ru>
2026-09-12 14:31:50 +0300

    Update fmain.pas

--- src/fmain.pas
+++ src/fmain.pas
@@ -969,7 +969,7 @@ uses
   Laz2_XMLRead, DCOSUtils, DCStrUtils, fOptions, fOptionsFrame, fOptionsToolbar, uClassesEx,
   uHotDir, uFileSorting, DCBasicTypes, foptionsDirectoryHotlist, uConnectionManager,
   fOptionsToolbarBase, fOptionsToolbarMiddle, fEditor, uColumns, StrUtils, uSysFolders,
-  uColumnsFileView, dmHigh, uFileSourceOperationMisc
+  uColumnsFileView, dmHigh, uFileSourceOperationMisc, uVfsModule
 {$IFDEF MSWINDOWS}
   , uShellFileSource, uNetworkThread
 {$ENDIF}
@@ -6370,8 +6370,11 @@ procedure TfrmMain.LoadTabsCommandLine(Params: TCommandLineParams);
     if Length(aPath) <> 0 then
     begin
       aPath:= ReplaceEnvVars(ReplaceTilde(aPath));
-      if not mbFileSystemEntryExists(aPath) and (Pos('\\', aPath) <> 1) then
-        aPath:= GetDeepestExistingPath(aPath);
+      if (gVfsModuleList.GetFileSource(aPath) = nil) then
+      begin
+        if not mbFileSystemEntryExists(aPath) then
+          aPath:= GetDeepestExistingPath(aPath);
+      end;
       if Length(aPath) <> 0 then
       begin
         if Params.NewTab then
